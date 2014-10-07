124904045c4cb962c4c22a6d69c781fe42035794
lixiangcliff <lixiang.cliff@gmail.com>
2014-10-06 22:07:28 -0700

    Reverse_Nodes_in_k_Group

--- lc/src/Reverse_Nodes_in_k_Group/Question.java
+++ lc/src/Reverse_Nodes_in_k_Group/Question.java
@@ -22,11 +22,11 @@ public class Question {
 			head = head.next;
 		}
 		System.out.println("");
-		//ListNode head2 = reverseKGroup(l1, 3);
-/*		while(head2 != null){
+		ListNode head2 = reverseKGroup(l1, 2);
+		while(head2 != null){
 			System.out.print(head2.val + ",");
 			head2 = head2.next;
-		}*/
+		}
 	}
 	//my way;
 /*    public static ListNode reverseKGroup(ListNode head, int k) {
@@ -70,15 +70,47 @@ public class Question {
     }*/
     
     //http://blog.csdn.net/linhuanmars/article/details/19957455
-    //need to understand "reversre" function well
-/*    public ListNode reverseKGroup(ListNode head, int k) {  
-    	
-    }
-    
-    private ListNode reverse(ListNode pre, ListNode end) {
-    	
-    }*/
-
+    //need to understand "reverse" function well
+	public static ListNode reverseKGroup(ListNode head, int k) {  
+	    if(head == null)  
+	    {  
+	        return null;  
+	    }  
+	    ListNode dummy = new ListNode(0);  
+	    dummy.next = head;  
+	    int count = 0;  
+	    ListNode pre = dummy;  
+	    ListNode cur = head;  
+	    while(cur != null)  
+	    {  
+	        count ++;  
+	        ListNode next = cur.next;  
+	        if(count == k)  // only do reverse when reach to k nodes
+	        {  
+	            pre = reverse(pre, next);  // new pre step to k nodes next to it
+	            count = 0;     
+	        }  
+	        cur = next;  
+	    }  
+	    return dummy.next;  
+	}  
+	// this makes sure after reverse this k nodes, they still connect to k-nodes' both left and right sides
+	private static ListNode reverse(ListNode pre, ListNode end)  
+	{  
+	    if(pre==null || pre.next==null)  
+	        return pre;  
+	    ListNode head = pre.next;  
+	    ListNode cur = pre.next.next;  
+	    while(cur!=end)  
+	    {  
+	        ListNode next = cur.next;  
+	        cur.next = pre.next;  
+	        pre.next = cur;  // connect from "left" side
+	        cur = next;  
+	    }  
+	    head.next = end;  // connect from "right" side (connect kth node to k.next)
+	    return head;  // head will be new "pre" for next possible k-group nodes
+	}  
 }
 
 class ListNode {

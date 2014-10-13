6e950e418d9ddf6acb25d0357d17d8ee45d783f8
lixiangcliff <lixiang.cliff@gmail.com>
2014-10-12 17:27:35 -0700

    addig O(n) time complex method by using DP

--- lc/src/Jump_Game/Question.java
+++ lc/src/Jump_Game/Question.java
@@ -7,11 +7,12 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] A = {2,0,0};
+		int[] A = {0,2,3};
 		System.out.println(canJump(A));
 	}
 	
-    public static boolean canJump(int[] A) {
+	//my way : O(n^2) time complex
+/*    public static boolean canJump(int[] A) {
         if(A == null || A.length == 0 || A.length > 1 && A[0] == 0){
         	return false;
         }
@@ -19,10 +20,6 @@ public class Question {
         	if(A[i]==0){
         		boolean found = false;
         		for(int j=i-1;j>=0;j--){
-/*        			System.out.println("j:" + j);
-        			System.out.println("A[j]:" + A[j]);
-        			System.out.println("i-j:" + (i-j));
-        			System.out.println("=========");*/
         		    // i==A.length-1 && A[j]>=i-j ---- when 0 at last index
         		    // A[j]>i-j ---- when 0 at other index
         			if (i==A.length-1 && A[j]>=i-j || A[j]>i-j){
@@ -36,6 +33,29 @@ public class Question {
         	}
         }
         return true;
-    }
+    }*/
+	
+	//O(n) time complex
+	//http://blog.csdn.net/linhuanmars/article/details/21354751
+	public static boolean canJump(int[] A) {
+		if (A==null || A.length==0){
+			return false;
+		}
+		//int global = A[0]; 
+		//for(int i= 1; i< A.length-1;i++){
+		//above and below both work!
+		int global = 0;
+		for(int i= 0; i< A.length-1;i++){ 
+			if (global < i){ //means current global cannot reach current i;
+				return false;
+			}
+			int local = A[i] + i;//at current i, local is the longest index it can reach
+			global = Math.max(local, global); //"global" is longest index it can ever reach for each current i;
+			if (global >= A.length-1){ // already find one index that can jump to the end directly
+				return true;
+			}
+		}
+		return (global >= A.length-1);
+	}
 
 }

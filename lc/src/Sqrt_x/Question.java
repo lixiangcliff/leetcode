a1168cc71b7834e9c16df7f31016af6a0356903f
lixiangcliff <lixiang.cliff@gmail.com>
2014-10-16 16:07:51 -0700

    create Sqrt_x

---
+++ lc/src/Sqrt_x/Question.java
@@ -0,0 +1,17 @@
+package Sqrt_x;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public int sqrt(int x) {
+        return 0;
+    }
+
+}

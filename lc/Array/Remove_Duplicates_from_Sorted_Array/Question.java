c91c87cc8af721744b21dd8f2c8b4bbaff9d5dee
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-05 09:01:58 -0800

    update Remove_Duplicates_from_Sorted_Array

--- lc/Array/Remove_Duplicates_from_Sorted_Array/Question.java
+++ lc/Array/Remove_Duplicates_from_Sorted_Array/Question.java
@@ -17,40 +17,30 @@ public class Question {
 
 	}
 	
-	//got runtime error on leetcode oj. don't know why..
- /*   public static int removeDuplicates(int[] A) {
-    	if(A == null){
-    		return 0;
-    	}
-    	if (A.length == 1){
-    		return 1;
-    	}
-    	int current = 0;
-    	int end = 1;
-    	while(end < A.length){
-    		while(A[current] == A[end]){
-    			if (end == A.length-1){
-    				return current+1;
-    			}
-    			end++;
-    		}
-    		A[++current] = A[end++];
-    	}
-        return current+1;
-    }
-    */
-    //http://answer.ninechapter.com/solutions/remove-duplicates-from-sorted-array/   
+	//http://blog.csdn.net/linhuanmars/article/details/20023993
     public static int removeDuplicates(int[] A) {
     	if(A == null || A.length==0){
     		return 0;
     	}
-    	int size =0;
-    	for (int i=0; i<A.length;i++ ){
-    		if(A[size] != A[i]){
-    			A[++size] = A[i];
+    	/*
+    	 * index表示的值有两层含义:
+    	 * 1.它作为一个位标记，凡是它左边的都是比较过之后确保没有重复的元素
+    	 * 2.同时它也从数值上表示了左半边数组的长度
+    	 * 【注】这种一举两得的方式经常遇到
+    	 */
+    	int index = 1;
+    	for (int i=1; i< A.length; i++){
+    		/*
+    		 * 比较相邻两个数，只有不相等才更新index位上的值
+    		 * 否则什么也不做，相当于 continue
+    		 */
+    		if(A[i-1] != A[i]){
+    			//更细完index位的值之后，index++；
+    			A[index++] = A[i];
     		}
     	}
-    	return size+1;
+    	//index可以表示左半边数组长度，可以直接返回
+    	return index;
     }
 
 }

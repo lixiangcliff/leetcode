e10a4f721271b0da0c5cf5467cc524535f6746f9
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-04 23:09:01 -0800

    update Pascals_Triangle_II

--- lc/Array/Pascals_Triangle_II/Question.java
+++ lc/Array/Pascals_Triangle_II/Question.java
@@ -15,51 +15,25 @@ public class Question {
 		}
 	}
 	
-/*    public ArrayList<Integer> getRow(int rowIndex) {
-    	ArrayList<Integer> currentRow = new ArrayList<Integer>(); 
-		if (rowIndex < 0){
-			return currentRow;
-		}
-		ArrayList<Integer> preRow = new ArrayList<Integer>();
-		preRow.add(1);
-		currentRow = preRow;
-		for(int i=0; i< rowIndex ;i++){
-			currentRow = getRow(preRow);
-			preRow = currentRow;
-		}
-		return currentRow;
-    }
-	
-	private ArrayList<Integer> getRow(ArrayList<Integer> preRow){
-		int count = preRow.size();
-		ArrayList<Integer> newRow = new ArrayList<Integer>();
-		newRow.add(1);
-		for(int i=1;i<count;i++){
-			newRow.add(preRow.get(i-1) + preRow.get(i));
-		}
-		newRow.add(1);
-		return newRow;
-	}*/
-	
 	//to use O(k) space only: rolling array
+	//http://blog.csdn.net/linhuanmars/article/details/23311629
 	//http://blog.csdn.net/abcbc/article/details/8982651
 	//http://fisherlei.blogspot.com/2012/12/leetcode-pascals-triangle-ii.html
-	
+	//一维DP，从后往前扫。
+	//【注】因为数据类型为ArrayList所以随着add元素，size会增长,所以要处理好后面的index问题
+	//而且ArrayList的元素一定要先数据，之后才能改值，即要先add 然后才能set
+	//【注】此题和Pascals_Triangle_I 对kth row的定义有区别
 	public static ArrayList<Integer> getRow(int rowIndex) {
 		ArrayList<Integer> result = new ArrayList<Integer>(); 
 		if (rowIndex < 0){
 			return result;
 		}
-		
-		for (int i=0;i<=rowIndex;i++){
-			result.add(0);
-		}
-		result.set(0, 1);
-		for(int i=1;i<=rowIndex;i++){
-			result.set(i, 1);
-			for(int j=i-1;j>0;j--){
+		result.add(1);//row 0
+		for(int i=1;i<=rowIndex;i++){//从第row 1到第row n
+			for(int j=result.size()-1;j>0;j--){//从后往前扫
 				result.set(j, result.get(j) + result.get(j-1));
 			}
+			result.add(1);//补最后一个1
 		}
 		return result;
 	}

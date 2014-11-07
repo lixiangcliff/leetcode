b3a956e2a2e78645ec6980701eb20683ec814f73
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-07 15:50:15 -0800

    update Reverse_Nodes_in_k_Group

--- lc/LinkedList/Reverse_Nodes_in_k_Group/Question.java
+++ lc/LinkedList/Reverse_Nodes_in_k_Group/Question.java
@@ -82,35 +82,38 @@ public class Question {
 	    int count = 0;  
 	    ListNode pre = dummy;  
 	    ListNode cur = head;  
-	    while(cur != null)  
-	    {  
+	    while(cur != null) {  
 	        count ++;  
-	        ListNode next = cur.next;  
-	        if(count == k)  // only do reverse when reach to k nodes
-	        {  
-	            pre = reverse(pre, next);  // new pre step to k nodes next to it
+	        ListNode next = cur.next; //要提前把cur.next存好。不然如果经过if里面的处理, 再取cur.next，取的就不是原来的了
+	        if(count == k){  // only do reverse when reach to k nodes
+	            pre = reverse(pre, next);  // 新的pre往右挪了k个node的位子
 	            count = 0;     
 	        }  
 	        cur = next;  
 	    }  
 	    return dummy.next;  
 	}  
-	// this makes sure after reverse this k nodes, they still connect to k-nodes' both left and right sides
-	private static ListNode reverse(ListNode pre, ListNode end)  
-	{  
-	    if(pre==null || pre.next==null)  
+	/*这个函数不但把pre和end之间的所有node reverse了，而且保证reverse之后，中间被reverse的node 左边仍连接平pre 右边仍连接end
+	 * 关于这个函数的两个参数pre 和 end
+	 * pre的下一个node是k个node里面的第1个
+	 * end的前一个node是k个node里面的第k个
+	 * 换言之：pre和end之间夹的node一共有k个
+	 */
+	private static ListNode reverse(ListNode pre, ListNode end) {  
+	    if(pre==null || pre.next==null){  
 	        return pre;  
+	    }
+	    //把head保留起来，reverse之后和end接上
 	    ListNode head = pre.next;  
 	    ListNode cur = pre.next.next;  
-	    while(cur!=end)  
-	    {  
+	    while(cur!=end) {  
 	        ListNode next = cur.next;  
 	        cur.next = pre.next;  
-	        pre.next = cur;  // connect from "left" side
+	        pre.next = cur;  // connect from "left" side每一轮都把右边挪过来的node和左边的pre接上
 	        cur = next;  
 	    }  
-	    head.next = end;  // connect from "right" side (connect kth node to k.next)
-	    return head;  // head will be new "pre" for next possible k-group nodes
+	    head.next = end;  // connect from "right" side (connect kth node to k.next)最后把之前保留好的head和end接上
+	    return head;  // 返回的head作为下一轮新的pre（如果还有下一轮的话）
 	}  
 }
 

e9cd499db44e38c087f6d732619661c250006083
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-06 17:08:48 -0800

    update Remove_Duplicates_from_Sorted_List

--- lc/LinkedList/Remove_Duplicates_from_Sorted_List/Question.java
+++ lc/LinkedList/Remove_Duplicates_from_Sorted_List/Question.java
@@ -10,24 +10,27 @@ public class Question {
 
 	}
 	
+	//http://blog.csdn.net/linhuanmars/article/details/24354291
 	public static ListNode deleteDuplicates(ListNode head) {
+		/*
+		 * 思路是维护两个指针，一个指向不重复的最后一个node，另一个作为当前node往后跑
+		 */
 		if(head==null || head.next ==null){
 			return head;
 		}
-		ListNode previous = head;
-		ListNode current = head.next;
-		
-		while(current != null){
-			if (current.val == previous.val){
-				previous.next = current.next;
-			}else{
-			    previous =  previous.next;
+		ListNode pre = head;
+		ListNode cur = head.next;
+		while(cur != null){
+			//如果重复了，则把当前node剔除
+			if (cur.val == pre.val){
+				pre.next = cur.next;
+			}else{//若不重复，则pre向下移动
+			    pre =  pre.next;
 			}
-			current = current.next;
+			cur = cur.next;
 		}		
 		return head;        
     }
-
 }
 
 

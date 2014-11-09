5d47270549b3a84006f9b6ba368c90d5e5d48cc3
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-08 23:01:59 -0800

    update Valid_Parentheses

--- lc/Stack&Queue/Valid_Parentheses/Question.java
+++ lc/Stack&Queue/Valid_Parentheses/Question.java
@@ -12,6 +12,7 @@ public class Question {
 		System.out.println(isValid("([)]"));
 	}
 	
+	//http://blog.csdn.net/linhuanmars/article/details/19800789
     public static boolean isValid(String s) {
     	if (s == null || s.length() == 0 || s.length() %2 ==1){
     		return false;
@@ -22,22 +23,22 @@ public class Question {
     	Stack<Character> stack = new Stack<Character>();
     	for(int i=0;i<s.length();i++){
     		char curChar = s.charAt(i);
-    		if(left.indexOf(curChar) > -1){
+    		if(left.indexOf(curChar) > -1){//如果是左括号就入栈
     			stack.push(curChar);
-    		}else if(right.indexOf(curChar) > -1){
-    			if (stack.isEmpty()){
+    		}else if(right.indexOf(curChar) > -1){//如果是右括号就来试着匹配栈顶的左括号
+    			if (stack.isEmpty()){//如果栈已经为空，则异常
     				return false;
     			}else{
     				char preChar = stack.pop();
-    				if(left.indexOf(preChar) != right.indexOf(curChar)){
+    				if(left.indexOf(preChar) != right.indexOf(curChar)){//不匹配， 则false
     					return false;
     				}
     			}
-    		}else{
+    		}else{//当前char既不是左括号 也不是右括号， 则false
     			return false;
     		}
     	}
-    	
+    	//如果栈内什么都没剩下, 则true
     	return stack.isEmpty();
     }
 

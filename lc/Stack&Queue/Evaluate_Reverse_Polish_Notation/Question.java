b3e830cdb7f31c4f0d8da254ede3958da384a04a
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-08 21:04:45 -0800

    update Evaluate_Reverse_Polish_Notation

--- lc/Stack&Queue/Evaluate_Reverse_Polish_Notation/Question.java
+++ lc/Stack&Queue/Evaluate_Reverse_Polish_Notation/Question.java
@@ -19,25 +19,49 @@ public class Question {
     		return 0;
     	}
         LinkedList<Integer> stack = new LinkedList<Integer>();
+    	/*
+    	 * 加法和乘法比较直接
+    	 * 减法和除法有trick。尤其除法要把弹出的两个数先存起来，再做运算。
+    	 * 每次pop之前，应该先判断stack是否为空，如果为空则报错。
+    	 */
         for(int i=0;i<tokens.length;i++){
-        	if(tokens[i].equals("+")){
-        		stack.push(stack.pop() + stack.pop());
-        	}else if(tokens[i].equals("-")){
-        		stack.push(-stack.pop() + stack.pop());
-        	}else if(tokens[i].equals("*")){
-        		stack.push(stack.pop() * stack.pop());
-        	}else if(tokens[i].equals("/")){
-        		//below is wrong:
-        		//imagine: 5/4 == 1; but if you do 1/4 * 5 == 0 * 5 == 0
-        		//stack.push((1/stack.pop()) * stack.pop());
-        		int num1 = stack.pop();
-        		int num2 = stack.pop();
-        		stack.push(num2/num1);
-        	}else{
+        	if (!tokens[i].equals("+") && !tokens[i].equals("-") && !tokens[i].equals("*") && !tokens[i].equals("/")){
         		stack.push(Integer.parseInt(tokens[i]));
+        	}else{
+        		int num1 = Integer.MAX_VALUE;
+        		int num2 = Integer.MAX_VALUE;
+        		if (!stack.isEmpty()){
+        			num1 = stack.pop();
+        		}else{
+        			return Integer.MAX_VALUE;
+        		}
+        		if (!stack.isEmpty()){
+        			num2 = stack.pop();
+        		}else{
+        			return Integer.MAX_VALUE;
+        		}
+        		
+            	if(tokens[i].equals("+")){
+            		stack.push(num1+num2);
+            	}else if(tokens[i].equals("-")){
+            		stack.push(num2-num1);
+            	}else if(tokens[i].equals("*")){
+            		stack.push(num1*num2);
+            	}else if(tokens[i].equals("/")){
+            		stack.push(num2/num1);
+            	}
         	}
         }
-        return stack.pop();
+        
+        int result = Integer.MAX_VALUE;
+        if(!stack.isEmpty()){
+        	result = stack.pop();
+        }
+        if(stack.isEmpty()){
+        	return result;
+        }else{
+        	return Integer.MAX_VALUE;
+        }
     }
-
+    
 }

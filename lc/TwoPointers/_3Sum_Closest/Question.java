f48129a198449f9cf776f9d89671f7b682eb316c
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-11 17:24:27 -0800

    update _3Sum_Closest

--- lc/TwoPointers/_3Sum_Closest/Question.java
+++ lc/TwoPointers/_3Sum_Closest/Question.java
@@ -12,34 +12,53 @@ public class Question {
 
 	}
 	
-	//http://answer.ninechapter.com/solutions/3sum-closest/
+	//http://blog.csdn.net/linhuanmars/article/details/19712011
     public int threeSumClosest(int[] num, int target) {
-        if(num == null || num.length <3){
-        	return Integer.MAX_VALUE; // means does not return a normal value;
+        if(num == null || num.length <=2){
+        	return Integer.MIN_VALUE; // means does not return a normal value;
         }
+        //【注】排序不能忘！
         Arrays.sort(num);
-        int diff = Integer.MAX_VALUE;
-        int result = Integer.MAX_VALUE/2;// otherwise it will overflow for opeartion (closet-target)'
-        for(int i=0; i<num.length-2;i++){
-        	int left = i+1;
-        	int right = num.length-1;
-        	while(left<right){
-        		int sum = num[i] + num[left] + num[right];
-        		if(sum == target){
-        			return sum;
-        		}else if(sum>target){
-        			right--;
-        		}else {
-        			left++;
-        		}
-        		
-        		if(Math.abs(sum-target)<diff){
-        			diff = Math.abs(sum-target);
-        			result = sum;
-        		}
+        int closest = num[0] + num[1] + num[2] - target;//closest赋初值
+        for (int i = 0; i < num.length - 2; i++){
+        	//为什么是target - num[i]而不是num[i] - target可以这样理解：
+        	//目标是使等式num[i]+num[j]+num[k] = target成立, 也就是使num[j]+num[k] = target-num[i]成立
+        	int curClosest = twoSum(num, target - num[i], i+1);//i+1是从i之后第一个数算起
+        	if (curClosest == 0){//找到最优解
+        		return target;
+        	}
+        	if (Math.abs(curClosest) < Math.abs(closest)){
+        		closest = curClosest;
         	}
         }
-        return result;
+        //可以这样理解，返回的是最接近的num[i]+num[j]+num[k]，而closest = num[0]+num[1]+num[2]-target, 所有下面的return值
+        return closest + target;
+    }
+    
+    //变形的subroutine,目标是找到两个数，使他们的和最接近target,返回的是他们的和与target的差值
+    //http://blog.csdn.net/linhuanmars/article/details/19711387
+    private int twoSum(int[] num, int curTarget, int start){
+    	if (num == null || num.length <= 1){
+    		return Integer.MAX_VALUE;
+    	}
+    	int l = start;
+    	int r = num.length - 1;
+    	int curClosest = num[start] + num[start+1] - curTarget; //curClosest赋初值
+    	while(l < r){
+    		if(num[l] + num[r] == curTarget){ //已经找到最优解
+    			return 0;
+    		}
+    		int tempDiff = num[l] + num[r] - curTarget;//如果当前的l和r的和会更接近target，则更新closet
+    		if (Math.abs(tempDiff) < Math.abs(curClosest)){
+    			curClosest = tempDiff;
+    		}
+    		if (num[l] + num[r] < curTarget){
+    			l++;
+    		}else{
+    			r--;
+    		}
+    	}
+    	return curClosest;
     }
 
 }

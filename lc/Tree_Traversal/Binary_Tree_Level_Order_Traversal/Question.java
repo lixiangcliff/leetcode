bed6b74c7371d8e41951f3f442bd8f5447bc8981
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-13 15:06:59 -0800

    update Binary_Tree_Level_Order_Traversal. There is a question need to ask trainer

--- lc/Tree_Traversal/Binary_Tree_Level_Order_Traversal/Question.java
+++ lc/Tree_Traversal/Binary_Tree_Level_Order_Traversal/Question.java
@@ -2,6 +2,7 @@ package Binary_Tree_Level_Order_Traversal;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Question {
 
@@ -13,34 +14,41 @@ public class Question {
 
 	}
 	
+	//http://blog.csdn.net/linhuanmars/article/details/23404111
+	//类似此题的iterative way：https://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
+	public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		if (root == null){
+			return result;
+		}
+		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+		int curNum = 1;
+		int nextNum = 0;
+		queue.offer(root);
+		ArrayList<Integer> item = new ArrayList<Integer>();
+		while(!queue.isEmpty()){
+			TreeNode node = queue.poll();
+			item.add(node.val);
+			curNum--;
+			if (node.left != null){
+				queue.offer(node.left);
+				nextNum++;
+			}
+			if (node.right != null){
+				queue.offer(node.right);
+				nextNum++;
+			}
+			if (curNum == 0){
+				curNum = nextNum;
+				nextNum = 0;
+				result.add(item);
+				//item.clear();【注】，这样写错误！原因问问老师。
+				item = new ArrayList<Integer>();
+			}
+		}
+		return result;
+	}
 	
-	//http://answer.ninechapter.com/solutions/binary-tree-level-order-traversal/
-    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
-        //Compile Error: incompatible types from leetcode
-        //ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        ArrayList result = new ArrayList();
-    	if(root == null){
-        	return result;
-        }        
-        ArrayDeque<TreeNode> queue=new ArrayDeque<TreeNode>();   
-        queue.add(root);
-        while(!queue.isEmpty()){        	
-        	ArrayList<Integer> level = new ArrayList<Integer>();
-        	int size = queue.size();
-        	for (int i=0;i < size;i++){
-        		TreeNode node = queue.remove();
-        		level.add(node.val);
-        		if(node.left != null){
-            		queue.add(node.left);
-            	}
-            	if(node.right != null){
-            		queue.add(node.right);
-            	}
-        	}
-        	result.add(level);        	
-        }
-        return result;
-    }
 }
 
  class TreeNode {

d3218c4615c1da94ebda98cd960917866b148899
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-12 23:01:06 -0800

    updating Symmetric_Tree using both recursive and iterative way

--- lc/Tree_Nature/Symmetric_Tree/Question.java
+++ lc/Tree_Nature/Symmetric_Tree/Question.java
@@ -1,5 +1,7 @@
 package Symmetric_Tree;
 
+import java.util.LinkedList;
+
 public class Question {
 
 	/**
@@ -9,20 +11,65 @@ public class Question {
 		// TODO Auto-generated method stub
 
 	}
+	
+	//http://blog.csdn.net/linhuanmars/article/details/23072829
+	//recursive
     public boolean isSymmetric(TreeNode root) {
     	if(root == null){
     		return true;
     	}
-        return isSymmetric(root.left, root.right);
+        return helper(root.left, root.right);
     }
-    private boolean isSymmetric(TreeNode l, TreeNode r) {
+    private boolean helper(TreeNode l, TreeNode r) {
     	if (l == null && r == null){
     		return true;
     	}
     	if (l ==null || r == null){
     		return false;
     	}
-        return (l.val == r.val) && isSymmetric(l.left, r.right) && isSymmetric(l.right, r.left);
+    	if (l.val != r.val){
+    		return false;
+    	}
+        return helper(l.left, r.right) && helper(l.right, r.left);
+    }
+    
+    //iterative way
+    public boolean isSymmetricBSF(TreeNode root) {
+    	if(root == null){
+    		return true;
+    	}
+    	if (root.left == null && root.right == null){
+    		return true;
+    	}
+    	if (root.left == null || root.right == null){
+    		return false;
+    	}
+    	LinkedList<TreeNode> q1 = new LinkedList<TreeNode>();
+    	LinkedList<TreeNode> q2 = new LinkedList<TreeNode>();
+    	q1.offer(root.left);
+    	q2.offer(root.right);
+    	while(!q1.isEmpty() && !q2.isEmpty()){
+    		TreeNode n1 = q1.poll();
+    		TreeNode n2 = q2.poll();
+    		if(n1.val != n2.val){
+    		    return false;
+    		}
+    		if(n1.left != null && n2.right == null || n1.left == null && n2.right != null){
+    			return false;
+    		}
+    		if(n1.right != null && n2.left == null || n1.right == null && n2.left != null){
+    			return false;
+    		}
+    		if(n1.left != null && n2.right != null){
+    			q1.offer(n1.left);
+    			q2.offer(n2.right);
+    		}
+    		if(n1.right != null && n2.left != null){
+    			q1.offer(n1.right);
+    			q2.offer(n2.left);
+    		}
+    	}
+    	return true;
     }
     
     

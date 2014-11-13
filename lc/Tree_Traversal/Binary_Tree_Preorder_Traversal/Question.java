e755863ed05272e136d57b9bafbfeac5b3c49456
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-13 12:21:57 -0800

    update Binary_Tree_Preorder_Traversal using recursive and iterative way

--- lc/Tree_Traversal/Binary_Tree_Preorder_Traversal/Question.java
+++ lc/Tree_Traversal/Binary_Tree_Preorder_Traversal/Question.java
@@ -1,6 +1,6 @@
 package Binary_Tree_Preorder_Traversal;
-import java.util.Stack;
 import java.util.ArrayList;
+import java.util.LinkedList;
 public class Question {
 
 	/**
@@ -11,27 +11,43 @@ public class Question {
 
 	}
 	
-	public static ArrayList<Integer> preorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        ArrayList<Integer> preorder = new ArrayList<Integer>();
-        
-        if (root == null) {
-            return preorder;
-        }       
-        stack.push(root);
-        while (!stack.empty()) {
-            TreeNode node = stack.pop(); // we need to keep this node!!!
-            preorder.add(node.val);    
-            if (node.right != null) {
-                stack.push(node.right);  // here is why we need to keep it. and we need to push right before left!
-            }
-            if (node.left != null) {
-                stack.push(node.left);
-            }
+	//http://blog.csdn.net/linhuanmars/article/details/21428647
+	//recursive way
+	public ArrayList<Integer> preorderTraversal(TreeNode root) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		helper(root, result);
+		return result;
+		
+	}
+	
+	private void helper(TreeNode root, ArrayList<Integer> result){
+		if (root == null){
+			return;
+		}
+		result.add(root.val);
+		helper(root.left, result);
+		helper(root.right, result);
+	}
+	
+	//iterative way
+	//更多注释，参考inOrder： https://oj.leetcode.com/problems/binary-tree-inorder-traversal/
+	public static ArrayList<Integer> preorderTraversalIterative(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        while(root != null || !stack.isEmpty()){
+        	if(root != null){
+        		result.add(root.val);//【根】
+        		stack.push(root);
+        		root = root.left; //【左】
+        	}else{
+        		root = stack.pop();
+        		root = root.right; //【右】
+        	}
         }
-        return preorder;
+        return result;
     }
 
+	//Morris Taversal to be continue later..
 }
 
 

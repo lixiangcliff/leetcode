c5f6899d5d3a0edfc0079906b0fe1d240fa139c8
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-14 14:02:14 -0800

    update Search_in_Rotated_Sorted_Array

--- lc/Binary_Search/Search_in_Rotated_Sorted_Array/Question.java
+++ lc/Binary_Search/Search_in_Rotated_Sorted_Array/Question.java
@@ -13,6 +13,7 @@ public class Question {
 	}
 	
 	//http://blog.csdn.net/linhuanmars/article/details/20525681
+	//看图！（此题元素不重复）
     public static int search(int[] A, int target) {
     	if(A == null || A.length == 0){
     		return -1;
@@ -21,25 +22,25 @@ public class Question {
     	int r = A.length-1;
     	while(l<=r){
     		int m = (l+r)/2;
-    		if (target == A[m]){
+    		if (target == A[m]){//找到target
     			return m;
     		}
-    		if(A[m] < A[r]){
-    			if(A[m] <= target && target <= A[r]){
-    				l = m+1;
+    		if(A[m] < A[r]){//说明从m到r这一部分是有序的
+    			if(A[m] <= target && target <= A[r]){ 
+    				l = m+1; //如果target在这一部分，更新左边界l变为m+1
     			}else{
-    				r = m-1;
+    				r = m-1; //如果target不在这一部分，更新右边界r变为m-1
     			}
-    		}else{
+    		}else{	//说明从l到m这一部分是有序的
     			if(A[l] <= target && target <= A[m]){
-    				r = m-1;
+    				r = m-1; //如果target在这一部分，更新右边界r变为m-1
     			}else{
-    				l = m+1;
+    				l = m+1; //如果target不在这一部分，更新左边界l变为m+1
     			}
     		}
     	}
     	
-        return -1;
+        return -1; //没找到
     }
 
 }

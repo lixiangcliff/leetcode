295e186f1601d390cab276eac1f407dd859bb5e6
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-13 22:35:40 -0800

    update Path_Sum_II and have a question

--- lc/Tree_Sum/Sum_Root_to_Leaf_Numbers/Question.java
+++ lc/Tree_Sum/Sum_Root_to_Leaf_Numbers/Question.java
@@ -10,18 +10,21 @@ public class Question {
 
 	}
 	//http://blog.csdn.net/kenden23/article/details/14100851
+	//算法的本质是一次先序遍历（为啥？），递归条件即是把当前的sum乘以10并且加上当前节点传入下一函数
+	//http://blog.csdn.net/linhuanmars/article/details/22913699
     public int sumNumbers(TreeNode root) {
-    	return doSumNumbers(root, 0);
+    	return helper(root, 0);
     }
     
-    private int doSumNumbers(TreeNode root, int sum){
-    	if (root == null){
+    private int helper(TreeNode root, int sum){
+    	if (root == null){//空节点，已经不是叶子了，不需要把数值加入结果，直接返回0
     		return 0;
     	}
-    	if(root.left == null && root.right == null){
+    	if(root.left == null && root.right == null){//已到达叶子节点，将这一个branch的结果加入总和中
     		return sum * 10 + root.val;
     	}else{
-    		return (doSumNumbers(root.left, sum*10+root.val) + doSumNumbers(root.right, sum*10+root.val));
+    		//不为空也不为叶子时，把左右两颗子树的返回结果相加
+    		return (helper(root.left, sum*10+root.val) + helper(root.right, sum*10+root.val));
     	}   	
     }
     

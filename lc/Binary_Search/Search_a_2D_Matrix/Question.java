573f34c82d5ba754240b45b133d7e30ba7c47ba0
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-14 15:23:58 -0800

    update Search_a_2D_Matrix

--- lc/Binary_Search/Search_a_2D_Matrix/Question.java
+++ lc/Binary_Search/Search_a_2D_Matrix/Question.java
@@ -21,34 +21,38 @@ public class Question {
 		
 
 	}
+	//http://blog.csdn.net/linhuanmars/article/details/24216235
     public static boolean searchMatrix(int[][] matrix, int target) {
-    	
-    	int rowLen = matrix.length;
-    	int colLen = matrix[0].length;
-    	int up= 0;
-    	int left = 0;
-    	int down = rowLen-1;
-    	int right = colLen-1;
-    	int findRow = 0;
-    	while(up <= down){
-    		int mid = (up+down)/2;
+    	if(matrix == null || matrix.length==0 || matrix[0].length==0)  
+            return false; 
+    	//处理所有行的第0个元素
+    	int l= 0;
+    	int r = matrix.length-1;
+    	while(l <= r){
+    		int mid = (l+r)/2;
     		if (target == matrix[mid][0]){
     			return true;
-    		}else if(target < matrix[mid][0]){
-    			down = mid-1;
+    		}else if(target > matrix[mid][0]){//target在右半边，所以重置左窗口
+    			l = mid+1;
     		}else{
-    			findRow = mid;
-    			up = mid+1;
+    			r = mid-1; //找的是比target小的最大值
     		}
-    	}    	
-    	while(left <= right){
-    		int mid = (left+right)/2;
-    		if (target == matrix[findRow][mid]){
+    	}
+    	int row = r;
+    	if (r < 0){//r越界，代表target比matrix[0][0]还要小，所以matrix里不包含target
+    		return false;
+    	}
+    	//处理这一行(row)的所有列
+    	l = 0;
+    	r = matrix[0].length-1;
+    	while(l <= r){
+    		int mid = (l+r)/2;
+    		if (target == matrix[row][mid]){
     			return true;
-    		}else if(target < matrix[findRow][mid]){
-    			right = mid-1;
+    		}else if(target > matrix[row][mid]){
+    			l = mid+1;
     		}else{
-    			left = mid+1;
+    			r = mid-1;
     		}
     	}
     	return false;

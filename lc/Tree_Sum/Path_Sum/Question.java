e6968a84c581d6704d036fa471bdc134c157ad06
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-13 19:26:22 -0800

    update Path_Sum

--- lc/Tree_Sum/Path_Sum/Question.java
+++ lc/Tree_Sum/Path_Sum/Question.java
@@ -9,26 +9,29 @@ public class Question {
 		// TODO Auto-generated method stub
 
 	}
-	
-	//http://blog.csdn.net/linhuanmars/article/details/23654413
-    public boolean hasPathSum(TreeNode root, int sum) {
-       if(root == null){
-    	   return false;
-       }
-       if(root.left == null && root.right == null && root.val == sum){
-    	   return true;
-       }
-       //wrong 
-       //return hasPathSum(root.left, sum - root.left.val ) || hasPathSum(root.right, sum - root.right.val );
-       //correct!
-       return hasPathSum(root.left, sum - root.val ) || hasPathSum(root.right, sum - root.val );
-    }
+
+	// http://blog.csdn.net/linhuanmars/article/details/23654413
+	public boolean hasPathSum(TreeNode root, int sum) {
+		if (root == null) {
+			return false;
+		}
+		// 说明此时root是叶子节点，并且剩余sum与其val相等，所以true
+		if (root.left == null && root.right == null && root.val == sum) {
+			return true;
+		}
+		//左右子树有任何一个符合的就行，所以用||连接
+		//继续的值为sum - root.val（不是sum - root.left.val！），因为要把当前的val（root.val）减去，然后继续递归。
+		return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+	}
 
 }
 
- class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
 }

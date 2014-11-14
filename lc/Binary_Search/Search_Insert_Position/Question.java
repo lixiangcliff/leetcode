1c77a0173e711a22e3a85b967962355501ffe1f9
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-14 13:00:38 -0800

    update Search_Insert_Position and have bunch of questions

--- lc/Binary_Search/Search_Insert_Position/Question.java
+++ lc/Binary_Search/Search_Insert_Position/Question.java
@@ -6,22 +6,34 @@ public class Question {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int A[] = {1,3,5,6};
+		int A[] = {1,2};
 		System.out.println(searchInsert(A, 0));
 	}
 	
-	public static int searchInsert(int[] A, int target) {
-		
-		if (A.length<1){
-			return 0;
-		}
-		for(int i=0; i<A.length;i++){
-			if(target <= A[i]){
-				return i;
-			}
-		}
-		return A.length;
-        
-    }
 
+	/*
+	 * 要找比target大的最小元素
+	 * 几个问题要问老师的
+	 */
+	//http://blog.csdn.net/linhuanmars/article/details/20278967
+	public static int searchInsert(int[] A, int target) {  
+	    if(A == null || A.length == 0)  
+	    {  
+	        return 0;  
+	    }  
+	    int l = 0;  
+	    int r = A.length-1;  
+	    while(l<=r)  //1. 循环条件中，l和r的关系，小于or小于等于
+	    {  
+	        int mid = (l+r+1)/2; //向上还是向下取整？这道题向上向下都行。为什么？  
+	        if(A[mid]==target)  
+	            return mid;  
+	        if(A[mid]<target)  
+	            l = mid+1; //l的边界怎么取？ 
+	        else  
+	            r = mid-1;  ////l的边界怎么取？ 
+	    }  
+	    //"如果没有找到目标元素，那么l一定停在恰好比目标大的index上，r一定停在恰好比目标小的index上" 为什么？
+	    return l;  
+	}  
 }

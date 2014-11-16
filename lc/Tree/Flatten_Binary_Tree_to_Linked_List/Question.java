a6d125cd2a79ab05525ecc5abe35abe7d8f559ea
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-16 11:20:51 -0800

    update Flatten_Binary_Tree_to_Linked_List

--- lc/Tree/Flatten_Binary_Tree_to_Linked_List/Question.java
+++ lc/Tree/Flatten_Binary_Tree_to_Linked_List/Question.java
@@ -11,6 +11,14 @@ public class Question {
 	}
 	
 	//http://www.cnblogs.com/feiling/p/3278639.html
+	/*
+	 * 看图！
+	 * 思想：整个树分成三份，根，左子树，右子树；
+	 * 递归体     1.根left指向null,
+	 * 		2.根right指向原来左子树
+	 * 		3.找到原左子树的最右孩子p
+	 * 		4.p指向原来右子树
+	 */
     public void flatten(TreeNode root) {
         if(root == null){
         	return;
@@ -18,7 +26,7 @@ public class Question {
         if(root.left != null){
         	TreeNode leftNode = root.left;
         	TreeNode rightNode = root.right;
-        	root.left = null; // cannot be missed!
+        	root.left = null; // 【注】勿忘！
         	root.right = leftNode;
         	TreeNode p = leftNode;
         	while(p.right != null){

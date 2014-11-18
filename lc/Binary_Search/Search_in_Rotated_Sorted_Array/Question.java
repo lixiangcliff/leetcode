f0af81c2a1141612ea472d44bb68753d8c52b264
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-18 08:38:27 -0800

    update Search_in_Rotated_Sorted_Array using BST template

--- lc/Binary_Search/Search_in_Rotated_Sorted_Array/Question.java
+++ lc/Binary_Search/Search_in_Rotated_Sorted_Array/Question.java
@@ -8,36 +8,44 @@ public class Question {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] A = {4,5,6,7,0,1,2};
-		System.out.println(search(A, 9));
+		System.out.println(search(A, 5));
 
 	}
 	
+	//用BST模板
 	//http://blog.csdn.net/linhuanmars/article/details/20525681
 	//看图！（此题元素不重复）
     public static int search(int[] A, int target) {
     	if(A == null || A.length == 0){
     		return -1;
     	}
-    	int l = 0;
-    	int r = A.length-1;
-    	while(l<=r){
-    		int m = (l+r)/2;
-    		if (target == A[m]){//找到target
-    			return m;
+    	int start = 0;
+    	int end = A.length-1;
+    	while(start + 1 < end){
+    		int mid = start+(end - start) / 2;
+    		if (target == A[mid]){//找到target
+    			return mid;
     		}
-    		if(A[m] < A[r]){//说明从m到r这一部分是有序的
-    			if(A[m] <= target && target <= A[r]){ 
-    				l = m+1; //如果target在这一部分，更新左边界l变为m+1
+    		if(A[mid] < A[end]){//说明从mid到end这一部分是有序的
+    			if(A[mid] <= target && target <= A[end]){ 
+    				start = mid; //如果target在这一部分，更新左边界start变为m
     			}else{
-    				r = m-1; //如果target不在这一部分，更新右边界r变为m-1
+    				end = mid; //如果target不在这一部分，更新右边界end变为mid
     			}
     		}else{	//说明从l到m这一部分是有序的
-    			if(A[l] <= target && target <= A[m]){
-    				r = m-1; //如果target在这一部分，更新右边界r变为m-1
+    			if(A[start] <= target && target <= A[mid]){
+    				end = mid; //如果target在这一部分，更新右边界end变为mid
     			}else{
-    				l = m+1; //如果target不在这一部分，更新左边界l变为m+1
+    				start = mid; //如果target不在这一部分，更新左边界start变为mid
     			}
     		}
+    	}
+    	
+    	if (A[start] == target){
+    		return start;
+    	}
+    	if (A[end] == target){
+    		return end;
     	}
     	
         return -1; //没找到

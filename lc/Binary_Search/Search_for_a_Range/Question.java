a061c306151118c6303205d6bc9da63f62a83205
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-17 17:44:20 -0800

    update Search_for_a_Range using BST template

--- lc/Binary_Search/Search_for_a_Range/Question.java
+++ lc/Binary_Search/Search_for_a_Range/Question.java
@@ -7,47 +7,64 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] A = {5, 7, 7, 8, 8, 10};
-		//int[] A = {1,4};
+		//int[] A = {5, 7, 7, 8, 8, 10};
+		int[] A = {2,2};
 		
-		System.out.println((searchRange(A, 8))[0]);
-		System.out.println((searchRange(A, 8))[1]);
+		System.out.println((searchRange(A, 2))[0]);
+		System.out.println((searchRange(A, 2))[1]);
 	}
 
-	//http://blog.csdn.net/linhuanmars/article/details/20593391
-	//看图！
+	//用bst模板【注】，要灵活利用模板！比如下面【注】处的else，体会start和end只能二者选一的逻辑关系
     public static int[] searchRange(int[] A, int target) {
     	int[] result = {-1, -1};
     	if(A == null || A.length == 0){
     		return result;
     	}
-    	int ll = 0;
-    	int lr = A.length -1;
-        while (ll <= lr){
-        	int m = (ll + lr)/2;
-        	if (A[m] < target){
-        		ll = m + 1;
-        	}else{
-        		lr = m - 1;
-        	}
-        }
-        
-    	int rl = 0;
-    	int rr = A.length -1; 
-        while (rl <= rr){
-        	int m = (rl + rr)/2;
-        	if (A[m] > target){
-        		rr = m - 1;
-        	}else{
-        		rl = m + 1;
-        	}
-        }
-        
-        if (ll <= rr){
-        	result[0] = ll;
-        	result[1] = rr;
-        }
-        
+    	    	
+    	int start = 0;
+    	int end = A.length - 1;
+    	//先找target在A中的first
+    	while (start + 1 < end) {//只要不相交或者相邻（或者说一旦相邻或者相交就跳出循环）
+    		int mid = start + (end - start) / 2; //防止 如果 start+end值极大导致溢出的情况
+    		if (A[mid] ==  target){//此题要找first，所以如果遇到相等的，则尽量把窗口往左移，即让end=mid 画图可知
+    			end = mid;
+    		}else if(A[mid] <  target){
+    			start = mid;
+    		}else{
+    			end = mid;
+    		}
+    	}
+    	
+    	if (A[start] == target) {//此题要找first，所以先看start符合否，如果符合就返回了。否则再看end
+    		result[0] = start;
+    	}else if (A[end] == target) { //【注】这个else非常重要！因为如果在start处找到了，我们就不需要也不可以再去end处找（否则会覆盖掉在start处）
+    		result[0] = end;
+    	}else {//说明干脆没找到target，则直接返回内容为{-1, -1}的result
+    		return result;
+    	}
+    	
+    	//恢复start和end初始值
+    	start = 0;
+    	end = A.length - 1;
+    	
+    	//再找target在A中的last
+    	while (start + 1 < end) {
+    		int mid = start + (end - start) / 2; 
+    		if (A[mid] ==  target){
+    			start = mid;
+    		}else if(A[mid] <  target){
+    			start = mid;
+    		}else{
+    			end = mid;
+    		}
+    	}
+    	
+    	if (A[end] == target) {
+    		result[1] = end;
+    	} else if (A[start] == target) {
+    		result[1] = start;
+    	}
+    	
         return result;   
     }
 }

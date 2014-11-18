b3f36e39a008894c5069105817e86d6655b67216
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-17 18:34:26 -0800

    resolve Search_Insert_Position

--- lc/Binary_Search/Search_Insert_Position/Question.java
+++ lc/Binary_Search/Search_Insert_Position/Question.java
@@ -7,33 +7,41 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		int A[] = {1,2};
-		System.out.println(searchInsert(A, 0));
+		System.out.println(searchInsert(A, 3));
 	}
 	
-
-	/*
-	 * 要找比target大的最小元素
-	 * 几个问题要问老师的
-	 */
-	//http://blog.csdn.net/linhuanmars/article/details/20278967
+	//用BST模板，注意下面的【注】的细节
+	//即找target，如果找不到就返回一个比target大(在程序中要写成大于等于，下面会解释)的最小值的index（如果A中的都比target小，则返回A最后一个index+1）
 	public static int searchInsert(int[] A, int target) {  
-	    if(A == null || A.length == 0)  
-	    {  
+	    if(A == null || A.length == 0) {  
 	        return 0;  
-	    }  
-	    int l = 0;  
-	    int r = A.length-1;  
-	    while(l<=r)  //1. 循环条件中，l和r的关系，小于or小于等于
-	    {  
-	        int mid = (l+r+1)/2; //向上还是向下取整？这道题向上向下都行。为什么？  
-	        if(A[mid]==target)  
-	            return mid;  
-	        if(A[mid]<target)  
-	            l = mid+1; //l的边界怎么取？ 
-	        else  
-	            r = mid-1;  ////l的边界怎么取？ 
-	    }  
-	    //"如果没有找到目标元素，那么l一定停在恰好比目标大的index上，r一定停在恰好比目标小的index上" 为什么？
-	    return l;  
+	    }
+	    int start = 0;
+	    int end = A.length - 1;
+	    while (start + 1 < end) {
+	    	int mid = start + (end - start) / 2;
+	    	if (A[mid] == target) {
+	    		return mid;
+	    	}else if (A[mid] < target){
+	    		start = mid;
+	    	}else {
+	    		end = mid;
+	    	}
+	    }
+	    
+	    //BST之后，target的值要么为A[start]和A[end]其中之一，要么在A[start]和A[end]之间；
+	    
+	    //现在找大于或等于target的最小值，之所以要加上等于，是因为 如果A中只有小于等于两个元素，则上面的BST循环会被直接跳过（因为start和end已经直接相邻或相交了）
+	    //我们要在此处处理边界情况，即A的长度小于等于2，并且A[start]==target
+	    //找大于或等于target的最小值,候选的有start和end，显然A[start]<A[end]，所以我们先试A[start]，如果它符合了（A[start]>target）,那么就不用再看A[end]了
+	    if (A[start] >= target) {//【注】，此处不能写成A[start] > target
+	    	return start;
+	    }
+	    //如果A[start]不符，再看A[end]
+	    if (A[end] >= target) {
+	    	return end;
+	    }
+	    //如果都不符合，说明A中没有比target大的值，所以返回A的最后一个index+1，即A的长度
+	    return A.length;  
 	}  
 }

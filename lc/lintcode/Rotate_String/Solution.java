03a4c4faf60ca0c543c2bbb8943ae7ab9acd62c8
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-19 10:37:29 -0800

    update Rotate_String

--- lc/lintcode/Rotate_String/Solution.java
+++ lc/lintcode/Rotate_String/Solution.java
@@ -13,18 +13,17 @@ public class Solution {
 	}
 
 	/*
-	 * 怀疑testcase有问题：
-	 * Input
-		"abccba", 3
-		Output
-		"baabcc"
-		Expected
-		"cbaabc"
+	 * 【注】index vs offset
+	 * index是从前往后数
+	 * offset是从后往前数
 	 */
     String rotateString(String A, int offset) {
         // wirte your code here
-    	String left = reverse(A, 0, offset);
-    	String right = reverse(A, offset + 1, A.length() - 1);
+    	if (A == null || A.length() == 0){
+    		return "";
+    	}
+    	String left = reverse(A, 0, A.length() - offset % A.length() - 1); //【注】此处要取模，因为要防止给的offset比length还大。
+    	String right = reverse(A, A.length() - offset % A.length(), A.length() - 1);
     	String str = left + right;
     	return reverse(str, 0, A.length() - 1);
     }

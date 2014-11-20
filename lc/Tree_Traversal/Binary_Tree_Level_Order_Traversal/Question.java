6817365827a5562c11f9c50af8526c8a1b139220
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-19 21:06:34 -0800

    update Binary_Tree_Level_Order_Traversal using BFS template

--- lc/Tree_Traversal/Binary_Tree_Level_Order_Traversal/Question.java
+++ lc/Tree_Traversal/Binary_Tree_Level_Order_Traversal/Question.java
@@ -1,6 +1,5 @@
 package Binary_Tree_Level_Order_Traversal;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -14,6 +13,7 @@ public class Question {
 
 	}
 	
+	//BFS template
 	//http://blog.csdn.net/linhuanmars/article/details/23404111
 	//类似此题的iterative way：https://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
 	public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
@@ -22,29 +22,21 @@ public class Question {
 			return result;
 		}
 		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
-		int curNum = 1;
-		int nextNum = 0;
 		queue.offer(root);
-		ArrayList<Integer> item = new ArrayList<Integer>();
 		while(!queue.isEmpty()){
-			TreeNode node = queue.poll();
-			item.add(node.val);
-			curNum--;
-			if (node.left != null){
-				queue.offer(node.left);
-				nextNum++;
+			ArrayList<Integer> item = new ArrayList<Integer>();
+			int size = queue.size();
+			for (int i = 0; i < size; i++){
+				TreeNode node = queue.poll();
+				item.add(node.val);
+				if (node.left != null){
+					queue.offer(node.left);
+				}
+				if (node.right != null){
+					queue.offer(node.right);
+				}
 			}
-			if (node.right != null){
-				queue.offer(node.right);
-				nextNum++;
-			}
-			if (curNum == 0){
-				curNum = nextNum;
-				nextNum = 0;
-				result.add(item);
-				//item.clear();【注】，这样写错误！原因问问老师。
-				item = new ArrayList<Integer>();
-			}
+			result.add(item);
 		}
 		return result;
 	}

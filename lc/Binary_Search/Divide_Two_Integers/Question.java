21dd215564145058aa24d78257d6b11a209a6ee0
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-20 14:55:33 -0800

    update Divide_Two_Integers

--- lc/Binary_Search/Divide_Two_Integers/Question.java
+++ lc/Binary_Search/Divide_Two_Integers/Question.java
@@ -7,68 +7,56 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(divide(-999999, 123));
+		System.out.println(divide(999999, 123));
 		//System.out.println(Integer.MIN_VALUE);
 	}
 	
-	//O(n) leetcode TLE...
-/*    public static int divide(int dividend, int divisor) {
-    	if (dividend == 0){
-    		return 0;
-    	}
-    	if (divisor == 0){
-    		return Integer.MAX_VALUE;
-    	}
-        boolean positive = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0);
-        if (!positive){
-        	dividend = -dividend;
-        }
-        int quot = 1;
-        while(Math.abs(dividend-divisor)>Math.abs(divisor)){
-        	dividend -= divisor;
-        	quot++;
-        }
-        return positive ? quot : -quot;
-    }*/
 	
 	//O(logn)
 	//http://blog.csdn.net/linhuanmars/article/details/20024907
 	public static int divide(int dividend, int divisor) {
-    	if (dividend == 0){
+		//边缘情况
+    	if (dividend == 0){ //被除数为0
     		return 0;
     	}
-    	if (divisor == 0){
+    	if (divisor == 0){ //除数为0
     		return Integer.MAX_VALUE;
     	}
 		int result = 0;
+		/*
+		 * 对于int类型最小的整数比最大的整数绝对值大1， 所以如果要取绝对值进行统一处理， 
+		 * 那么就要单独处理最小整数的情况， 下面代码的做法是把它加一个除数的绝对值，让被除数以后可以取绝对值。
+		 */
 		if (dividend == Integer.MIN_VALUE){//process min int value to avoid exceed boundary when "Math.abs()" it 
 			dividend += Math.abs(divisor);
 			result = 1;
 		}
-		if (divisor ==  Integer.MIN_VALUE){
+		if (divisor ==  Integer.MIN_VALUE){//如果dividend == Integer.MIN_VALUE，则返回1，否则其他情况返回0
 			return result;
 		}
-		//to determine whether dividend and divisor share the same positive or negative sign
+		//简便方法判断dividend和divisor是否同号。
 		boolean isNeg = (dividend^divisor)>>>31 == 1; // any difference between >> and >>> here? Yes! see below:
 		//http://docs.oracle.com/javase/tutorial/java/nutsandbolts/op3.html
 		//"The unsigned right shift operator ">>>" shifts a zero into the leftmost position, while the leftmost position after ">>" depends on sign extension."
 		dividend = Math.abs(dividend);
 		divisor = Math.abs(divisor);
 		int digit = 0;
+		//计算商中含有最大的2的指数次幂的指数值为digit
 		while(divisor <= dividend>>1){
 			divisor <<= 1;
 			digit++;
 		}
+		//从被除数中每次减去除数
 		while(digit>=0){
+			//只要当前被除数中含有1个除数，就做如下操作
 			if(dividend-divisor>=0){
-				dividend -= divisor;
-				//result += divisor; wrong !!
-				result += 1<<digit;
+				dividend -= divisor; //被除数中减去当前除数
+				result += 1<<digit; //result累加当前2的digit次幂
 			}
-			digit--;
-			divisor >>= 1;
+			digit--; //2的幂指数减1
+			divisor >>= 1; //除数减半
 		}
-		return isNeg ? -result : result;
+		return isNeg ? -result : result; //恢复符号
 	}
 
 }

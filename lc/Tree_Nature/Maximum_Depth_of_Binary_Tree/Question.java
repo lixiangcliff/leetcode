b377e6ec191898e88be29adf192cfc743c994c83
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-22 00:33:57 -0800

    update Maximum_Depth_of_Binary_Tree using BFS template

--- lc/Tree_Nature/Maximum_Depth_of_Binary_Tree/Question.java
+++ lc/Tree_Nature/Maximum_Depth_of_Binary_Tree/Question.java
@@ -20,34 +20,29 @@ public class Question {
 		}
 		return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
 	}
+	
+	//using BFS template
 	//traverse by level(using bsf)
 	public int maxDepthBSF(TreeNode root) {
 		if (root == null){
 			return 0;
 		}
 		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
-		//curNum和nextNum的作用，是用他们来标记在queue的不同层的位置，每多加一层level，深度就+1
-		int curNum = 1;
-		int nextNum = 0;
 		int level = 0; //根据题意，只有一个root的的Tree高度为0
 		queue.offer(root);
 		//BSF的原理，看图！
 		while(!queue.isEmpty()){
-			TreeNode node = queue.poll();
-			curNum--;
-			if(node.left != null){
-				queue.offer(node.left);
-				nextNum++;
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				if(node.left != null){
+					queue.offer(node.left);
+				}
+				if(node.right != null){
+					queue.offer(node.right);
+				}
 			}
-			if(node.right != null){
-				queue.offer(node.right);
-				nextNum++;
-			}
-			if(curNum == 0){//本层的node已经全部从队列中取出
-				curNum = nextNum;
-				nextNum = 0;
-				level++;
-			}
+			level++;
 		}
 		return level;
 	}

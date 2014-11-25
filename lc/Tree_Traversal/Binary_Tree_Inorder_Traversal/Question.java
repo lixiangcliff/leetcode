b0c408497f212ee6b9b5460bd7739f5fdf5984ea
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-25 15:15:06 -0800

    update Binary_Tree_Inorder_Traversal uisng iterative way from Mo

--- lc/Tree_Traversal/Binary_Tree_Inorder_Traversal/Question.java
+++ lc/Tree_Traversal/Binary_Tree_Inorder_Traversal/Question.java
@@ -14,7 +14,7 @@ public class Question {
 	}
 	//http://blog.csdn.net/linhuanmars/article/details/20187257
 	//recursion
-	public ArrayList<Integer> inorderTraversal(TreeNode root) {
+	public ArrayList<Integer> inorderTraversalRecursive(TreeNode root) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		helper(root,result);
         return result;
@@ -30,7 +30,7 @@ public class Question {
 	
 	//Iterative
 	//this is easier for understanding than linhuan's
-	public ArrayList<Integer> inorderTraversalIterative(TreeNode root) {
+	public ArrayList<Integer> inorderTraversalIterativeIterative(TreeNode root) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
 		if (root==null){
@@ -50,6 +50,46 @@ public class Question {
 			}
 		}
 		return result;
+	}
+	
+	//interative from Mo
+	//之所以和preorder不同（preorder不需要保存保存node是否被访问过），是因为inorder时候，第一次经过某node时候不能当时就处理它，要等待会回来时候才能处理。
+	//所以需要标记它是否被访问过。即我们需要NodeStatusPair这个class
+	//左根右。看图（看步骤！）看【注】
+	public ArrayList<Integer> inorderTraversal(TreeNode root) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		if (root == null) {
+			return result;
+		}
+		LinkedList<NodeStatusPair> stack = new LinkedList<NodeStatusPair>(); //保存node的访问状态 
+		stack.push(new NodeStatusPair(root, false));//赋初值是not ready，因为现在是第一次访问，我们要按[左右根]的顺序，要一直往左找下去。等再次回来时，再把root改为ready
+		while (!stack.isEmpty()) {
+			TreeNode node = stack.peek().node;
+			boolean ready = stack.peek().ready;
+			stack.pop();
+			if (ready) {
+				result.add(node.val);
+			} else {
+				if (node.right != null) {
+					stack.push(new NodeStatusPair(node.right, false));//先压入【右】
+				}
+				stack.push(new NodeStatusPair(node, true)); //再压入【根】
+				if (node.left != null) {
+					stack.push(new NodeStatusPair(node.left, false)); //最后压入【左】 (于是弹出的顺序就是【左根右】)
+				}
+			}
+		}
+		return result;
+	}
+	
+	//【注】重点理解这个ready，意思是访问过了，但是还没有ready（如果还没访问的话，栈中根本就没有这个node对应的NodeStatusPair存在）
+	private class NodeStatusPair {
+		TreeNode node;
+		boolean ready;
+		NodeStatusPair(TreeNode node, boolean ready) {
+			this.node = node;
+			this.ready = ready;
+		}
 	}
 	
 	//Morris Traversal下一轮再说...

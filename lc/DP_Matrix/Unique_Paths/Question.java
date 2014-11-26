14990f6726adaf4a05067513a2331a549859f83b
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-25 22:22:04 -0800

    update Unique_Paths

--- lc/DP_Matrix/Unique_Paths/Question.java
+++ lc/DP_Matrix/Unique_Paths/Question.java
@@ -29,13 +29,13 @@ public class Question {
 			return 0;
 		}
 		int[][] result = new int[m][n];
-		//给第一列赋初值
-		for (int i = 0; i < m; i++) {
-			result[i][0] = 1;
-		}
 		//给第一行赋初值
-		for (int i = 1; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			result[0][i] = 1;
+		}
+		//给第一列赋初值
+		for (int j = 1; j < m; j++) {
+			result[j][0] = 1;
 		}
 		for (int i = 1; i < m; i++) {
 			for (int j = 1; j < n; j++) {

02cfaf0b78ba58f95d5b18d0aca5ce149b22d592
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-25 23:28:14 -0800

    update Minimum_Path_Sum

--- lc/DP_Matrix/Minimum_Path_Sum/Question.java
+++ lc/DP_Matrix/Minimum_Path_Sum/Question.java
@@ -1,6 +1,5 @@
 package Minimum_Path_Sum;
 
-import java.util.Arrays;
 
 public class Question {
 
@@ -11,54 +10,38 @@ public class Question {
 		// TODO Auto-generated method stub
 
 	}
-	
+	/**
+	 * https://oj.leetcode.com/problems/minimum-path-sum/
+	 * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+		Note: You can only move either down or right at any point in time.
+	 */
 	//quite similar to unique path: https://oj.leetcode.com/problems/unique-paths/
-	//http://fisherlei.blogspot.com/2012/12/leetcode-minimum-path-sum.html
-   /* public int minPathSum(int[][] grid) {
-    	if (grid.length == 0){
-    		return 0;
-    	}
-    	int row  = grid.length;
-    	int col = grid[0].length;
-        int min[][] = new int[row][col];
-        min[0][0] = grid[0][0];
-        //initialize left most col
-        for (int i=1; i<row; i++){
-        	min[i][0] = min[i-1][0] + grid[i][0];
-        }
-      //initialize upper most row
-        for (int i=1; i<col; i++){
-        	min[0][i] = min[0][i-1] + grid[0][i];
-        }
-        
-        for (int i=1; i<row;i++ ){
-        	for(int j=1;j<col;j++){
-        		min[i][j] =  Math.min(min[i-1][j], min[i][j-1]) + grid[i][j];
-        	}
-        }
-        return min[row-1][col-1];
-    }*/
-	
-	//row by row to update grid, only maintain the scroll array
-	//http://joycelearning.blogspot.com/2013/10/leetcode-minimum-path-sum.html
+	//http://www.ninechapter.com/solutions/minimum-path-sum/
+	//result[i][j]表示从[0][0]到[i][j],sum最小的路径
+	//通项公式general formula: result[i][j] = min(result[i - 1][j] + result[i][j - 1]) + grid[i][j];
 	public int minPathSum(int[][] grid) {
-       if (grid == null || grid.length == 0 || grid[0].length == 0) {
-            return 0;
-        }
+		if (grid == null || grid.length == 0 || grid[0].length == 0) {
+			return 0;
+		}
 		int row = grid.length;
 		int col = grid[0].length;
-		int scroll[] = new int[col];
-		Arrays.fill(scroll, Integer.MAX_VALUE);
-		//wrong!
-		//scroll[0] = grid[0][0];
-		scroll[0] = 0;
-		for (int i=0; i < row;i++){
-			scroll[0] += grid[i][0];
-			for(int j=1;j<col;j++){
-				scroll[j] = grid[i][j] + Math.min(scroll[j-1], scroll[j]);
+		int[][] result = new int[row][col];
+		result[0][0] = grid[0][0];
+		// 处理第一行
+		for (int j = 1; j < col; j++) {
+			result[0][j] = result[0][j - 1] + grid[0][j];
+		}
+		// 处理第一列
+		for (int i = 1; i < row; i++) {
+			result[i][0] = result[i - 1][0] + grid[i][0];
+		}
+
+		for (int i = 1; i < row; i++) {
+			for (int j = 1; j < col; j++) {
+				result[i][j] = Math.min(result[i][j - 1], result[i - 1][j]) + grid[i][j];
 			}
 		}
-		return scroll[col-1];
+		return result[row - 1][col - 1];
 	}
 
 }

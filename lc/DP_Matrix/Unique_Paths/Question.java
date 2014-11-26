f7450452e32eadf814caa2159f6c36a2a1f2d1eb
lixiangcliff <lixiang.cliff@gmail.com>
2014-11-25 18:09:26 -0800

    update Unique_Paths

--- lc/DP_Matrix/Unique_Paths/Question.java
+++ lc/DP_Matrix/Unique_Paths/Question.java
@@ -8,43 +8,41 @@ public class Question {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(uniquePaths(100, 100)); 
+		//System.out.println(uniquePaths(100, 100)); 
 	}
-	//time out from leetcode...
-   /* public static int uniquePaths(int m, int n) {
-    	int[][] result = new int[m][n];
-        return findUniquePaths(result,m-1,n-1);        
-    }
 
-    private static int findUniquePaths(int[][] result, int m, int n) { 
-    	if (m < 0 || n < 0){
-    		return 0;
-    	}
-    	if ( m ==0 && n ==0){
-    		return 1;
-    	}else{
-    		result[m][n] = findUniquePaths(result, m-1, n) + findUniquePaths(result, m, n-1);
-    		return result[m][n];
-    	}
-    }*/
-	// a good way from http://answer.ninechapter.com/solutions/unique-paths/
-	public static long uniquePaths(int m, int n) {
+	/***
+	 * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+		The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+		How many possible unique paths are there?
+		
+		Above is a 3 x 7 grid. How many possible unique paths are there?
+		
+		Note: m and n will be at most 100.
+	 */
+	// http://answer.ninechapter.com/solutions/unique-paths/
+	//result[i][j]表示从[0][0]到[i][j],一共有多少种不同path
+	//通项公式general formula: result[i][j] = result[i - 1][j] + result[i][j - 1];
+	public int uniquePaths(int m, int n) {
 		if (m == 0 || n == 0) {
-	            return 0;
-	        }
-		long[][] result = new long[m][n];
-    	for (int i=0; i< m; i++){
-    		result[i][0] = 1;
-    	}
-    	for (int i=1; i< n; i++){
-    		result[0][i] = 1;
-    	}
-    	for (int i= 1; i<m; i++){
-    		for (int j=1; j<n;j++){
-    			result[i][j] = result[i-1][j] + result[i][j-1];
-    		}
-    	}
-    	return result[m-1][n-1];
-    }
+			return 0;
+		}
+		int[][] result = new int[m][n];
+		//给第一列赋初值
+		for (int i = 0; i < m; i++) {
+			result[i][0] = 1;
+		}
+		//给第一行赋初值
+		for (int i = 1; i < n; i++) {
+			result[0][i] = 1;
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				result[i][j] = result[i - 1][j] + result[i][j - 1];
+			}
+		}
+		//最右下角的即为所求
+		return result[m - 1][n - 1];
+	}
 
 }

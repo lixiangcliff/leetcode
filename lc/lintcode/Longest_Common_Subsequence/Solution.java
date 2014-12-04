55ac515e64a81cba5b8cf5119ff67a2f411774db
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-03 16:14:11 -0800

    update comment

--- lc/lintcode/Longest_Common_Subsequence/Solution.java
+++ lc/lintcode/Longest_Common_Subsequence/Solution.java
@@ -25,7 +25,7 @@ public class Solution {
 	//			      当A[i] != B[j]， result[i] = max(result[i - 1][j], result[i][j - 1]) （A[i]和B[j]配不上，就让A的前i和B的前j-1配一下，就让A的前i-1和B的前j配一下，看哪个大就选哪个）
 	//3.initialize: result[0][j] = 0;
 	//				result[i][0] = 0;
-	//4.answer: result[A.length - 1][B.length - 1];
+	//4.answer: result[A.length][B.length];
 	//【注】result[][]和A，B有位差
     public int longestCommonSubstring(String A, String B) {
     	if (A == null || B == null || A.length() == 0 || B.length() == 0) {

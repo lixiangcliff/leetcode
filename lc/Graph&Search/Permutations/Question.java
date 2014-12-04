33df15d190bff272a7c3ec5939aba145f7e7142d
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-04 14:12:58 -0800

    update Permutations

--- lc/Graph&Search/Permutations/Question.java
+++ lc/Graph&Search/Permutations/Question.java
@@ -28,134 +28,68 @@ public class Question {
 	}
 	
 	/**
-     * see method 3 and 4
-     * 
+	 * https://oj.leetcode.com/problems/permutations/
+	 * Given a collection of numbers, return all possible permutations.
+	 * 
+	 * For example, 
+	 * [1,2,3] have the following permutations: 
+	 * [1,2,3], [1,3,2],[2,1,3], [2,3,1], [3,1,2], and [3,2,1].
 	 */
-	//idea from http://www.coderli.com/datastructure-c-perm
-/*    public ArrayList<ArrayList<Integer>> permute(int[] num) {
-    	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-    	if (num == null || num.length == 0) {
-            return result; 
-        }
-    	ArrayList<Integer> perm= new ArrayList<Integer>();
-    	doPermute(result, num, 0, num.length-1);
-    	return result;
-    }
-    private void doPermute(ArrayList<ArrayList<Integer>> result, int[] num, int k, int m){
-    	if( k == m){
-    		ArrayList<Integer> perm = new ArrayList<Integer>();
-    		for (int i=0;i<num.length;i++){
-    			perm.add(num[i]);
-    		}
-    		for (int i=0;i<perm.size();i++){
-    			System.out.print(perm.get(i));
-    		}
-    		System.out.println("");
-    		result.add(perm);
-    	}else{
-    		for (int i=k;i<=m;i++){
-    			swap(num, k, i);
-    			doPermute(result, num, k+1, m);
-    			swap(num, k, i);
-    		}
-    	}
-    }
-    private void swap(int[] num, int i, int j){
-    	int temp = num[i];
-    	num[i] = num[j];
-    	num[j] = temp;
-    }*/
-	
-	 //http://www.cnblogs.com/lifegoesonitself/p/3225803.html ------GOOD WAY!
-    /*public static ArrayList<ArrayList<Integer>> permute(int[] num) {
-    	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-    	if (num == null || num.length == 0) {
-            return result; 
-        }
-    	ArrayList<Integer> onePerm = new ArrayList<Integer>();
-    	doPermute(result, num, onePerm, num.length);
-    	return result;
-    }
-	
-    public static void doPermute(ArrayList<ArrayList<Integer>> result, int[] num ,ArrayList<Integer> onePerm ,int len){
-        if(onePerm.size()==len){ 
-        	for (int i=0;i<onePerm.size();i++){
-				System.out.print(onePerm.get(i));
-			}
-			System.out.println("");
-			result.add(onePerm);
-        } else{
-			  for(int i=0;i<num.length;i++){
-			      if(onePerm.indexOf(num[i]) < 0){ 
-			    	  //here is the tricky part:
-			    	  //in doPermute, must give another ArrayList<Integer>, not the original one!
-			    	  ArrayList<Integer> newOnePerm = (ArrayList<Integer>)onePerm.clone();
-			    	  newOnePerm.add(num[i]);
-			    	  doPermute(result, num, newOnePerm,len);
-			      }
-			  } 
-          }
-    }*/
 
 	
-	//递归函数必须保证在进入和离开函数的时候，变量的状态是一样的, 这样才能保证正确性.
-	// recursive function must meet requirement that:
-	// variable must stay the same after entering and before leaving current function body
-	//http://blog.csdn.net/linhuanmars/article/details/21569031
 	//recursive
+	//递归函数必须保证在进入和离开函数的时候，变量的状态是一样的, 这样才能保证正确性.
+	//http://www.ninechapter.com/solutions/permutations/
 	public ArrayList<ArrayList<Integer>> permute(int[] num) {
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		if (num == null || num.length == 0){
 			return result;
 		}
 		ArrayList<Integer> item = new ArrayList<Integer>();
-		boolean[] used = new boolean[num.length];
-		helper(num, used, item, result);
+		helper(result, item, num);
 		return result;
 		
 	}
 	
-	private void helper(int[] num, boolean[] used, ArrayList<Integer> item, ArrayList<ArrayList<Integer>> result){
-		if (item.size() == num.length){// got a permutation
+	private void helper( ArrayList<ArrayList<Integer>> result, ArrayList<Integer> item, int[] num) {
+		if (item.size() == num.length) {// got a permutation
 			result.add(new ArrayList<Integer>(item)); // must "new" a new item, because item is shared among recursion, 
 			return;//everything will add on this item if not "new" a item every single time
 		}
-		for(int i=0;i<num.length;i++){
-			if(!used[i]){//important! to determine whether to process it for permuting
-				// recursive function must meet requirement that:
-				// variable must stay the same after entering and before leaving current function body
-				used[i] = true;
+		for( int i=0;i<num.length;i++) {
+			if (!item.contains(num[i])) {//important! to determine whether to process it for permuting
 				item.add(num[i]);
-				helper(num, used, item, result);
-				item.remove(item.size()-1); // recover to its original statuts
-				used[i] = false; // recover to its original statuts
+				helper(result, item, num);
+				item.remove(item.size() - 1); // recover to its original status
 			}
 		}
 	}
 	
 	//iterative
-/*	public ArrayList<ArrayList<Integer>> permute(int[] num) {
+	//http://blog.csdn.net/linhuanmars/article/details/21569031
+	//思路：设有了当前前i个元素的所有permutation，当第i+1个元素加进来时，我们要做的就是将这个元素带入每一个之前的结果，并且放到每一个结果的各个位置中。
+	public ArrayList<ArrayList<Integer>> permuteIterative(int[] num) {
 		ArrayList<ArrayList<Integer>> result= new ArrayList<ArrayList<Integer>>();
 		if (num== null || num.length == 0){
 			return result;
 		}
-		ArrayList<Integer> first = new ArrayList<Integer>();
-		first.add(num[0]);
-		result.add(first);
-		for(int i=1;i<num.length;i++){
-			ArrayList<ArrayList<Integer>> newResult = new ArrayList<ArrayList<Integer>>();
-			for(int j=0;j<result.size();j++){
-				ArrayList<Integer> cur = result.get(j);
-				for(int k=0;k<cur.size()+1;k++){
-					ArrayList<Integer> item = new ArrayList<Integer>(cur);
+		ArrayList<Integer> firstItem = new ArrayList<Integer>();
+		firstItem.add(num[0]);//先把第一个元素加进item
+		result.add(firstItem);//把这个item加进result
+		for (int i = 1; i < num.length; i++) { //从第二个元素开始，依次把当前元素加入result里的所有item，产生新的item，并存入另一个result，再交换result
+			ArrayList<ArrayList<Integer>> newResult = new ArrayList<ArrayList<Integer>>();//制造一个之后用来交换的result
+			for (int j = 0; j < result.size(); j++) {//从result中取出每个item并处理【注】此处result.size()一直在增长，所以整个算法时间复杂度不是O(n^2)而是指数级的
+				ArrayList<Integer> curItem = result.get(j);
+				for (int k = 0; k < curItem.size() + 1; k++) {//对于当前item依次把当前元素num[i]塞入该item中各个可以插入的位置（包括头尾一共curItem.size() + 1个位子）
+					ArrayList<Integer> item = new ArrayList<Integer>(curItem);
 					item.add(k, num[i]);
-					//result.add(item); wrong!
-					newResult.add(item);
+					newResult.add(item);//把新产生的item放入另一个result。
+										//之所以不放回原来的result是因为我们要用result.size()来标记哪些处理过了而哪些没有。如果再放回同一个result，size就不会减小，便陷入死循环。
 				}
 			}
 			result = newResult;
 		}
 		return result;
-	}*/
+	}
 
 }

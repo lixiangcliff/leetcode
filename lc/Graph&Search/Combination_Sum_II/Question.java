efccbc768b90a0a774af6f84fff682266c27a785
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-05 15:06:15 -0800

    update Combination_Sum_II

--- lc/Graph&Search/Combination_Sum_II/Question.java
+++ lc/Graph&Search/Combination_Sum_II/Question.java
@@ -3,6 +3,7 @@ package Combination_Sum_II;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class Question {
 
 	/**
@@ -10,9 +11,41 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] num = {10,1,2,7,6,1,5};
+		int target = 8;
+		Question q = new Question();
+		
+		ArrayList<ArrayList<Integer>> result = q.combinationSum2(num, target);
+		for (int i = 0; i < result.size(); i++) {
+			ArrayList<Integer> item = result.get(i);
+			for (int j = 0; j < item.size(); j++) {
+				System.out.print(item.get(j)+ ",");
+			}
+			System.out.println("");
+		}
 	}
 	
+	/**
+	 * Given a collection of candidate numbers (C) and a target number (T), find
+	 * all unique combinations in C where the candidate numbers sums to T.
+	 * Each number in C may only be used once in the combination.
+	 * 
+	 * Note: 
+	 * All numbers (including target) will be positive integers. 
+	 * Elements in a combination (a1, a2, … , ak) must be in non-descending order. 
+	 * (ie, a1 ≤ a2 ≤ … ≤ ak). 
+	 * The solution set must not contain duplicate combinations. 
+	 * 
+	 * For example, 
+	 * given candidate set 10,1,2,7,6,1,5 and target 8, 
+	 * A solution set is: 
+	 * [1, 7] 
+	 * [1, 2, 5] 
+	 * [2, 6] 
+	 * [1, 1, 6]
+	 */
+	//看【注】
+	//类似：https://oj.leetcode.com/problems/combination-sum/ 更多解释参考该题
 	//http://blog.csdn.net/linhuanmars/article/details/20829099
     public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
     	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
@@ -21,28 +54,26 @@ public class Question {
     	}
     	Arrays.sort(num);
     	ArrayList<Integer> item = new ArrayList<Integer>();
-    	int start = 0;
-    	helper(num, start, target, item, result);
+    	int pos = 0;
+    	helper(result, item, num, pos, target);
     	return result;
     }
     
-    private void helper(int[] num, int start, int target, ArrayList<Integer> item, ArrayList<ArrayList<Integer>> result){
-    	if(target < 0){ // necessary? 
-    		return;		// answer: yes! if "new target" ---- "target-candidates[i]" < 0, consider it is invalid then return
+    private void helper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> item, int[] num, int pos, int target){
+    	if(target < 0){ 
+    		return;		
     	}
     	if(target == 0){
-    		//result.add(item);
-    		result.add(new ArrayList<Integer>(item)); // still kind of not clear the difference...
+    		result.add(new ArrayList<Integer>(item)); 
     		return;
     	}
-    	for(int i=start; i<num.length; i++){ 
-    	    if(i>start && num[i]==num[i-1]){ //cannot be missed! //kind of not clear...
-    	        continue;     
-    	    }
+    	for (int i = pos; i < num.length; i++) { 
+    		if( i > pos && num[i] == num[i - 1]) { //【注】i > pos而不是i > 0，因为如果当前值和pos之前的值相同的话，还是可以用的（而i > 0则排出了这种情况）
+    			continue;
+    		}
     		item.add(num[i]);
-    		helper(num, i+1, target-num[i], item, result); //"i+1" means can be used only once!
-        	item.remove(item.size()-1);
+    		helper(result, item, num, i + 1, target - num[i]); //【注】元素不可以重复使用，所以此处就要用i + 1（如果元素可以重复使用，则为i）
+        	item.remove(item.size() - 1);
     	}
     }
-
 }

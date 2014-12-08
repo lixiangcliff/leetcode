dc7220ebaf5e837750bda286a97dd3493cb056c0
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-08 15:24:49 -0800

    update comment

--- lc/DP_Sequence/Maximum_Subarray/Question.java
+++ lc/DP_Sequence/Maximum_Subarray/Question.java
@@ -16,6 +16,7 @@ public class Question {
 	}
 	
 	/**
+	 * https://oj.leetcode.com/problems/maximum-subarray/
 	 * Find the contiguous subarray within an array (containing at least one
 	 * number) which has the largest sum.
 	 * 

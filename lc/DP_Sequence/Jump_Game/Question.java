03e7818660a2f757ada36c434799781d00730ca0
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-09 14:08:22 -0800

    update Jump_Game

--- lc/DP_Sequence/Jump_Game/Question.java
+++ lc/DP_Sequence/Jump_Game/Question.java
@@ -7,8 +7,9 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] A = {0,2,3};
-		//System.out.println(canJump(A));
+		int[] A = {3,2,1,0,4};
+		Question q = new Question();
+		System.out.println(q.canJump(A));
 	}
 	
 	/**
@@ -22,50 +23,31 @@ public class Question {
 	 * A = [2,3,1,1,4], return true. 
 	 * A = [3,2,1,0,4], return false.
 	 */
+	//DP 1Seq
 	//http://www.ninechapter.com/solutions/jump-game/
 	//1.state: result[i]表示能否最终跳到位置i
-	//2.function: 则result[i] = 能够从0跳到i || 能够从1跳到i ||...|| 能够从i-1跳到i
+	//2.function: 则result[i] = 能够从第1个位置跳到第i个位置 || 能够从2跳到i ||...|| 能够从i-1跳到i
 	//能从j跳到i的条件是：result[j] && A[j] + j >= i
-	//通项公式general formula: result[i] = OR(result[j] && j < i && A[j] + j >= i) （j取值从0到i-1） ;
-	//3.initialize: result[0] = true
-	//4.answer: result[A.length - 1]
+	//通项公式general formula: result[i] = OR(result[j] && j < i && A[j] + j >= i) （j取值从1到i-1） ;
+	//3.initialize: result[1] = true
+	//4.answer: result[A.length]
+	//result和A有1个位差
 	//O(n^2)时间复杂度，不是最优解，但是容易想。
 	public boolean canJump(int[] A) {
 		if (A == null || A.length == 0) {
 			return false;
 		}
-		boolean[] result = new boolean[A.length];
-		result[0] = true; // 赋初值！【重要】
-		for (int i = 1; i < A.length; i++) {
-			for (int j = 0; j < i; j++) {
-				if (result[j] && A[j] + j >= i) { // 找到任何一个j能跳到i，则可以直接把result[i]置true，并跳出循环。
+		boolean[] result = new boolean[A.length + 1];
+		result[1] = true; // 赋初值！【重要】
+		for (int i = 2; i <= A.length; i++) {
+			for (int j = 1; j < i; j++) {
+				if (result[j] && A[j - 1] + j >= i) { // 找到任何一个j能跳到i，则可以直接把result[i]置true，并跳出循环。【注】位差
 					result[i] = true;
 					break;
 				}
 			}
 		}
-		return result[A.length - 1];
+		return result[A.length];
 	}
 	
-	//DP 1D
-	//O(n) time complex
-	//http://blog.csdn.net/linhuanmars/article/details/21354751
-	public boolean canJump_BigO_n(int[] A) {
-		if (A == null || A.length==0){
-			return false;
-		}
-		int global = 0;
-		for (int i = 0; i< A.length-1;i++) { 
-			if (global < i) { //means current global cannot reach current i;
-				return false;
-			}
-			int local = A[i] + i;//at current i, local is the longest index it can reach
-			global = Math.max(local, global); //"global" is longest index it can ever reach for each current i;
-			if (global >= A.length-1) { // already find one index that can jump to the end directly
-				return true;
-			}
-		}
-		return (global >= A.length-1);
-	}
-
 }

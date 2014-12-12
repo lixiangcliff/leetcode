dac06d14833115ad4d6da2d5e244971f61476586
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-12 11:08:46 -0800

    update Best_Time_to_Buy_and_Sell_Stock

--- lc/DP_1_Sequence/Best_Time_to_Buy_and_Sell_Stock/Question.java
+++ lc/DP_1_Sequence/Best_Time_to_Buy_and_Sell_Stock/Question.java
@@ -8,63 +8,39 @@ public class Question {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] prices = {1, 6, 3,20, 4, 10};
-		System.out.println(maxProfit(prices));
+		Question q = new Question();
+		System.out.println(q.maxProfit(prices));
 
 	}
 	
-	/*public static int maxProfit(int[] prices) {
-		if (prices.length <=1){
-			return 0;
-		}
-		int max = 0;
-		for (int i=1; i<prices.length;i++){
-			int min=findMin(prices,i);
-			if (prices[i] - min > max){
-				max = prices[i] - min;
-			}
-		}		
-        return max;
-    }
-	
-	public static int findMin(int[] prices, int pos){
-		if (prices.length <1){
-			return 0;
-		}
-		int min=prices[0];
-		for (int i=0;i<pos;i++){
-			if (prices[i] < min){
-				min=prices[i];
-			}
-		}
-		return min;
-	}
-*/
-/*	public static int maxProfit(int[] prices) {
-	    if (prices.length <=1){
+	/**
+	 * https://oj.leetcode.com/problems/best-time-to-buy-and-sell-stock/
+	 * Say you have an array for which the ith element is the price of a given
+	 * stock on day i. If you were only permitted to complete at most one
+	 * transaction (ie, buy one and sell one share of the stock), design an
+	 * algorithm to find the maximum profit.
+	 */
+	//DP 1Seq
+	//1.state: result[i]代表直到前i天，在第i天卖出股票，所获得的最大profit值。
+	//		   minPrice直到i最小的price值	
+	//2.function: result[i] = prices[i] - minPrice
+	//3.initialize: result[1] = 0;（即第1天就卖，相当于当天买卖，profit为0）
+	//4.answer: max(result[0], result[1]...result[nums.length];
+	//【注】result和prices有位差
+	//http://blog.csdn.net/linhuanmars/article/details/23162793
+	public int maxProfit(int[] prices) {  
+	    if (prices == null || prices.length==0) {  
 	        return 0;
 	    }
-	    int maxProfit = 0;
+	    int[] result = new int[prices.length + 1];
+	    result[1] = 0;
+	    int max = 0;
 	    int minPrice = prices[0];
-	    for(int i=1;i<prices.length;i++){
-	        maxProfit = Math.max(maxProfit, prices[i]-minPrice);
-	        minPrice = Math.min(minPrice, prices[i]);
+	    for (int i = 2; i <= prices.length; i++) {
+	    	minPrice = Math.min(minPrice, prices[i - 1]); //位差
+	    	result[i] = prices[i - 1] - minPrice;
+	    	max = Math.max(max, result[i]);
 	    }
-	    return maxProfit;
-	}*/
-	
-	//http://blog.csdn.net/linhuanmars/article/details/23162793
-	public static int maxProfit(int[] prices) {  
-	    if(prices==null || prices.length==0)  
-	        return 0;  
-	    int local = 0;  
-	    int global = 0;  
-	    for(int i=0;i<prices.length-1;i++)  
-	    {   
-	        //local means: if sell at current i, what is the biggest profit it can make
-	    	local = Math.max(local+prices[i+1]-prices[i],0); 
-	    	//global means: until current i, what is the biggest profit ever made
-	        global = Math.max(local, global);  
-	    }  
-	    return global;  
+	    return max;
 	}
 }

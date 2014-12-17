8b7e4812241114573f5a8cdcca5688ef3a13bec2
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-17 00:24:20 -0800

    update comment in Combination_Sum

--- lc/Graph&Search/Combination_Sum/Question.java
+++ lc/Graph&Search/Combination_Sum/Question.java
@@ -45,13 +45,15 @@ public class Question {
 	 * [7] 
 	 * [2, 2, 3]
 	 */
+	
+	//【注】类似subsets为了避免相同元素导致的结果集重复，需要pos这个参数来标记，每次递归的时的，起始处理元素的位置。
 	//http://blog.csdn.net/linhuanmars/article/details/20828631
     public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
     	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
     	if (candidates == null || candidates.length == 0){
     		return result;
     	}
-    	Arrays.sort(candidates);
+    	Arrays.sort(candidates); // 这类题基本都需要先sort。对于此题sort之后，递归时如果当前元素已经比target大了，则之后的元素就不用再看了，直接返回即可。
     	ArrayList<Integer> item = new ArrayList<Integer>();
     	int pos = 0;
     	helper(result, item, candidates, pos, target);
@@ -62,17 +64,18 @@ public class Question {
     	if(target < 0){ // 因为所有元素都为正，如果target为负，则不可能找到数字组合来达到target，故返回
     		return;		
     	}
-    	if(target == 0){
+    	if(target == 0){ // 找到了和为target的一个组合
     		result.add(new ArrayList<Integer>(item)); 
     		return;
     	}
     	for (int i = pos; i < candidates.length; i++) { //i从pos开始处理，因为pos之前的我们已经处理完了
-    		if( i > pos && candidates[i] == candidates[i - 1]) {//这样做避免了因为重复元素而导致的重复结果
+    		//这样做避免了因为重复元素而导致的重复结果（如果遇到相同元素则跳过，直到遇到不同的元素，再进行下一层递归。此处类似subsets II）
+    		if( i > pos && candidates[i] == candidates[i - 1]) {
     			continue;
     		}
     		item.add(candidates[i]);//把当前值加入item中
-    		helper(result, item, candidates, i, target - candidates[i]); //target减去当前值。进一步递归。递归时仍为i，是因为元素可以重复使用，（如果不可以重复使用，此处就要用i + 1）
-        	item.remove(item.size() - 1); //恢复现场
+    		helper(result, item, candidates, i, target - candidates[i]); //【注】target减去当前值。进一步递归。递归时pos仍取i，这是因为元素可以多次使用，（如果不可以重复使用，此处就要用i + 1）
+        	item.remove(item.size() - 1); //回溯
     	}
     }
 }

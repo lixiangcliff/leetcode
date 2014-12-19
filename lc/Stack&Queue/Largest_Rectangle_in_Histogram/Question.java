eeea677e7e0f841166443d3909276ae76a54ea50
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-19 12:08:07 -0800

    adding picture link

--- lc/Stack&Queue/Largest_Rectangle_in_Histogram/Question.java
+++ lc/Stack&Queue/Largest_Rectangle_in_Histogram/Question.java
@@ -19,9 +19,11 @@ public class Question {
 	 * where the width of each bar is 1, find the area of largest rectangle in
 	 * the histogram.
 	 * 
+	 * http://leetcode.com/wp-content/uploads/2012/04/histogram.png
 	 * Above is a histogram where width of each bar is 1, given height =
 	 * [2,1,5,6,2,3].
 	 * 
+	 * http://leetcode.com/wp-content/uploads/2012/04/histogram_area.png
 	 * The largest rectangle is shown in the shaded area, which has area = 10
 	 * unit.
 	 * 

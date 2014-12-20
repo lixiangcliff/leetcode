55c3a520b9153161da2128a33f4acfa515ee7b95
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-20 10:58:59 -0800

    update Generate_Parentheses

--- lc/Graph&Search/Generate_Parentheses/Question.java
+++ lc/Graph&Search/Generate_Parentheses/Question.java
@@ -9,25 +9,43 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//System.out.println("Heelow!");
+		Question q = new Question();
+		ArrayList<String> result = q.generateParenthesis(3);
+		for (int i = 0; i < result.size(); i++) {
+			System.out.println(result.get(i));
+		}
 	}
+	
+	/**
+	 * https://oj.leetcode.com/problems/generate-parentheses/
+	 * Given n pairs of parentheses, write a function to generate all
+	 * combinations of well-formed parentheses.
+	 * 
+	 * For example, given n = 3, a solution set is:
+	 * "((()))", "(()())", "(())()", "()(())", "()()()"
+	 */
+	
+	//http://www.ninechapter.com/solutions/generate-parentheses/
+	//http://blog.csdn.net/linhuanmars/article/details/19873463
     public ArrayList<String> generateParenthesis(int n) {
     	ArrayList<String> result =  new ArrayList<String>();
-    	addParen(result, "", n, n);
+    	String item = "";
+    	helper(result, item, n, n);
     	return result;
     }
     
-    private void addParen(ArrayList<String> result, String str, int left, int right){ // left means how many "(" are still unused
-    	if (left < 0 || left > right){
+    // item为到目前为止已经组成的部分合法结；left和right分别为还剩多少未使用的左括号和右括号
+    private void helper(ArrayList<String> result, String item, int left, int right){ // left means how many "(" are still unused
+    	if (left < 0 || right < 0|| left > right) { // 左或右括号剩余量为负；或者如果左括号剩下的比右括号剩下的多，即已经使用的右括号比左括号多，则非法，则返回。
     		return;
     	}
-    	if (left == 0 && right == 0){ // all parens have been used. then finished one possible combination    		
-    		result.add(str);
+    	if (left == 0 && right == 0){ // 所有左右括号都使用完了，返回一个	完整的合法解
+    		result.add(item);
     	}
-    	// use left
-    	addParen(result, str+"(", left-1, right);
-    	// use right
-    	addParen(result, str+")", left, right-1);
+		// 把一个左括号加入到item的尾部，然后递归处理
+		helper(result, item + "(", left - 1, right);
+		// 把一个右括号加入到item的尾部，然后递归处理t
+		helper(result, item + ")", left, right -1);
     }
 
 }

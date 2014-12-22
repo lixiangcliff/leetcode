358c6488cd3ba4c275eb6ac8ae3907e16019b6f8
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-21 23:06:01 -0800

    update Word_Break_II

--- lc/Graph&Search/Word_Break_II/Question.java
+++ lc/Graph&Search/Word_Break_II/Question.java
@@ -1,6 +1,7 @@
 package Word_Break_II;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Question {
@@ -10,91 +11,71 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Question q = new Question();
+		String s = "catsanddog";
+		Set<String> dict = new HashSet<String>();
+		String[] dictStrs = {"cat", "cats", "and", "sand", "dog"}; 
+		for (String str : dictStrs) {
+			dict.add(str);
+		}
+		ArrayList<String> result = q.wordBreak(s, dict);
+		for (String item : result) {
+			System.out.println(item);
+		}
+	}
+	
 
-	}
-	
-	//http://www.binglu.me/leetcode-word-break-and-word-break-ii/
-/*	ArrayList<String> wordBreak(String s, Set<String> dict){
-		ArrayList<ArrayList<Integer>> record = new ArrayList<ArrayList<Integer>>();
-		if (s == null){
-			return null;
-		}
-		int len = s.length();
-		for(int i=0;i<len;i++){
-			record.add(new ArrayList<Integer>());
-		}
-		
-		// the reason i starts from len instead of len-1 is because
-		// stored index is the actual ending index plus 1 
-		for(int end=len; end>=0;end--){ 
-			//if record.get(i).isEmpty(), we will skip this i;
-			//only exception is when i==len
-			if(end<len && record.get(end).isEmpty()){
-				continue;
-			}
-			for(int runner=end-1;runner>=0;runner--){
-				if(dict.contains(s.substring(runner, end))){
-					record.get(runner).add(end); // use "end" and "runner" to avoid confusion
-				}
-			}
-		}
-		
-		ArrayList<String> solutions = new ArrayList<String>();
-		buildSolution(s, "", 0, record, solutions);
-		return solutions;
-	}
-	
-	private void buildSolution(String s, String oneSolution, int current, ArrayList<ArrayList<Integer>> record, ArrayList<String> solutions){
-		//iterate on current character's word ending list
-		for(int end: record.get(current)){
-			String sub = s.substring(current, end);
-			String newOneSolution = oneSolution + (current == 0 ? sub : " "+sub); 
-			//if(end == s.length()-1) //wrong!!
-			if(end == s.length()){ // got one solution
-				//solutions.add(oneSolution); //wrong !!
-				solutions.add(newOneSolution);
-			}else{
-				//buildSolution(s, newOneSolution, current+1, record, solutions); //wrong!!
-				buildSolution(s, newOneSolution, end, record, solutions);
-			}
-		}
-	}*/
-	
-	//brutal force
+	/**
+	 * https://oj.leetcode.com/problems/word-break-ii/
+	 * Given a string s and a dictionary of words dict, add spaces in s to
+	 * construct a sentence where each word is a valid dictionary word.
+	 * Return all such possible sentences.
+	 * 
+	 * For example, given 
+	 * s = "catsanddog", 
+	 * dict = ["cat", "cats", "and", "sand", "dog"].
+	 * A solution is ["cats and dog", "cat sand dog"].
+	 */
+	//DFS
 	//http://blog.csdn.net/linhuanmars/article/details/22452163
-	ArrayList<String> wordBreak(String s, Set<String> dict){
+	ArrayList<String> wordBreak(String s, Set<String> dict){ // 【注1】必须用Set而不是HashSet来通过leetcode
 		ArrayList<String> result = new ArrayList<String>();
-		if(s==null || s.length() == 0 || !isWBable(s, dict)){
+		if (s == null || s.length() == 0 || !isWordBreakable(s, dict)) { // 如果s不能wordbreak则直接返回result，避免输入字符串过长，导致回溯超时。
 			return result;
 		}
-		helper(s, 0, dict, "", result);
+		String item = "";
+		helper(result, item, s, dict, 0);
 		return result;
 	}
 	
-	private void helper(String s, int start, Set<String> dict, String item, ArrayList<String> result){
-		//if (start == s.length()-1){ wrong! when start == s.length() means finished traversing whole s
-		if (start == s.length()){
+	private void helper(ArrayList<String> result, String item, String s, Set<String> dict, int start){
+		if (start == s.length()) {
 			result.add(item);
 			return;
 		}
 		StringBuilder sb = new StringBuilder();
-		for(int i=start; i<s.length();i++){
-			sb.append(s.charAt(i)); //detect by adding one char each time to see whether current sb.toString is in dict; if yes, recurse. 
-			if (dict.contains(sb.toString())){
-				String newItem = (item.length()>0) ? (item + " " + sb.toString()) : sb.toString();  
-				helper(s, i+1, dict, newItem, result);
+		for (int i = start; i < s.length(); i++) {
+			sb.append(s.charAt(i)); // 每次增加一个s中的字符加到sb中，看sb是否为dict中的一个单词
+			if (dict.contains(sb.toString())) { // 如果是sb是否dict中的一个单词，就在后面进行递归 
+				String newItem = "";
+				if (item.length() == 0) {
+					newItem = sb.toString();
+				} else {
+					newItem = item + " " + sb.toString();
+				}
+				helper(result, newItem, s, dict, i + 1); // 用item置为newItem，start置为i + 1，继续递归
 			}
 		}
 	}
 	
-	//reuse function from below:
-	//https://oj.leetcode.com/problems/word-break/	
-	private boolean isWBable(String s, Set<String> dict) {  
+	//【注2】加上这个函数纯粹只是为了pass leetcode上下面这个test case
+	//"Last executed input:	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", 
+	//["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]"
+	//函数重用: https://oj.leetcode.com/problems/word-break/	
+	private boolean isWordBreakable(String s, Set<String> dict) {  
 	    if(s==null || s.length()==0)  
 	        return true;  
 	    boolean[] result = new boolean[s.length()+1];  
-	    //result[i] means until index i, whether s can still be segmented by words in dict
-	    //result[0] means when s is empty, apparently it can be. 
 	    result[0] = true;  
 	    for(int i=0;i<s.length();i++)  
 	    {  
@@ -109,6 +90,6 @@ public class Question {
 	        }  
 	    }  
 	    return result[s.length()];  
-	} 
-
+	}
+	
 }

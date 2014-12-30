2b2f89c6217a15e0876b2511b6dab5aed25e5425
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-29 17:42:01 -0800

    regroup Longest_Palindromic_Substring

--- lc/DP_1_Sequence/Longest_Palindromic_Substring/Question.java
+++ lc/DP_1_Sequence/Longest_Palindromic_Substring/Question.java
@@ -12,6 +12,12 @@ public class Question {
 		System.out.println(longestPalindrome(s));
 	}
 	
+	/**
+	 * https://oj.leetcode.com/problems/longest-palindromic-substring/
+	 * Given a string S, find the longest palindromic substring in S. You may
+	 * assume that the maximum length of S is 1000, and there exists one unique
+	 * longest palindromic substring.
+	 */
 	//my way
 /*   public static String longestPalindrome(String s) {
         if(s == null){
@@ -57,24 +63,25 @@ public class Question {
 	//http://blog.csdn.net/linhuanmars/article/details/20888595
 	//method 2 using DP
 	 public static String longestPalindrome(String s) {
-		 if( s== null || s.length() == 0){
-			 return "";
-		 }
-		 String result = "";
-		 int maxLen = 0;
-		 int len = s.length();
-		 boolean[][] isPalindromeEnd2End = new boolean[len][len];
-		 for(int i=len-1;i>=0;i--){
-			 for(int j=i;j<len;j++){
-				 if(s.charAt(i) == s.charAt(j) && (j-i<=2 || isPalindromeEnd2End[i+1][j-1])){
-					 isPalindromeEnd2End[i][j] = true;
-					 if(j-i+1 > maxLen){
-						 maxLen = j-i+1;
-						 result = s.substring(i,j+1);
-					 }
-				 }
-			 }
-		 }
-		 return result;
+		if (s == null || s.length() == 0) {
+			return "";
+		}
+		String result = "";
+		int maxLen = 0;
+		int len = s.length();
+		boolean[][] isPalindromeEnd2End = new boolean[len][len];
+		for (int i = len - 1; i >= 0; i--) {
+			for (int j = i; j < len; j++) {
+				if (s.charAt(i) == s.charAt(j)
+						&& (j - i <= 2 || isPalindromeEnd2End[i + 1][j - 1])) {
+					isPalindromeEnd2End[i][j] = true;
+					if (j - i + 1 > maxLen) {
+						maxLen = j - i + 1;
+						result = s.substring(i, j + 1);
+					}
+				}
+			}
+		}
+		return result;
 	 }
 }

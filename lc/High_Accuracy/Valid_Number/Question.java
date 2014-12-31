1ba79e2444e209a6bae453e95738d23f753f0042
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-30 23:47:20 -0800

    regroup Valid_Number

--- lc/High_Accuracy/Valid_Number/Question.java
+++ lc/High_Accuracy/Valid_Number/Question.java
@@ -10,6 +10,16 @@ public class Question {
 
 	}
 	
+	/**
+	 * https://oj.leetcode.com/problems/valid-number/
+	 * Validate if a given string is numeric.
+	 * 
+	 * Some examples: "0" => true " 0.1 " => true "abc" => false "1 a" => false
+	 * "2e10" => true Note: It is intended for the problem statement to be
+	 * ambiguous. You should gather all requirements up front before
+	 * implementing one.
+	 */
+	
 	//http://blog.csdn.net/linhuanmars/article/details/23809661
     public boolean isNumber(String s) {
         if(s == null || s.length() ==0){

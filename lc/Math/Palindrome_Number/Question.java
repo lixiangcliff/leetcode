5f795d3edbc6fb4f7695c815dc4b9f3b68c00d9a
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-30 18:14:45 -0800

    update Palindrome_Number

--- lc/Math/Palindrome_Number/Question.java
+++ lc/Math/Palindrome_Number/Question.java
@@ -7,84 +7,47 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Question q = new Question();
 		int a = 12033021;
 		//int a = -1881;
-		System.out.print(isPalindrome(a));
+		System.out.print(q.isPalindrome(a));
 		//System.out.print((a));
 
 	}
 	
-	//no good when inout is 1000021..
-    /*public static boolean isPalindrome(int x) {   
-    	if (x < 0){
-    		return false;
-    	}
-        while(x>0){
-        	int len = getLength(x);
-        	int high = (int) (x / Math.pow(10, len-1));
-        	int low = x%10;
-        	System.out.println("x1:  "+ x);
-        	System.out.println("high:" + high);
-        	System.out.println("low :" + low);
-        	if(high != low){
-        		return false;
-        	}       	
-        	x = (int) (x - high*Math.pow(10, len-1))/10;
-        }
-        return true;
-    }
+	/**
+	 * https://oj.leetcode.com/problems/palindrome-number/
+	 * Determine whether an integer is a palindrome. Do this without extra
+	 * space.
+	 * 
+	 * click to show spoilers.
+	 * Some hints: Could negative integers be palindromes? (ie, -1)
+	 * If you are thinking of converting the integer to string, note the
+	 * restriction of using extra space.
+	 * You could also try reversing an integer. However, if you have solved the
+	 * problem "Reverse Integer", you know that the reversed integer might
+	 * overflow. How would you handle such case?
+	 * There is a more generic way of solving this problem.
+	 */
     
-    public static int getLength(int a){
-    	int count =0;
-    	while(a>0){
-	    	a = a/10;
-	    	count++;
-    	}
-    	return count;
-    }*/
-    
-	//use exra space
-/*    public static boolean isPalindrome(int x) {
-    	if (x<0){
-    		return false;
-    	}
-    	String str = Integer.toString(x);
-    	for(int i=0;i<str.length();i++){
-    		if(str.charAt(i) != str.charAt(str.length()-1 -i)){
-    			return false;
-    		}
-    	}
-    	return true;
-    }*/
-    
-    public static boolean isPalindrome(int x) {   
+	//http://blog.csdn.net/linhuanmars/article/details/21145231
+	//思想：每次取第一位和最后一位，如果不相同则返回false，否则继续直到位数为0
+    public boolean isPalindrome(int x) {   
 		if (x < 0){
 			return false;
 		}
-		int len = getLength(x);
-		for (int i =0; i<len-1;i++){
-			if(getDigit(x, i) != getDigit(x, len-1-i)){
+		int div = 1; // 表示比跟x位数相同的10的正整数次幂（目的是为了x / div时可以得到x的最高位数值）
+		while (div <= x / 10) { //【注】，此处一定要用除法，以防越界。
+			div *= 10;
+		}
+		while (x > 0) {
+			if (x / div != x % 10) {
 				return false;
 			}
+			x = ( x % div ) / 10; // 【注】技巧: “x % div” 去掉了x的首位，再 “/10”又去掉了末位
+			div /= 100; // x被减少了2位，所以div也要相应减少2位
 		}
-		    return true;
-	}
-	
-	public static int getLength(int a){
-		int count =0;
-		while(a>0){
-	    	a = a/10;
-	    	count++;
-		}
-		return count;
-	}
-	
-	public static int getDigit(int a, int b){
-		while(b>0){
-	    	a = a/10;
-	    	b--;
-		}
-		return a % 10;
+		return true;
 	}
 
 }

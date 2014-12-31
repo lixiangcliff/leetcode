f2c4070f9231115c3dce1db55c17e64b39c59835
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-31 12:12:44 -0800

    update Valid_Palindrome

--- lc/Two_Pointers/Valid_Palindrome/Question.java
+++ lc/Two_Pointers/Valid_Palindrome/Question.java
@@ -7,41 +7,53 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Question q = new Question();
 //		String s = "A man, a plan, a canal: Panama";
 //		String s = "race a car";
-		String s = "aa2";
-		//System.out.println(isPalindrome(s));
-		System.out.println('1'-'9');
+		String s = ".,";
+		System.out.println(q.isPalindrome(s));
 	}
 	
-    public static boolean isPalindrome(String s) {
-        if(s==null){
-        	return false;
-        }
+	/**
+	 * https://oj.leetcode.com/problems/valid-palindrome/
+	 * Given a string, determine if it is a palindrome, considering only
+	 * alphanumeric characters and ignoring cases.
+	 * 
+	 * For example, "A man, a plan, a canal: Panama" is a palindrome.
+	 * "race a car" is not a palindrome.
+	 * 
+	 * Note: Have you consider that the string might be empty? This is a good
+	 * question to ask during an interview.
+	 * 
+	 * For the purpose of this problem, we define empty string as valid
+	 * palindrome.
+	 */
+	
+	//左右两个指针往中间移动
+	//http://www.cnblogs.com/yuzhangcmu/p/4194369.html
+    public boolean isPalindrome(String s) {
+		if (s == null) {
+			return false;
+		}
         s = s.trim();
-        if(s.length() <= 1){
+        if (s.length() <= 1) {
         	return true;
         }
         int l = 0;
         int r = s.length()-1;
-        while(l<r){
-        	//considering only alphanumeric characters
-        	while(!(s.charAt(l)>='a' && s.charAt(l)<='z') && !(s.charAt(l)>='A' && s.charAt(l)<='Z') && !(s.charAt(l)>='0' && s.charAt(l)<='9') && l<s.length()-1){
-        		l++;
-        	}
-        	while(!(s.charAt(r)>='a' && s.charAt(r)<='z') && !(s.charAt(r)>='A' && s.charAt(r)<='Z') && !(s.charAt(r)>='0' && s.charAt(r)<='9') && r>0){
-        		r--;
-        	}
-        	if(l<r){
-        		//'A' is considered to be same as 'a' for Palindrome checking
-        		if(Character.toUpperCase(s.charAt(l)) != Character.toUpperCase(s.charAt(r))){
-        			return false;
-        		}
-        		l++;
-        		r--;
-        	}
+        s = s.toLowerCase(); // 都转为小写
+        while (l < r) { // 每次只做下面四种操作中的一种，然后就再次重新判断l和r的关系，有效避免了越界
+			if (!Character.isLetterOrDigit(s.charAt(l))) {
+				l++;
+			} else if (!Character.isLetterOrDigit(s.charAt(r))) {
+				r--;
+			} else if (s.charAt(l) != s.charAt(r)) {
+				return false;
+			} else {
+				l++;
+				r--;
+			}
         }
         return true;
     }
-
 }

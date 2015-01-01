28f3d9495669b76f095260e0b07455963260aff2
lixiangcliff <lixiang.cliff@gmail.com>
2014-12-31 17:57:28 -0800

    update Recover_Binary_Search_Tree

--- lc/Tree_Nature/Recover_Binary_Search_Tree/Question.java
+++ lc/Tree_Nature/Recover_Binary_Search_Tree/Question.java
@@ -8,6 +8,7 @@ public class Question {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Question q = new Question();
 		// TODO Auto-generated method stub
 		TreeNode t1 = new TreeNode(1);
 		TreeNode t2 = new TreeNode(2);
@@ -22,54 +23,63 @@ public class Question {
 		t2.right = t3;
 		t6.left = t1;
 		t6.right = t7;
-		inorder(t4);
+		q.inorder(t4);
 		System.out.println("");
-		recoverTree(t4);
-		inorder(t4);
+		q.recoverTree(t4);
+		q.inorder(t4);
 		System.out.println("");
 		
 
 	}
 	
+	/**
+	 * https://oj.leetcode.com/problems/recover-binary-search-tree/
+	 * Two elements of a binary search tree (BST) are swapped by mistake.
+	 * Recover the tree without changing its structure.
+	 * 
+	 * Note: A solution using O(n) space is pretty straight forward. Could you
+	 * devise a constant space solution?
+	 */
+	//思想：BST中两个元素交换位置后，再做中旬遍历时会产生两种情况:
+	//1.如果是相邻的两个元素被调换了，只会出现一次逆序情况，只需要把这个两个节点记录下来最后调换值就可以；
+	//2.如果是不相邻的两个元素被调换了,会发生两次逆序的情况，那么这时候需要调换的元素是第一次逆序前面的元素，和第二次逆序后面的元素。
+	//情况2举例: 如1234567,1和5调换了，会得到5234167，逆序发生在52和41，我们需要把4和1调过来，那么就是52的第一个元素，41的第二个元素调换即可。
 	//http://blog.csdn.net/linhuanmars/article/details/24566995
-	public static void recoverTree(TreeNode root) {
+	public void recoverTree(TreeNode root) {
 		if (root == null){
 			return;
 		}
-		ArrayList<TreeNode> result = new ArrayList<TreeNode>();
-		ArrayList<TreeNode> pre = new ArrayList<TreeNode>();
-		pre.add(null);
+		ArrayList<TreeNode> result = new ArrayList<TreeNode>(); // 里面存有待恢复的两个node
+		ArrayList<TreeNode> pre = new ArrayList<TreeNode>(); // 标记以inorder遍历时当前node之前的那个node（作为一个指针在递归不同层之间传递）
+		pre.add(null); // pre初始内容为null
 		helper(root, pre, result);
-		if(result.size()>0){
+		if (result.size() > 0) { //只要找到有逆序存在
 			int temp = result.get(0).val;
 			result.get(0).val = result.get(1).val;
 			result.get(1).val = temp;
 		}
 	}
-	private static void helper(TreeNode root, ArrayList<TreeNode> pre, ArrayList<TreeNode> result){
-		if(root == null){
+	
+	//DFS -- inorder
+	private void helper(TreeNode root, ArrayList<TreeNode> pre, ArrayList<TreeNode> result) {
+		if (root == null) {
 			return;
 		}
-		helper(root.left, pre, result);
-		if(pre.get(0)!= null && pre.get(0).val > root.val){//find current TreeNode(root) is bigger than pre.
-			if(result.size() == 0){	//first time find a descended node 
-				//result.set(0, pre.get(0)); Wrong!
-				//result.set(1, root);
-				//http://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html
-				//set(int index, E element)
-				//Replaces the element at the specified position in this list with the specified element.
-				//i.e. you must add something, then set(to replace)
-				result.add(0, pre.get(0)); 
-				result.add(1, root);
-			}else{
-				result.set(1, root); // update second node in result if finding another descended node 
+		helper(root.left, pre, result); // 递归左子树
+		if (pre.get(0) != null &&  root.val < pre.get(0).val) { // 找到了一个逆序的node
+			if (result.size() == 0) { // 通过result.size()的大小，知道这是第一个逆序的node 
+				result.add(pre.get(0)); // result[0]装的是第一次逆序前面的元素
+				result.add(root); // result[1]装的是第一次逆序后面的元素
+			} else { // 如果已经不是第一次逆序
+				result.set(1, root); // result[1]更新为第二次逆序后面的元素
 			}
 		}
-		pre.set(0, root);// update pre
-		helper(root.right, pre, result);
+		pre.set(0, root);// update pre 
+		helper(root.right, pre, result); // 递归右子树
 	}
     
-    public static void inorder(TreeNode root) {
+	//测试用（与recoverTree无关）
+    public void inorder(TreeNode root) {
         if(root == null){
         	return;
         }

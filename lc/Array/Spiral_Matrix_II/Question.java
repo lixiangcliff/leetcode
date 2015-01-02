524a9afce0e4ebbfc9671f500279856db858bbcd
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-02 13:36:14 -0800

    update Spiral_Matrix_II

--- lc/Array/Spiral_Matrix_II/Question.java
+++ lc/Array/Spiral_Matrix_II/Question.java
@@ -1,65 +1,84 @@
 package Spiral_Matrix_II;
 
-import java.util.ArrayList;
-
 public class Question {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	int n = 0;
-	int[][] result = generateMatrix(n);	
+		Question q = new Question();
+		int n = 3;
+		int[][] result = q.generateMatrix(3);
 		// TODO Auto-generated method stub
-	for (int i = 0; i < n; i++){
-		for (int j = 0; j < n; j++){
-			System.out.print(result[i][j] + ",");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(result[i][j] + ",");
+			}
+			System.out.println();
 		}
-		System.out.println();
 	}
-	}
-	public static int[][] generateMatrix(int n) {
-
-        int[][] result = new int[n][n];
-/*    	if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
-    		return result;*/
-        int l = 0;
-        int u = 0;
-        int r = n-1;
-		int d = n-1;
-        int num = 0;
-        int end = n*n;
-        while(true){
-        	//rightward
-        	for(int i=l; i<= r; i++){
-        		result[u][i] = ++num;
-        	}   
-        	if (num == end)
-        		break;
-        	//downward
-        	for(int i=u+1; i<=d; i++){
-        		result[i][r] = ++num;
-        	}
-        	if (num == end)
-        		break;
-        	//leftward
-        	for(int i=r-1; i>=l; i--){
-        		result[d][i] = ++num;
-        	}  
-        	if (num == end)
-        		break;
-        	//upward
-        	for(int i=d-1; i>u; i--){
-        		result[i][l] = ++num;
-        	}
-        	if (num == end)
-        		break;
-        	l++;
-        	r--;
-        	u++;
-        	d--;
-        }
-        return result;
+	
+	/**
+	 * https://oj.leetcode.com/problems/spiral-matrix-ii/
+	 * Given an integer n, generate a square matrix filled with elements from 1
+	 * to n2 in spiral order.
+	 * 
+	 * For example, 
+	 * Given n = 3,
+	 * You should return the following matrix: 
+	 * [ 
+	 * [ 1, 2, 3 ], 
+	 * [ 8, 9, 4 ], 
+	 * [ 7, 6, 5 ] 
+	 * ]
+	 */
+	
+	//类似  Spiral_Matrix，只是多了一步边走边赋值，result填满了就说明走完了，便返回。
+	//http://www.cnblogs.com/yuzhangcmu/p/4047789.html
+	public int[][] generateMatrix(int n) {
+		int[][] result = new int[n][n];
+		int l = 0;
+		int u = 0;
+		int r = n - 1;
+		int d = n - 1;
+		int num = 1;
+		int end = n * n + 1; //【注】，因为最后一次num++导致 end需要等于matrix.size()+ 1才符合判断条件
+		while (true) {
+			// rightward
+			for (int j = l; j <= r; j++) {
+				result[u][j] = num++; // 先把num存入result，然后num自增
+			}
+			if (num == end) {
+				break;
+			}
+			// downward
+			for (int i = u + 1; i <= d; i++) {
+				result[i][r] = num++; 
+			}
+			if (num == end) {
+				break;
+			}
+			// leftward
+			for (int j = r - 1; j >= l; j--) {
+				result[d][j] = num++;
+			}
+			if (num == end) {
+				break;
+			}
+			// upward
+			for (int i = d - 1; i > u; i--) {
+				result[i][l] = num++;
+			}
+			if (num == end) {
+				break;
+			}
+			//左右上下边界都向中间生长
+			l++;
+			r--;
+			u++;
+			d--;
+		}
+		return result;
     }
 
 }

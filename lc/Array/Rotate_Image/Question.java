9bee5ce8c0a3eae94793e05db38d2cf797b5ae4b
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-07 20:20:50 -0800

    update Rotate_Image

--- lc/Array/Rotate_Image/Question.java
+++ lc/Array/Rotate_Image/Question.java
@@ -7,6 +7,7 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Question q = new Question();
 		int n = 4;
 		int[][] matrix = new int[n][n];
 		for (int i=0;i<n;i++){
@@ -17,7 +18,7 @@ public class Question {
 			System.out.println( "");
 		}
 		System.out.println( "==========");
-		rotate(matrix);
+		q.rotate(matrix);
 		for (int i=0;i<n;i++){
 			for (int j=0; j<n;j++){
 				System.out.print(matrix[i][j] + ",");
@@ -34,16 +35,18 @@ public class Question {
 	 * 
 	 * Follow up: Could you do this in-place?
 	 */
-    public static void rotate(int[][] matrix) {
-    	int size = matrix[0].length;
-        for (int i=0; i<size/2;i++){       	
-        	for (int j=i; j<size-i-1;j++ ){
-        		int temp = matrix[i][j]; // store upleft
-        		matrix[i][j] = matrix[size-1-j][i];// downleft to upleft
-        		matrix[size-1-j][i] = matrix[size-1-i][size-1-j];//  downright to downleft
-        		matrix[size-1-i][size-1-j] = matrix[j][size-1-i];//upright to downright
-        		matrix[j][size-1-i] = temp;// temp to upright
-        	}
-        }
-    }
+	
+	// 画图举例
+	public void rotate(int[][] matrix) {
+		int size = matrix[0].length;
+		for (int i = 0; i < size / 2; i++) {
+			for (int j = i; j < size - i - 1; j++) {
+				int temp = matrix[i][j]; // store upleft
+				matrix[i][j] = matrix[size - 1 - j][i];// downleft to upleft
+				matrix[size - 1 - j][i] = matrix[size - 1 - i][size - 1 - j];// downright to downleft
+				matrix[size - 1 - i][size - 1 - j] = matrix[j][size - 1 - i];// upright  to  downright
+				matrix[j][size - 1 - i] = temp;// temp to upright
+			}
+		}
+	}
 }

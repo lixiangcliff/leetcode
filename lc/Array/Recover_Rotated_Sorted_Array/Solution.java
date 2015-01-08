c09eb09e7c4d026a885b633becc63ed51b739c13
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-07 16:57:59 -0800

    update Recover_Rotated_Sorted_Array

--- lc/Array/Recover_Rotated_Sorted_Array/Solution.java
+++ lc/Array/Recover_Rotated_Sorted_Array/Solution.java
@@ -27,31 +27,32 @@ public class Solution {
 	}
 	
     /**
-     * @param nums: The rotated sorted array
-     * @return: The recovered sorted array
-     */
-	
-	/*
-	 * 例子：Example
-		[4, 5, 1, 2, 3] -> [1, 2, 3, 4, 5]
+     * http://lintcode.com/en/problem/recover-rotated-sorted-array/
+	 * Given a rotated sorted array, recover it to sorted array in-place.
+	 * Example [4, 5, 1, 2, 3] -> [1, 2, 3, 4, 5]
+	 * Challenge In-place, O(1) extra space and O(n) time.
+	 * Clarification What is rotated array:
+	 * 
+	 * - For example, the orginal array is [1,2,3,4], The rotated array of it
+	 * can be [1,2,3,4], [2,3,4,1], [3,4,1,2], [4,1,2,3]
 	 */
+	
     public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
-        // write your code
     	if (nums ==  null || nums.size() <= 1){
     		return;
     	}
     	int start = 0;
     	int end = nums.size() - 1; //【注】 end是最后一个有效位
-    	//找array的min
-    	while (start + 1 < end) {
-    		int mid = start +  (end - start) / 2;
+    	//找array的min，用了binary search模板
+    	while (start + 1 < end) { 
+    		int mid = start + (end - start) / 2;
     		if (nums.get(mid) < nums.get(end)) {
     			end = mid;
     		} else {
     			start = mid;
     		}
     	}
-    	int min = nums.get(start) < nums.get(end) ? start : end;
+    	int min = Math.min(start, start);
     	//三步翻转
     	reverse(nums, 0, min - 1);
     	reverse(nums, min, nums.size() - 1);

d27f2225376d27c3c530de623bc97b676f2a45b5
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-08 22:01:27 -0800

    update Find_a_Peak

--- lc/Binary_Search/Find_a_Peak/Solution.java
+++ lc/Binary_Search/Find_a_Peak/Solution.java
@@ -7,15 +7,28 @@ public class Solution {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Solution s = new Solution();
+		int[] A = {1, 2, 1, 3, 4, 5, 7, 6};
+		System.out.println(s.findPeak(A));
 	}
 	
     /**
-     * @param A: An integers array.
-     * @return: return any of peek positions.
-     */
+     * http://lintcode.com/en/problem/find-a-peak/
+	 * There is an integer array which has the following features:
+	 * The numbers in adjacent positions are different.
+	 * A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
+	 * We define a position P is a peek if A[P] > A[P-1] && A[P] > A[P+1].
+	 * Find a peak in this array. Return the index of the peak.
+	 * Note The array may contains multiple peeks, find any of them.
+	 * 
+	 * Example [1, 2, 1, 3, 4, 5, 7, 6]
+	 * return index 1 (which is number 2) or 6 (which is number 7)
+	 * 
+	 * Challenge Time complexity O(logN)
+	 */
+	
+	
     public int findPeak(int[] A) {
-        // write your code here
     	if (A == null || A.length < 3){
     		return -1;
     	}
@@ -23,13 +36,12 @@ public class Solution {
     	int end = A.length - 1;
     	while (start + 1 < end) {
     		int mid = start + (end - start) / 2; 
-    		if (A[mid] < A[mid + 1]) { //当前mid往右边看，如果升高的话，说明右边肯定有peak，把start挪到mid
+    		if (A[mid] < A[mid + 1]) { // 当前mid往右边看，如果升高的话，说明右边肯定有peak，把start挪到mid
     			start = mid;
     		} else {
     			end = mid;
     		}
     	}
-    	
     	return Math.max(start, end);
     }
 

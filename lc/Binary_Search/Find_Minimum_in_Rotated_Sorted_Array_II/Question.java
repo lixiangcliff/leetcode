ca132e44164a499f7191fe54dcfa319348967b4b
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-08 23:36:49 -0800

    update Find_Minimum_in_Rotated_Sorted_Array_II

--- lc/Binary_Search/Find_Minimum_in_Rotated_Sorted_Array_II/Question.java
+++ lc/Binary_Search/Find_Minimum_in_Rotated_Sorted_Array_II/Question.java
@@ -7,11 +7,12 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Question q = new Question();
 		//int[] num = {4, 5, 6, 7, 0, 1, 2};
 		//int[] num = {3,3,3,3,3,1,2};
 		//int[] num = {3,1,2,3,3,3,3};
 		int[] num = {1,1,2,-1,0,1};
-		System.out.println(findMin(num));
+		System.out.println(q.findMin(num));
 	}
 	
 	/**
@@ -25,55 +26,28 @@ public class Question {
 	 * Find the minimum element.
 	 * The array may contain duplicates.
 	 */
-	//binary search template
-	public static int findMin(int[] num) {
+	
+	//http://www.cnblogs.com/yuzhangcmu/p/4049117.html
+	public int findMin(int[] num) {
 		if(num == null || num.length ==0){
         	return Integer.MAX_VALUE;
         }
 		int start = 0;
 		int end = num.length - 1;
-		int min = Integer.MAX_VALUE;
+		
 		while (start + 1 < end) {
+			if (num[start] < num[end]) { // 这一句很重要，因为我们移除一些元素后，可能会使整个数组变得有序...
+				return num[start];
+			}
 			int mid = start + (end - start) / 2;
 			if (num[mid] < num [end]) {
 				end = mid;
-				min = Math.min(num[mid], min);
 			} else if (num[mid] > num [end]) {
 				start = mid;
-				min = Math.min(num[end], min);
-			} else {
+			} else { // 如果num[mid] == num [end]， 则end左移1位
 				end--;
 			}
 		}
-		return Math.min(Math.min(num[start], num[end]), min);
+		return Math.min(num[start], num[end]);
 	}
-	
-	
-	//http://blog.csdn.net/linhuanmars/article/details/40449299
-/*    public static int findMin(int[] num) {
-        if(num == null || num.length ==0){
-        	return 0;
-        }
-        int l =0;
-        int r = num.length-1;
-        int min = num[0];
-        while(l<r-1){ // why not l<r?..
-        	//comment from linhuanmars:
-        	//这里的问题是这样的，原先因为是搜索某一个元素，所以可以夹逼到找到元素或者他们交错为止，
-        	//现在的主要问题是如果l和r相差1以内，那么m就会等于l，如此下面的条件就会跳过r，
-        	//而最小元素可能就在r里面，所以终止条件才改成l<r-1使得夹逼就停止在最后两个元素中哈～
-        	int m = (l+r)/2;
-        	if(num[l]<num[m]){
-        		min = Math.min(min, num[l]);
-        		l = m+1;
-        	}else if(num[l]>num[m]){
-        		min = Math.min(min, num[m]);
-        		r = m-1;
-        	}else{
-        		l++;
-        	}
-        }
-        return Math.min(Math.min(min, num[l]), num[r]);
-    }
-*/
 }

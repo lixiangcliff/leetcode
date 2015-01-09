1d67514ba503b49ecf86149a5f2a71dd29387e8e
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-08 23:37:00 -0800

    update Find_Minimum_in_Rotated_Sorted_Array

--- lc/Binary_Search/Find_Minimum_in_Rotated_Sorted_Array/Question.java
+++ lc/Binary_Search/Find_Minimum_in_Rotated_Sorted_Array/Question.java
@@ -7,7 +7,9 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Question q = new Question();
+		int[] num = {4, 5, 6, 7, 0, 1, 2};
+		System.out.println(q.findMin(num));
 	}
 	
 	/**
@@ -19,16 +21,17 @@ public class Question {
 	 * Find the minimum element.
 	 * You may assume no duplicate exists in the array.
 	 */
+	
 	//Using Binary Search template
     public int findMin(int[] num) {
-        if(num == null || num.length == 0){
+        if (num == null || num.length == 0) {
         	return Integer.MAX_VALUE;
         }
         int start = 0;
         int end = num.length - 1;
         while (start + 1 < end) {
         	int mid = start + (end - start) / 2;
-        	if (num[mid] < num[end]) { //右边有序，所以min一定在左边
+        	if (num[mid] < num[end]) { //“num[mid] < num[end]”和“右边有序”是等价的。所以min一定在左边
         		end = mid;
         	} else {
         		start = mid;

28ca03469bd0e5206a561fc81e6b1f622bc6cee0
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-09 22:40:46 -0800

    update Remove_Element

--- lc/Array/Remove_Element/Question.java
+++ lc/Array/Remove_Element/Question.java
@@ -9,7 +9,7 @@ public class Question {
 		// TODO Auto-generated method stub
 		Question q = new Question();
 		//int[] A = {5,1,5,1,2,4,8,5,3,8,5,1,2,4};
-		int[] A = {5,1,5,1,2,4,8,5,3,8,5,1,2,4};
+		int[] A = {5,1,5,1,2,4};
 		int a = q.removeElement(A,5);
 		System.out.println(a);
 		for(int i = 0; i<A.length; i++){
@@ -35,10 +35,9 @@ public class Question {
 		}
 		int len = A.length - 1; // len从右往左扫 
 		for (int i = 0; i <= len; i++) { // i和len相遇才能停，因为i负责检查该位是否为element
-    		if (A[i] == elem) { //只要A[i]==elem, 就用A[len]值来replace A[i]值, 即使A[len]也等于elem也照样换
-    			A[i--] = A[len--]; // 因为新换来的A[i]上的数值(A[len])可能仍然是elem，所以i--，使其仍留在此位置，继续循环，直到A[i]!=elem, i才继续向右移动
-    		}
+    		while (A[i] == elem && i <= len) { //只要A[i]==elem, 就用A[len]值来replace A[i]值,并一直检查i是否仍然<=len
+    			A[i] = A[len--];     		}
     	}
-    	return len + 1; //len最终表示：值不是element的最右一个数。所以len + 1即为数组长度 
+    	return len + 1; //len最终所在位置， 是值不同于element的最右一个数。所以len + 1即为数组长度 
     }
 }

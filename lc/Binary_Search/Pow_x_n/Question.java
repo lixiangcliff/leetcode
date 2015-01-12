63d8b1f1b63e2761f35a7c9dd1186135c778d436
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-11 22:45:58 -0800

    update Pow_x_n

--- lc/Binary_Search/Pow_x_n/Question.java
+++ lc/Binary_Search/Pow_x_n/Question.java
@@ -7,36 +7,33 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(pow(-13.62608,3));
+		Question q = new Question();
+		System.out.println(q.pow(5, -2));
 	}
 	
 	/**
 	 * https://oj.leetcode.com/problems/powx-n/
 	 * Implement pow(x, n).
 	 */
-    public static double pow(double x, int n) {
-    	boolean positive;
-    	if (n>0){
-    		positive = true;
-    	}else{
-    		n *= -1;
-    		positive = false;
-    	}
-    	if(x == 1){
-    		return 1;
-    	}
-    	if (x == -1){
-    		return n%2==0 ? 1 : -1;
-    	}
-        double result = 1.0;
-        while(n>0){
-        	if (Math.abs(result) < Double.MIN_VALUE){
-        		return Double.MIN_VALUE;
-        	}
-        	result *= x;
-        	n--;
-        }
-        return positive? result : 1/result;
-    }
-
+	
+	//http://www.cnblogs.com/yuzhangcmu/p/4174683.html
+	//n < 0时，转化为n > 0的处理，要注意可能的越界的问题。(Interger.MIN_VALUE的绝对值比Interger.MAX_VALUE大1，所以Interger.MIN_VALUE取反会越界)
+	public double pow(double x, int n) {
+		if (x == 0) { // 这一步可有可无
+			return 0;
+		}
+		if (n == 0) { // base case
+			return 1;
+		}
+		if (n < 0) { //【注】n + 1防止越界；再取反转化为计算次方数为正的运算（即下面的二分递归）；再多乘一个x，恢复数值
+			double reciprocal = pow(x, -(n + 1)) * x ;
+			return 1 / reciprocal; // 取倒数，返回真实值
+		}
+		double result = pow(x , n /2); // 下面是n > 0， 二分递归，再将结果相乘
+		result *= result;
+		if (n % 2 == 1) { // 如果有余数，则再补乘一个x
+			result *= x;
+		}
+		return result;
+	}
 }

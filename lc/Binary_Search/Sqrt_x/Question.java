b521c58398221694e98ca3b80f2d587ad49ab1ef
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-13 14:22:22 -0800

    update Sqrt_x

--- lc/Binary_Search/Sqrt_x/Question.java
+++ lc/Binary_Search/Sqrt_x/Question.java
@@ -7,7 +7,8 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(sqrt(2147395599));
+		Question q = new Question();
+		System.out.println(q.sqrt(2147395599));
 
 	}
 	
@@ -16,34 +17,35 @@ public class Question {
 	 * Implement int sqrt(int x). 
 	 * Compute and return the square root of x.
 	 */
-	//using BST template
+	
+	//using BS template
 	//http://blog.csdn.net/linhuanmars/article/details/20089131
-    public static int sqrt(int x) {
-    	if(x<0){
-    		return -1;
-    	}
-    	if(x<=1){
-    		return x;
-    	}
-    	int start = 1;
-    	//【注】 当x>=2时， 有x/2 >= sqrt(x) 恒成立。所以可以取end = x/2作为 sqrt(x)的上界
-    	int end = x / 2;
-    	while (start + 1 < end) {
-    		int mid = start + (end - start) / 2;
-    		//下面条件即是 mid^2 <= x < (mid+1)^2 但是需要除法代替乘法。道理和上面mid=start+(end-start)/2一样，都是为了避免overflow
-    		if (mid <= x / mid && (mid + 1) > x / (mid + 1)) { //夹逼找到所求值
-    			return mid;
-    		} else if (mid < x / mid) { //mid^2 < x
-    			start = mid;
-    		} else {
-    			end = mid;
-    		}
-    	}
-    	if (start <= x / start && (start + 1) > x / (start + 1)){
-    		return start;
-    	} else {
-    		return end;
-    	}
-    }
+	public int sqrt(int x) {
+		if (x < 0) {
+			return -1;
+		}
+		if (x <= 1) {
+			return x;
+		}
+		int start = 1; // sqrt(x)的下限
+		int end = x / 2; // 【注】 当x>=2时， 有x/2 >= sqrt(x) 恒成立。所以可以取end = x/2作为 sqrt(x)的上限
+		// sqrt(x)在start和end之间的某个数，即x在start^2和end^2之间的某个数，下面的二分法就是就是这样实现的。
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			// 下面条件即是 mid^2 <= x < (mid+1)^2  【注】但是需要除法代替乘法。道理和上面mid=start+(end-start)/2一样，都是为了避免overflow
+			if (mid <= x / mid && (mid + 1) > x / (mid + 1)) { // 夹逼找到所求值
+				return mid;
+			} else if (x / mid > mid) { // x > mid^2
+				start = mid;
+			} else {
+				end = mid;
+			}
+		}
+		if (start <= x / start && (start + 1) > x / (start + 1)) { // 即  start^2 <= x < (start + 1)^2
+			return start;
+		} else {
+			return end;
+		}
+	}
 	
 }

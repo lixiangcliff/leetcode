318ed165237f2f6e4ddb34787dacf182f3c42963
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-13 12:05:02 -0800

    update Search_a_2D_Matrix_II

--- lc/Binary_Search/Search_a_2D_Matrix_II/Solution.java
+++ lc/Binary_Search/Search_a_2D_Matrix_II/Solution.java
@@ -11,25 +11,28 @@ public class Solution {
 		// TODO Auto-generated method stub
 
 	}
-    /**
-     * @param matrix: A list of lists of integers
-     * @param: A number you want to search in the matrix
-     * @return: An integer indicate the occurrence of target in the given matrix
-     */
-	/*
-	 * 例子：to find count of 7
+	
+	/**
+	 * http://lintcode.com/en/problem/search-a-2d-matrix-ii/
+	 * Write an efficient algorithm that searches for a value in an m x n
+	 * matrix, return the occurrence of it.
+	 * This matrix has the following properties:
+	 * Integers in each row are sorted from left to right.
+	 * Integers in each column are sorted from up to bottom.
+	 * No duplicate integers in each row or column.
+	 * 
+	 * Example Consider the following matrix:
 	 * [
-
-	    [1, 3, 5, 7],
-	
-	    [2, 4, 7, 8],
-	
-	    [3, 5, 9, 10]
-
-		]
+	 * 	[1, 3, 5, 7],
+	 * 	[2, 4, 7, 8],
+	 * 	[3, 5, 9, 10]
+	 * ]
+	 * Given target = 3, return 2.
+	 * 
+	 * Challenge O(m+n) time and O(1) extra space
 	 */
+	
     public int searchMatrix(ArrayList<ArrayList<Integer>> matrix, int target) {
-        // write your code
     	if (matrix == null || matrix.size() == 0 || matrix.get(0).size() == 0) {
     		return 0;
     	}
@@ -47,7 +50,6 @@ public class Solution {
     			row--;
     		}
     	}
-    	
     	return count;
     }
 

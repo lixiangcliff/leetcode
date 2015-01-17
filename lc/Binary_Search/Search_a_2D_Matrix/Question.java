69e0d113b51516f85d213c0a87d00e9fff022f42
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-16 22:10:54 -0800

    update Search_a_2D_Matrix

--- lc/Binary_Search/Search_a_2D_Matrix/Question.java
+++ lc/Binary_Search/Search_a_2D_Matrix/Question.java
@@ -49,15 +49,16 @@ public class Question {
 	
 	//BS模板。看【注】！
     public boolean searchMatrix(int[][] matrix, int target) {
-    	if(matrix == null || matrix.length==0 || matrix[0].length==0)  
-            return false; 
+    	if(matrix == null || matrix.length == 0 || matrix[0].length == 0) { 
+            return false;
+    	}
     	//处理所有行的第0个元素
     	int start= 0;
     	int end = matrix.length-1;
     	//用模板。即求第0列中，等于target值的，或者比target小的最大值   	
-    	while ( start + 1 < end) {
+    	while (start + 1 < end) {
     		int mid = start + (end - start) / 2;
-    		if (matrix[mid][0] == target) {//求比target小的最大值，所以窗口尽量左移
+    		if (matrix[mid][0] == target) {
     			return true;
     		} else if (matrix[mid][0] < target){
     			start = mid;
@@ -69,7 +70,7 @@ public class Question {
     	//【注】BST的模板的核心就在这里了！要找的是等于target或者比target小的里面最大的，两个候选start和end，end肯定比start的值大，所以先试end
     	if (matrix[end][0] <= target) {
     		row = end;
-    	}else {
+    	} else {
     		row = start;
     	}
     	if (matrix[row][0] > target) {//【注】由上面可知，matrix[row][0]小于等于target的。即使这样如果仍有matrix[row][0] > target，则说明matrix里不包含target
@@ -77,21 +78,21 @@ public class Question {
     	}
     	//处理这一行(row)的所有列
     	start = 0;
-    	end = matrix[0].length-1;
+    	end = matrix[0].length - 1;
     	//用模板。
-    	while (start + 1 < end) {
-    		int mid = start + (end - start) / 2;
-    		if (matrix[row][mid] == target) {
-    			return true;
-    		}else if(matrix[row][mid] < target){
-    			start = mid;
-    		}else{
-    			end = mid;
-    		}
-    	}
-    	if (matrix[row][start] == target || matrix[row][end] == target) {
-    		return true;
-    	}
-    	return false;
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if (matrix[row][mid] == target) {
+				return true;
+			} else if (matrix[row][mid] < target) {
+				start = mid;
+			} else {
+				end = mid;
+			}
+		}
+		if (matrix[row][start] == target || matrix[row][end] == target) {
+			return true;
+		}
+		return false;
     }
 }

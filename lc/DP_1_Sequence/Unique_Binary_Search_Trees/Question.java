8f5d4aa8a0addf2d20e1f570ae8b211b00f09b45
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-17 16:43:22 -0800

    update Unique_Binary_Search_Trees

--- lc/DP_1_Sequence/Unique_Binary_Search_Trees/Question.java
+++ lc/DP_1_Sequence/Unique_Binary_Search_Trees/Question.java
@@ -23,28 +23,31 @@ public class Question {
 		    /     /       \                 \
 		   2     1         2                 3
 	 */
+	
 	//DP 1Seq
 	//http://blog.csdn.net/linhuanmars/article/details/24761459
 	//http://www.ninechapter.com/solutions/unique-binary-search-trees/
 	//1.state: result[i]表示当有i个node时 一共有多少独立的BST
 	//2.function: 则result[i] = result[0] * result[i - 1] + result[1] * result[i - 2] + ... + result[n - 1] * result[0]
-	//（即，如果有i个node，根占用了1个node，还剩i - 1个node；可以左子树分0个，右子树分i - 1，这种情况左右子树的所有可能组合个数为：result[0] * result[i - 1]
-	// 同理，还可以左子树分1个，右子树分i - 2个，这种情况左右子树的所有可能组合个数为：result[1] * result[i - 2]；etc，把这些所有可能加和起来就可以得到result[i]了）
+	//（即，如果有i个node，根占用了1个node，还剩i - 1个node：
+	// 这时可以左子树分0个，右子树分i - 1，这种情况左右子树的所有可能组合个数为：result[0] * result[i - 1]
+	// 同理，还可以左子树分1个，右子树分i - 2个，这种情况左右子树的所有可能组合个数为：result[1] * result[i - 2]；
+	// etc...把这些所有可能加和起来就可以得到result[i]了）
 	//3.initialize: result[0] = 1; result[1] = 1
 	//4.answer: result[n]
 	public int numTrees(int n) {
 		if (n < 0) {
 			return Integer.MIN_VALUE;
 		}
-		if (n == 0) {
+		if (n == 0) {//【注】 没有node 也是一种BST的组成方式（联想下result[0]*result[i-1]肯定不为0，所以result[0]也不该为0才合理。）
 			return 1;
 		}
 		int[] result = new int[n + 1];
-		result[0] = 1;
+		result[0] = 1; //【注】 没有node 也是一种BST的组成方式
 		result[1] = 1;
-		for (int i = 2; i <= n; i++) {
-			for (int j = 0; j <= i - 1; j++) {
-				result[i] += result[j] * result[i - 1 - j];
+		for (int i = 2; i <= n; i++) { //理解为：i表示当前一共有几个node
+			for (int j = 0; j <= i - 1; j++) { // 理解为：j表示根的左孩子有几个node
+				result[i] += result[j] * result[i - 1 - j]; // 左右孩子node总数为i - 1。
 			}
 		}
 		return result[n];

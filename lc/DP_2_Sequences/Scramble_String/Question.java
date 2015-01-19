357433f94d982accc8a2be9f3adfecb87155ea9c
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-19 11:24:10 -0800

    update Scramble_String

--- lc/DP_2_Sequences/Scramble_String/Question.java
+++ lc/DP_2_Sequences/Scramble_String/Question.java
@@ -56,11 +56,10 @@ public class Question {
 	 */
 	
 	//DP 2_Seq
-	//state: res[i][j][len]，其中i是s1的起始字符，j是s2的起始字符，而len是当前的字符串长度，res[i][j][len]表示的是以i和j分别为s1和s2起点的长度为len的字符串是不是互为scramble。
-	//function: res[i][j][len] = || (res[i][j][k]&&res[i+k][j+k][len-k] || res[i][j+len-k][k]&&res[i+k][j][len-k]) 对于所有1<=k<len，也就是对于所有len-1种劈法的结果求或运算
+	//state: res[i][j][len]，其中i是s1的起始字符，j是s2的起始字符，而len是当前所截取的字符串长度，res[i][j][len]表示在s1和s2中分别以i和j为起点，len为长度的字符串是不是互为scramble。
+	//function: res[i][j][len] = || (res[i][j][k]&&res[i+k][j+k][len-k] || res[i][j+len-k][k]&&res[i+k][j][len-k]) k表示劈一刀的位置，k范围[1,len)，也就是对于所有len-1种劈法的结果求“或”运算
 	//initialize: result[i][j][1] = s1.charAt(i) == s2.charAt(j);
 	//result: result[0][0][strLen]
-	
 	//http://blog.csdn.net/linhuanmars/article/details/24506703
     public boolean isScramble(String s1, String s2) {
         if (s1 == null || s2 == null || s1.length() != s2.length()) {
@@ -78,11 +77,13 @@ public class Question {
         	}
         }
         for (int len = 2; len <= strLen; len++) { // 即组成左边scramble+右边scramble可能的长度从2一直到strLEn
-        	for (int i = 0; i < strLen + 1 - len; i++) { // 关于边界：i < len + 1 - k; 令k为初始值2，i本来要走到len-1，则带入对比系数课得之。
-        		for (int j = 0; j < strLen + 1 - len; j++) {
+        	for (int i = 0; i <= strLen - len; i++) { // 关于边界：i <= strLen - len; 举例令len为2，strLen为8，则i最远只能走到index==6，带入对比系数可得表达式。
+        		for (int j = 0; j <= strLen - len; j++) {
         			for (int k = 1; k < len; k++) { //k可以把len劈len - 1刀，即k可以出现在len内部的（1~len - 1）位置上
         				//对于当前k，只要有一组i,j满足条件就可以把result[i][j][k]置为true，所以用“|=”来“或”每一步的结果来得到最终结果
-        				//【注】要时刻分清k和len的区别：len是当前的字符串长度，k表示的是在len这么长的串里劈那1刀的位置（此处的表达式里不可能再出现strLen了）
+        				//【注】要时刻分清k和len的区别：len是当前的字符串长度，k表示的是在len这么长的串里劈那1刀的位置（此处的表达式里不应该再出现strLen了）
+        				//result[i][j][k] && result[i + k][j + k][len - k]：在i和j的第k个位置之后劈开，左与左([起点i][长度k]与[起点j][长度k])为真&&右与右([起点i+k][长度len-k]与[起点j+k][长度len-k])为真（看图）
+        				//result[i][j][k] && result[i + k][j + k][len - k]：在i的第k个位置之后,j的第len-k劈开，左与右([起点i][长度k]与[起点j+len-k][长度k])为真&&右与左([起点i+k][长度len-k]与[起点j][长度len-k])为真（看图）
         				result[i][j][len] |= result[i][j][k] && result[i + k][j + k][len - k] || result[i][j + len - k][k] && result[i + k][j][len - k];
         			}
         		}
@@ -93,7 +94,7 @@ public class Question {
     
     //recursion: 是O(3^n)。
     //但剪枝可以降低复杂度，即"进入递归前，先把2个字符串排序，再比较，如果不相同，则直接退出掉。这样能有效地减少复杂度，具体多少算不清。但能通过leetcode的检查。"
-  //在leetcode实际效果中，recursion比DP要快
+    //在leetcode实际效果中，recursion比DP要快(DP:283ms; Recursion: 244ms)
     //http://www.cnblogs.com/yuzhangcmu/p/4189152.html
     public boolean isScrambleRecursion(String s1, String s2) {
     	if (s1 == null || s2 == null || s1.length() != s2.length()) {
@@ -113,7 +114,7 @@ public class Question {
     	//剪枝，如果s1和s2排序好之后不相同，则他们肯定不可能是scramble的
     	char[] s1Char = s1.toCharArray();
     	Arrays.sort(s1Char);
-    	String s1Sort = new String(s1Char); // 【注】s1Char.toString();这样写是错的！s1Sort的实际内容是hashcode而不是s1Char的内容！
+    	String s1Sort = new String(s1Char); // 【注】s1Char.toString();这样写是错的！s1Char.toString()的实际内容是hashcode而不是s1Char的内容！
     	char[] s2Char = s2.toCharArray();
     	Arrays.sort(s2Char);
     	String s2Sort = new String(s2Char);
@@ -121,13 +122,13 @@ public class Question {
     		return false;
     	}
     	//划分两个字符串
-    	for (int i = 1; i < len; i++) { // 只要在len长度中有任何一种划分位置i，可以满足scramble，则true
+    	for (int i = 1; i < len; i++) { // i表示划分的位置（len中一共有len-1个间隔可以划分）。只要存在一个划分位置i，可以满足scramble，则true
     		//scramble两种情况：满足其中一种则true
     		//left - left && right - right 
     		if (helper(s1.substring(0, i), s2.substring(0, i)) && helper(s1.substring(i, len), s2.substring(i, len))) {
     			return true;
     		}
-    		//left - right && right - left 【注】留意right的起始位置
+    		//left - right && right - left 【注】边界画图举例
     		if (helper(s1.substring(0, i), s2.substring(len - i, len)) && helper(s1.substring(i, len), s2.substring(0, len - i))) {
     			return true;
     		}

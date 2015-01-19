c9fc503f076ed7f6254f75a6e96ddbe0436a8b59
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-18 16:43:26 -0800

    update Interleaving_String

--- lc/DP_2_Sequences/Interleaving_String/Question.java
+++ lc/DP_2_Sequences/Interleaving_String/Question.java
@@ -7,15 +7,15 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Question q = new Question();
 /*		String s1 = "aabcc";
 		String s2 = "dbbca";
 		String s3 = "aadbbcbcac";*/
 		String s1 = "abc";
 		String s2 = "def";
-		//String s3 = "abcdef";
-		String s3 = "aadbbbaccc";
-		
-		//System.out.println(isInterleave(s1, s2, s3));
+		String s3 = "abcdef";
+		//String s3 = "aadbbbaccc";
+		System.out.println(q.isInterleave(s1, s2, s3));
 	}
 
 	/**
@@ -26,9 +26,10 @@ public class Question {
 	 * When s3 = "aadbbcbcac", return true. 
 	 * When s3 = "aadbbbaccc", return false.
 	 */
+	
 	//1.state: result[i][j]代表： 在s1的前i个字符配上s2的前j个字符，是否可以交错表达出s3的前i+j个字符。
 	//2.function: result[i][j] = (s1[i] == s3[i+j] && result[i-1][j] ) OR (s2[j] == s3[i+j] && result[i][j-1]) 
-	//（即此时s1和s2是等价的，以s1为例：只要s1[i]==s3[i+j]，并且s1的前i-1和s2的前j个可以表达出s3的前i=j-1个，即result[i-1][j]，result[i][j]则为真。s2类似可推）
+	//（即此时s1和s2是等价的，以s1为例：只要s1[i]==s3[i+j]，并且s1的前i-1和s2的前j个可以表达出s3的前i+j-1个，即result[i-1][j]，result[i][j]则为真。s2类似可推）
 	//3.initialize: result[0][j] = s2[j]==s3[j]; 第一行（s1为空，所以只能靠s2来构造s3。 如果s2[j]==s3[j]则result[0][j]为真）
 	//				result[i][0] = s1[i]==s3[i]; 第一列（类似第一行）
 	//				result[0][0] = true;
@@ -41,26 +42,27 @@ public class Question {
 		if (s2 == null || s2.length() == 0) {
 			return s1.equals(s3);
 		}
-		if (s1.length() + s2.length() != s3.length()) {
+		int len1 = s1.length();
+		int len2 = s2.length();
+		int len3 = s3.length();
+		if (len1 + len2 != len3) {
 			return false;
 		}
-		
-		boolean[][] result = new boolean[s1.length() + 1][s2.length() + 1];
-		result[0][0] = true;
-		//初始化第一行
-		for (int j = 1; j <= s2.length(); j++) {
-			result[0][j] = s2.charAt(j - 1)==s3.charAt(j - 1); //有位差
-		}
-		//初始化第一列
-		for (int i = 1; i <= s1.length(); i++) {
-			result[i][0] = s1.charAt(i - 1)==s3.charAt(i - 1); //有位差
-		}
-		for (int i = 1; i <= s1.length(); i++) {
-			for (int j= 1; j <= s2.length(); j++) {
-				result[i][j] = s1.charAt(i - 1)==s3.charAt(i + j - 1) && result[i - 1][j] || s2.charAt(j - 1)==s3.charAt(i + j - 1) && result[i][j - 1];
+		boolean[][] result = new boolean[len1 + 1][len2 + 1];
+		for (int i = 0; i <= len1; i++) {
+			for (int j = 0; j <= len2; j++) {
+				if (i == 0 && j == 0) {
+					result[i][j] = true;
+				} else if (i == 0) { //初始化第一行.有位差
+					result[0][j] = s2.charAt(j - 1) == s3.charAt(j - 1); 
+				} else if (j == 0) { //初始化第一列.有位差
+					result[i][0] = s1.charAt(i - 1) == s3.charAt(i - 1); 
+				} else { // 其余位置。有位差
+					result[i][j] = s1.charAt(i - 1) == s3.charAt(i + j - 1) && result[i - 1][j] || s2.charAt(j - 1) == s3.charAt(i + j - 1) && result[i][j - 1];
+				}
 			}
 		}
-		return result[s1.length()][s2.length()];
+		return result[len1][len2];
 	}
 	
 	

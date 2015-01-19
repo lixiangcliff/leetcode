b2f5245e9abbf7c422250f9733fedbfa4dc65c27
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-19 01:21:58 -0800

    update Wildcard_Matching

--- lc/DP_2_Sequences/Wildcard_Matching/Question.java
+++ lc/DP_2_Sequences/Wildcard_Matching/Question.java
@@ -15,6 +15,7 @@ public class Question {
 		System.out.println(q.isMatch("aa", "a*"));
 		System.out.println(q.isMatch("ab", "?*"));
 		System.out.println(q.isMatch("aab", "c*a*b"));
+		System.out.println(q.isMatch("", "*"));
 	}
 	
 	/**
@@ -36,6 +37,7 @@ public class Question {
 	 * isMatch("ab", "?*") → true 
 	 * isMatch("aab", "c*a*b") → false
 	 */
+	
 	//Match可以理解为前面的string可否合理用后面的string表示出来
 	//DP 2_Seq
 	//http://blog.csdn.net/linhuanmars/article/details/21198049
@@ -44,16 +46,66 @@ public class Question {
 	//2.function: 
 	//	(1)当p[j] != '*': 
 	//			如果result[i - 1][j - 1]为true，并且isEqual(s[i], p[j]), 则result[i][j]为true
-	//	(3)当p[j] == '*': 
-	//			如果result[i][j - 1]为true，因为"*"可以match任意字符串，所以 result[i, i + 1, ... s.length][j]都为true
-	//3.initialize: result[0][0] = true;
+	//	(2)当p[j] == '*': 
+	//			因为"*"可以表示任何字符，所以以下几种情况可以使result[i][j]为true
+	//			(a)只要有任何一个result[k][j-1]（k范围[0,i]）为true。（因为k后面的剩余都可以被"*"来match上）
+	//			(b)j==1时。 即如果p就是"*"，那么p可以匹配任何s
+	//			(c)result[i][j - 1]为true。这时"*"表示空
+	//3.initialize: result[0][0] = true; // s和p都为空
+	//				result[i][0] = false; // p为空，并且s不为空
+	//				result[0][j] = p[j]为'*',并且result[0][j - 1]为true，则result[0][j]才为true // s为空 ，并且p不为空。
 	//4.answer: result[A.length][B.length];
 	//
 	//【注】 isEqual(s[i], p[j])定义为s[i] == p[j] or p[j] = '?'
 	//【注】result[][]和s，p有位差
+	
+	public boolean isMatch(String s, String p) {
+		if (p.length() == 0) {
+			return s.length() == 0;
+		}
+        //to pass leetcode start;
+        // without this optimization, it will fail for large data set
+		int pNoneStarLen = 0; //p中的非'*'的字符数量
+		for (char c : p.toCharArray()) {
+			if (c != '*') {
+				pNoneStarLen++;
+			}
+		}
+		if (pNoneStarLen > s.length()){ //如果p中非'*'字符数量比s还多，则说明p中肯定有冗余，所以false
+			return false;
+		}
+        //to pass leetcode end;
+		boolean[][] result = new boolean[s.length() + 1][p.length() + 1];
+		for (int i = 0; i <= s.length(); i++) {
+			for (int j = 0; j <= p.length(); j++) {
+				if (i == 0 && j == 0) {
+					result[i][j] = true;
+				} else if (i == 0) { //p[j]为'*',并且result[0][j - 1]为true，则result[0][j]才为true
+					result[i][j] = result[0][j - 1] && p.charAt(j - 1) == '*'; // 位差
+				} else if (j == 0) {
+					result[i][j] = false;
+				} else {
+					if (p.charAt(j - 1) != '*') { // 位差
+		    			result[i][j] = result[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?');
+					} else {
+						boolean foundPrevMatch = false;
+						for (int k = 0; k <= i; k++) {
+							if (result[k][j - 1]) {
+								foundPrevMatch = true;
+								break;
+							}
+						}
+						result[i][j] = foundPrevMatch || j == 1 || result[i][j - 1];
+					}
+				}
+			}
+		}
+		return result[s.length()][p.length()];
+	}
+	
 	//【注】外层循环应该是p,然后待匹配串s内层循环扫过来
 	//http://blog.csdn.net/linhuanmars/article/details/21198049
-	public boolean isMatch(String s, String p) {
+	public boolean isMatchDP2(String s, String p) {
 		if (p.length() == 0) {
 			return s.length() == 0;
 		}
@@ -100,35 +152,4 @@ public class Question {
 		}
 		return result[s.length()][p.length()];
 	}
-	
-	//http://blog.csdn.net/linhuanmars/article/details/21198049
-    public boolean isMatch2(String s, String p) {
-        if (p.length() == 0){
-        	return s.length() == 0;
-        }
-        //to pass leetcode start;
-        if(s.length()>300 && p.charAt(0)=='*' && p.charAt(p.length()-1)=='*')  
-            return false;
-        ////to pass leetcode end;
-        boolean[] result = new boolean[s.length() + 1];
-        result[0] = true;
-        for(int j=0; j<p.length(); j++){
-        	if(p.charAt(j) != '*'){
-        		for(int i=s.length()-1; i>=0; i--){
-        			result[i+1] = result[i] && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='?');
-        		}
-        	}else{
-        		int i=0;
-        		while(i<=s.length() && !result[i]){
-        			result[i++] = false;
-        		}
-        		while(i<=s.length()){
-        			result[i++] = true;
-        		}
-        	}
-        	result[0] = result[0] && p.charAt(j)=='*';
-        }
-        return result[s.length()];
-    }
-
 }

1b9b656cde64c5ea45bc25660e44eabfe93845f1
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-19 18:23:20 -0800

    update Backpack

--- lc/DP_Backpack/Backpack/Solution.java
+++ lc/DP_Backpack/Backpack/Solution.java
@@ -8,11 +8,12 @@ public class Solution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] A = {2, 3, 5, 7};
-		int m = 11;
+		int m = 12;
 		Solution s = new Solution();
 		System.out.println(s.backPack(m, A));
 	}
 	/**
+	 * http://lintcode.com/en/problem/backpack/
 	 * Given n items with size A[i], an integer m denotes the size of a
 	 * backpack. How full you can fill this backpack?
 	 * 
@@ -27,12 +28,13 @@ public class Solution {
 	 * You function should return the max size we can fill in the given
 	 * backpack.
 	 */
+	
 	//1.state: result[i][m]代表A的前i个数字，取出一些，是否能组成和为m。
 	//2.function: result[i][m] = result[i - 1][m - A[i]] OR result[i - 1][m] 
 	//	result[i - 1][m - A[i]]表示“和”中选取A[i], 即在前i - 1个数能取出“和”为m - A[i]，那么在第i个数取A[i]就行了
 	//	result[i - 1][m]表示“和”中不选取A[i]，即在前i - 1个数就已经取好了“和”为m
 	//3.initialize: result[0][0] = true;
-	//				result[i][0] = false;
+	//				result[i][0] = true;
 	//				result[0][j] = false;
 	//4.answer: 找从右往左(result[A.length][m], result[A.length][m - 1]...第一个为true的result[A.length][n]，返回n);
 	//【注】技巧是，DP数组表示“能不能”取出来，然后求结果时候，就从“能”取的里找最大的。
@@ -43,14 +45,16 @@ public class Solution {
     		return 0;
     	}
     	boolean[][] result = new boolean[A.length + 1][m + 1];
-    	result[0][0] = true;
-    	for (int i = 1; i <= A.length; i++) {
+    	//result[0][0] = true;
+    	for (int i = 0; i <= A.length; i++) {
     		for (int j = 0; j <= m; j++) {
-    			if (j == 0) {
+    			if (i == 0 && j == 0) {
+    				result[i][j] = true;
+    			} else if (i == 0) {
+    				result[i][j] = false;
+    			} else if (j == 0) {
         			result[i][0] = true;
-        			continue;
-        		}
-    			if ( (j >= A[i - 1] && result[i - 1][j - A[i - 1]]) || result[i - 1][j]) { //A有位差
+        		} else if ((j >= A[i - 1] && result[i - 1][j - A[i - 1]]) || result[i - 1][j]) { //A有位差
     				result[i][j] = true;
     			}
     		}

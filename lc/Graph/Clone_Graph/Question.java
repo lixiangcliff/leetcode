dae26b561446bf21149c58cad0234a62d9c4b2f6
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-21 21:25:51 -0800

    update Clone_Graph

--- lc/Graph/Clone_Graph/Question.java
+++ lc/Graph/Clone_Graph/Question.java
@@ -25,7 +25,7 @@ public class Question {
 	//通过遍历原来的graph，得到原来各个node的neighbor，再根据上面map的对应关系找到在新graph里对应的copyNode及其copyNeighborNode。
 	//http://blog.csdn.net/linhuanmars/article/details/22715747
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-        if (node == null){
+        if (node == null) {
         	return null;
         }
         //map的key是原graph的node，value是对应的clone出来的node
@@ -36,9 +36,9 @@ public class Question {
         queue.offer(node);
         while (!queue.isEmpty()) { //BFS 【注】下面过程看着图更容易一步步写出来
         	UndirectedGraphNode curNode = queue.poll();
-        	for(int i = 0;i < curNode.neighbors.size(); i++){ //遍历当前node的所有neighbors
+        	for (int i = 0; i < curNode.neighbors.size(); i++) { //遍历当前node的所有neighbors
         		//1.in new graph, clone neighbor
-        		if(!map.containsKey(curNode.neighbors.get(i))){//如果map里还没有curNode.neighbors.get(i)，即还没有访问过curNode.neighbors.get(i)这个节点，
+        		if (!map.containsKey(curNode.neighbors.get(i))) {//如果map里还没有curNode.neighbors.get(i)，即还没有访问过curNode.neighbors.get(i)这个节点，
         			copyNode = new UndirectedGraphNode(curNode.neighbors.get(i).label);//那么就先clone一个curNode.neighbors.get(i)的copy节点：copyNode，出来
         			map.put(curNode.neighbors.get(i), copyNode);//然后再把当前节点的第i个neighbor和clone出来的copyNeighborNode一起加入map中
         			queue.offer(curNode.neighbors.get(i));//再把当前节点的第i个neighbor放入queue中等待下一轮处理。
@@ -56,7 +56,6 @@ public class Question {
     }
 
 }
-
 
  class UndirectedGraphNode {
      int label;

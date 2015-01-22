fac467c4f118e1e4302220a0fc678edb2fc24673
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-22 14:44:46 -0800

    update Palindrome_Partitioning

--- lc/Backtracking/Palindrome_Partitioning/Question.java
+++ lc/Backtracking/Palindrome_Partitioning/Question.java
@@ -24,7 +24,7 @@ public class Question {
 	}
 
 	/**
-	 * 
+	 * https://oj.leetcode.com/problems/palindrome-partitioning/
 	 * Given a string s, partition s such that every substring of the partition
 	 * is a palindrome.
 	 * Return all possible palindrome partitioning of s.

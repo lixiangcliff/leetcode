ec412a6e50898b2dbc4f192212166bf1fb962b33
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-22 20:55:47 -0800

    update Combination_Sum

--- lc/Backtracking/Combination_Sum/Question.java
+++ lc/Backtracking/Combination_Sum/Question.java
@@ -50,7 +50,7 @@ public class Question {
 	//http://blog.csdn.net/linhuanmars/article/details/20828631
     public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
     	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-    	if (candidates == null || candidates.length == 0){
+    	if (candidates == null || candidates.length == 0) {
     		return result;
     	}
     	Arrays.sort(candidates); // 这类题基本都需要先sort。对于此题sort之后，递归时如果当前元素已经比target大了，则之后的元素就不用再看了，直接返回即可。
@@ -61,16 +61,16 @@ public class Question {
     }
     
     private void helper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> item, int[] candidates, int pos, int target){
-    	if(target < 0){ // 因为所有元素都为正，如果target为负，则不可能找到数字组合来达到target，故返回
+    	if (target < 0) { // 因为所有元素都为正，如果target为负，则不可能找到数字组合来达到target，故返回
     		return;		
     	}
-    	if(target == 0){ // 找到了和为target的一个组合
+    	if (target == 0) { // 找到了和为target的一个组合
     		result.add(new ArrayList<Integer>(item)); 
     		return;
     	}
     	for (int i = pos; i < candidates.length; i++) { //i从pos开始处理，因为pos之前的我们已经处理完了
     		//这样做避免了因为重复元素而导致的重复结果（如果遇到相同元素则跳过，直到遇到不同的元素，再进行下一层递归。此处类似subsets II）
-    		if( i > pos && candidates[i] == candidates[i - 1]) {
+    		if (i > pos && candidates[i] == candidates[i - 1]) {
     			continue;
     		}
     		item.add(candidates[i]);//把当前值加入item中

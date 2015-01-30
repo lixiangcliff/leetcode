5ecf0d73ba6899cbc2c84a08f165e29e9b40da00
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-30 13:57:10 -0800

    update Max_Points_on_a_Line

--- lc/HashMap/Max_Points_on_a_Line/Question.java
+++ lc/HashMap/Max_Points_on_a_Line/Question.java
@@ -1,6 +1,5 @@
 package Max_Points_on_a_Line;
 
-import java.awt.Point;
 import java.util.HashMap;
 
 public class Question {
@@ -18,47 +17,57 @@ public class Question {
 	 * Given n points on a 2D plane, find the maximum number of points that lie
 	 * on the same straight line.
 	 */
+	
 	//http://blog.csdn.net/linhuanmars/article/details/21060933
 	//用HashMap, time: O(n^2); space O(n)
-    public int maxPoints(Point[] points) {
-        if(points == null || points.length == 0){
-        	return 0;
-        }
-        int max = 1; //【注】初始值为1!
-        for (int i = 0; i < points.length-1; i++){//i < points.length-1是因为j=i+1起始，要防止j越界
-        	//HashMap<ratio(斜率), 与当前点i构成斜率为ratio的点的个数>
-        	HashMap<Double, Integer> map = new HashMap<Double, Integer>();
-        	int numOfSame = 0;//有多少点和当前点位置重复
-        	double ratio = 0.0;
-        	int localMax = 1; //【注】初始值为1!
-        	for(int j = i+1; j < points.length; j++){
-        		//判断j点是否和i点重复；如不重复求j和i的斜率
-        		if(points[i].x == points[j].x && points[i].y == points[j].y){
-        			numOfSame++;
-        			continue;
-        		}else if (points[i].x == points[j].x){
-        			ratio = (double)Integer.MAX_VALUE;
-        		}else if (points[i].y == points[j].y){
-        			ratio = 0.0;
-        		}else{
-        			ratio = (double)(points[i].y - points[j].y) / (double)(points[i].x - points[j].x);
-        		}
-        		//看ratio是否已经在map里
-        		if(map.containsKey(ratio)){
-        			map.put(ratio, map.get(ratio)+1);
-        		}else{
-        			map.put(ratio, 2);
-        		}
-        	}
-        	//完成了对当前点i，所有与之构成直线ratio的统计，取其中value最大的作为localMax
-        	for (Integer value : map.values()){
-        		localMax = Math.max(value, localMax);
-        	}
-        	//如果有和i重复的点，也要计算上（因为ratio可以理解为相同）
-        	localMax += numOfSame;
-        	//确定了当前i的localMax，现在更新max
-        	max = Math.max(localMax, max);
-        }
-        return max;
-    }
+	public int maxPoints(Point[] points) {
+		if (points == null || points.length <= 0) {
+			return 0;
+		}
+		int max = 1; // 【注】初始值为1.因为至少两个点，且两点可以确定一条直线。
+		for (int i = 0; i < points.length - 1; i++) { // 【注】i < points.length-1是因为j从i + 1起始，到 points.length-1为止，要防止j越界
+			HashMap<Double, Integer> map = new HashMap<Double, Integer>(); //<ratio(斜率), 与当前点i构成斜率为ratio的点的个数>。每个i都需要一个新的map
+			int numOfSame = 0;// 有多少点和当前点位置重复
+			double ratio = 0.0;
+			int localMax = 1; 
+			for (int j = i + 1; j < points.length; j++) {
+				// 判断j点是否和i点重复；如不重复，则求j和i的斜率（求ratio是要特殊考虑x和y等于0时的情况）
+				if (points[i].x == points[j].x && points[i].y == points[j].y) {
+					numOfSame++;
+					continue;
+				} else if (points[i].x == points[j].x) {
+					ratio = (double) Integer.MAX_VALUE;
+				} else if (points[i].y == points[j].y) {
+					ratio = 0.0;
+				} else {
+					ratio = (double) (points[i].y - points[j].y) / (double) (points[i].x - points[j].x);
+				}
+				if (map.containsKey(ratio)) { // 看ratio是否已经在map里
+					map.put(ratio, map.get(ratio) + 1);
+				} else {
+					map.put(ratio, 2); //【注】此处点的个数为2，而不是1。因为一条直线需要两个点。
+				}
+			}
+			// 完成了对当前点i，所有与之构成直线ratio的统计，取其中value最大的作为localMax
+			for (Integer value : map.values()) {
+				localMax = Math.max(value, localMax);
+			}
+			localMax += numOfSame; // 如果有和i重复的点，也要计算上（因为ratio可以理解为相同）
+			max = Math.max(localMax, max); // 更新max
+		}
+		return max;
+	}
+}
+
+class Point {
+	int x;
+	int y;
+	Point() {
+		x = 0;
+		y = 0;
+	}
+	Point(int a, int b) {
+		x = a;
+		y = b;
+	}
 }

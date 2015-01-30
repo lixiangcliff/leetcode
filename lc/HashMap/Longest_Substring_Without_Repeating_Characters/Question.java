5d1b53fe1561ea9260ee5d1850835614c60d4b9d
lixiangcliff <lixiang.cliff@gmail.com>
2015-01-30 11:49:02 -0800

    update Longest_Substring_Without_Repeating_Characters

--- lc/HashMap/Longest_Substring_Without_Repeating_Characters/Question.java
+++ lc/HashMap/Longest_Substring_Without_Repeating_Characters/Question.java
@@ -1,6 +1,6 @@
 package Longest_Substring_Without_Repeating_Characters;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class Question {
 
@@ -9,7 +9,9 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Question q = new Question();
+		String s = "abba";
+		System.out.println(q.lengthOfLongestSubstring(s));
 	}
 	
 	/**
@@ -19,38 +21,27 @@ public class Question {
 	 * repeating letters for "abcabcbb" is "abc", which the length is 3. For
 	 * "bbbbb" the longest substring is "b", with the length of 1.
 	 */
-	//http://blog.csdn.net/linhuanmars/article/details/19949159
-	/* 思想：
-	 * 维护一个HashSet, 正常情况下移动右窗口，如果没有出现重复则继续移动右窗口，
-	 * 如果发现重复字符，则说明当前窗口中的串已经不满足要求，继续移动有窗口不可能得到更好的结果，
-	 * 此时移动左窗口，直到不再有重复字符为止
-	 * 此题很多小细节，要仔细！
-	 */
-    public int lengthOfLongestSubstring(String s) {
-        if(s==null || s.length() == 0){
-        	return 0;
-        }
-        HashSet<Character> set = new HashSet<Character>();
-        int max = 0;
-        int walker = 0;
-        int runner = 0;
-        while(runner!=s.length()){
-        	if(set.contains(s.charAt(runner))){//当前值在set中已经有了，
-        		max = Math.max(runner-walker, max);//更新max
-        		//walker持续右移，直到找到和runner当前值相等的那一个
-        		while(s.charAt(walker)!=s.charAt(runner)){
-        			set.remove(s.charAt(walker));//更新set
-        			walker++;
-        		}
-        		walker++;//相当于剔除和runner当前值相等的那一个
-        	}else{//当前值不重复，把当前值加入set
-        		set.add(s.charAt(runner));
-        	}
-        	runner++;//如论重复与否（实际上重复的情况处理完之后，相当于不再重复了），runner都要右移
-        }
-        //当runner走到尽头后，需要再判断（更新）一次max
-        max = Math.max(runner-walker, max);
-        return max;
-    }
+	
+	// http://www.cnblogs.com/yuzhangcmu/p/4188973.html
+	// 思想：hashmap和two pointer的结合。维护一个HashMap, <元素值， 元素index>。
+	// 只要还没有出现重复则持续移动右窗口，并把;
+	// 如果发现重复字符（设为c），则通过HashMap找到第一次出现c的位置(设为idx)，更新l为idx + 1；同时新HashMap：<c, r>。
+	public int lengthOfLongestSubstring(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		int max = 0;
+		int l = 0;
+		for (int r = 0; r < s.length(); r++) {
+			char c = s.charAt(r);
+			if (map.containsKey(c) && map.get(c) >= l) { //【注】如果map.get(c)<l，说明该c的index已经在左窗口的左边了，是无效的
+				l = map.get(c) + 1;
+			} 
+			map.put(c, r); // 更新c最新出现的位置
+			max = Math.max(max, r - l + 1);
+		}
+		return max;
+	}
 
 }

474a9741dd2b3582fad427497ed356c9c08fe758
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-02 16:16:12 -0800

    update Substring_with_Concatenation_of_All_Words

--- lc/HashMap/Substring_with_Concatenation_of_All_Words/Question.java
+++ lc/HashMap/Substring_with_Concatenation_of_All_Words/Question.java
@@ -10,10 +10,13 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Question q = new Question();
 		String S = "barfoothefoobarman";
 		String[] L = {"foo", "bar"};
-		ArrayList result = findSubstring(S, L);
-		for (int i=0;i<result.size();i++){
+/*		String S = "aaaaaaaa";
+		String[] L = {"aa", "aa", "aa"};*/
+		ArrayList result = q.findSubstring(S, L);
+		for (int i = 0; i < result.size(); i++) {
 			System.out.println(result.get(i));
 		}
 		
@@ -33,78 +36,41 @@ public class Question {
 	 * 
 	 * You should return the indices: [0,9]. (order does not matter).
 	 */
-	//O(n) way
-	//http://blog.csdn.net/linhuanmars/article/details/20342851
-	//此题核心在于左窗口的维护伴随着curMap的更新
-	//此题很多细节！
-	 public static ArrayList<Integer> findSubstring(String S, String[] L) {
+	
+	//http://www.cnblogs.com/yuzhangcmu/p/4114656.html
+	//"每次复制一个HashMap，找到一个单词，即减少此单词的计数，直到HashMap为空，表示我们找到一个解。"
+	public ArrayList<Integer> findSubstring(String S, String[] L) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
-		if(S == null || S.length()==0 || L == null ||L.length ==0){
-	    		return result;
-	    }
-		//把L里面所有string都放入map中，<string, 该string在L中出现的次数>
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-    	int wordNum = L.length;
-    	int wordLen = L[0].length();
-    	for(int i=0; i<wordNum; i++){
-    		if(map.containsKey(L[i])){
-    			map.put(L[i], map.get(L[i])+1);
-    		}else{
-    			map.put(L[i], 1);
-    		}
-    	}
-    	//关于为什么如下方法遍历，看图。
-    	for(int i=0;i<wordLen;i++){
-    		int count = 0;//记录在S中已经连续出现了几个符合要求的L[]中的string（符合题意的only once）
-    		int left = i;//记录左窗口位置，方便以后可能的右移
-    		HashMap<String, Integer> curMap = new HashMap<String, Integer>();//curMap要在j loop外，以为针对同一个i，不同的j都要参考同一个curMap
-    		for(int j=i;j<=S.length()-wordLen;j+=wordLen){//j<=S.length()-wordLen因为当j==S.length()-wordLen，仍然符合条件
-    			String str = S.substring(j, j+wordLen);
-    			if(map.containsKey(str)){//当前str在map中能找到
-    				//把str加入curMap
-    				if(curMap.containsKey(str)){
-    					curMap.put(str, curMap.get(str)+1);
-    	    		}else{
-    	    			curMap.put(str, 1);
-    	    		}
-    				
-    				//一旦发现str在 S中出现次数大于在 L[]中, 我们就把left右移，直到str在S和L[]中出现的次数相等
-    				if(curMap.get(str) > map.get(str)){
-    					while(curMap.get(str) > map.get(str)){
-    						//随着left右移，我们需要依次减少当前string在curMap的value（即当前string在curMap中出现的次数）
-    						String temp = S.substring(left, left+wordLen); //记录当前要减少次数的string为temp
-    						if(curMap.containsKey(temp)){//
-    							curMap.put(temp, curMap.get(temp)-1);//减少temp在curMap出现的次数
-    							//只有在temp在curMap中的次数已经小于map中，才减小count(即，如果curMap.get(temp)==map.get(temp)时，并不需要减小count )
-    							if(curMap.get(temp)<map.get(temp)){  
-    								count--;
-    							}
-    						}
-    						left += wordLen;//left右移
-    					}
-    				}else{
-    					count++;
-    				}
-    				
-    				//count == wordNum时得到结果。此结果应该在j loop中得到，而不是在j loop外。因为对同一个i，可以有不同的结果
-    				if(count == wordNum){
-    					result.add(left);// 存入当前left值【注】不是 add(i)!!!
-    					//当找到一个结果之后，left右移一个wordLen，继续寻找接下来可能的结果
-    					String temp = S.substring(left, left+wordLen);
-    					if(curMap.containsKey(temp)){//left右移，curMap必须随之更新
-    						curMap.put(temp, curMap.get(temp)-1);
-    					}
-    					count--;//刚才count是等于wordNum(也意味着curMap.get(temp)==map.get(temp)）现在右移了一次（所以必然有curMap.get(temp)<map.get(temp)），所以count必须要减少1
-    					left += wordLen;
-    				}
-    				
-    			}else{//当前str在map中找不到，所以curMap以及count要从头累计起了
-    				curMap.clear();
-    				count = 0;
-    				left = j+wordLen;//重新标记left
-    			}
-    		}
-    	}
+		if (S == null || S.length() == 0 || L == null || L.length == 0) {
+			return result;
+		}
+		// 把L里面所有string都放入map中，<string, 该string在L中出现的次数>
+		HashMap<String, Integer> aimMap = new HashMap<String, Integer>();
+		int wordLen = L[0].length();
+		for (String word: L) {
+			if (aimMap.containsKey(word)) {
+				aimMap.put(word, aimMap.get(word) + 1);
+			} else {
+				aimMap.put(word, 1);
+			}
+		}
+		for (int i = 0; i <= S.length() - wordLen * L.length; i++) { // i的边界，举例最清晰
+			HashMap<String, Integer> matchMap = new HashMap<String, Integer>(aimMap); // 从matchMap中依次减去
+			for (int j = i; j <= S.length() - wordLen; j += wordLen) { // j标记当前word的起始index, j的边界也要画图为好
+				String curWord = S.substring(j, j + wordLen);
+				if (matchMap.containsKey(curWord)) {
+					matchMap.put(curWord, matchMap.get(curWord) - 1);
+					if (matchMap.get(curWord) == 0) { //【注】如果此时curWord的数量已经为0了，则从matchMap中删去，以避免之后的混淆
+						matchMap.remove(curWord);
+					}
+				} else {
+					break;
+				}
+				if (matchMap.isEmpty()) { // matchMap里的元素已经被一一剔除了，则说明i是一个合法的起始index
+					result.add(i);
+				}
+			}
+		}
     	return result;
 	 }
 }

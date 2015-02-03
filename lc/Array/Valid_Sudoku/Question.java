ba7b3c046b33011d819cdb3e68b333293b0447fd
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-03 12:31:50 -0800

    update Valid_Sudoku

--- lc/Array/Valid_Sudoku/Question.java
+++ lc/Array/Valid_Sudoku/Question.java
@@ -40,8 +40,7 @@ public class Question {
 	 * solvable. Only the filled cells need to be validated.
 	 */
 	
-	
-	//另一个思想是，每次遍历当前加入的数，是否仍然使棋盘合法。
+	//思想是，每次遍历当前加入的数，是否仍然使棋盘合法。
 	//http://blog.csdn.net/linhuanmars/article/details/20748761
 	public boolean isValidSudoku(char[][] board) {
 		for (int i = 0; i < 9; i++) {
@@ -77,59 +76,7 @@ public class Question {
 		}
 		return true;
 	}
-	//另一个思想是，每次遍历当前加入的数，是否仍然使棋盘合法。
-	//http://blog.csdn.net/linhuanmars/article/details/20748171
-	//【注】boolean[] visited = new boolean[9]的位置不同，原则是没处理9个元素就出reset(new)一次visited
-	public boolean isValidSudoku2(char[][] board) {
-		if (board == null || board.length != 9 || board[0].length != 9) {
-			return false;
-		}
-		// rows
-		for (int i = 0; i < 9; i++) {
-			boolean[] visited = new boolean[9];
-			for (int j = 0; j < 9; j++) {
-				if (board[i][j] != '.') {
-					if (visited[(int) (board[i][j] - '1')]) {// 已经曾经被访问过了
-						return false;
-					}
-					visited[(int) (board[i][j] - '1')] = true;
-				}// 如果board[i][j] == '.' 什么也不需要做。因为'.'有几个，位置在哪里都无所谓
-			}
-		}
-		// columns
-		for (int j = 0; j < 9; j++) {
-			boolean[] visited = new boolean[9];
-			for (int i = 0; i < 9; i++) {
-				if (board[i][j] != '.') {
-					if (visited[(int) (board[i][j] - '1')]) {
-						return false;
-					}
-					visited[(int) (board[i][j] - '1')] = true;
-				}
-			}
-		}
-		// for each 3*3 square
-		// 看图！block示意图如下
-		// 0 1 2
-		// 3 4 5
-		// 6 7 8
-		for (int block = 0; block < 9; block++) {// block 从0到9 对应了九个3*3的小block
-			boolean[] visited = new boolean[9];
-			for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {// block取0,1,2时，i要取0,1,2
-				// 【注】j和i的取值方式不同！
-				for (int j = block % 3 * 3; j < block % 3 * 3 + 3; j++) {// block取0,3,6时，j要取0,1,2
-					if (board[i][j] != '.') {
-						if (visited[(int) (board[i][j] - '1')]) {
-							return false;
-						}
-						visited[(int) (board[i][j] - '1')] = true;
-					}
-				}
-			}
-		}
-		return true;
-	}
-    
-    
-
+	
+	//另一个方法是用hashset，但是时间复杂度是一样的(棋盘点数 * 3)。
+	//http://www.cnblogs.com/yuzhangcmu/p/4067608.html 
 }

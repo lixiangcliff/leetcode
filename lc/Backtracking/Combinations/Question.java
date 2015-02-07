f66194d848b0449cb23c55e1431030c9ad0ffa3c
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-06 21:13:27 -0800

    update

--- lc/Backtracking/Combinations/Question.java
+++ lc/Backtracking/Combinations/Question.java
@@ -44,19 +44,18 @@ public class Question {
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 		ArrayList<Integer> item = new ArrayList<Integer>();
 		int start = 1; // 元素的可选起始值【注】start初值并不是0
-		getCombine(result, item, n, k, start);
+		helper(result, item, n, k, start);
 		return result;
 	}
 	
-	private void getCombine(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> item, int n, int k, int start){
+	private void helper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> item, int n, int k, int start){
 		if (item.size() == k) {
 			result.add(new ArrayList<Integer>(item));
-		} else {
-			for (int i = start; i <= n; i++) {
-				item.add(i); // 加入一个符合条件的数
-				getCombine(result, item, n, k, i + 1); // i的可选数的起始值往右挪一个，然后递归.【注】下一层传入的start是i + 1, 因为当前处理的值是i（不是start + 1）
-				item.remove(item.size() - 1); // 回溯
-			}
+		} 
+		for (int i = start; i <= n; i++) {
+			item.add(i); // 加入一个符合条件的数
+			helper(result, item, n, k, i + 1); // i的可选数的起始值往右挪一个，然后递归.【注】下一层传入的start是i + 1, 因为当前处理的值是i（不是start + 1）
+			item.remove(item.size() - 1); // 回溯
 		}
 	}
 

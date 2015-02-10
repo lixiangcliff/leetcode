9e4b426a6533e6386b09c74e2379dd5153b28eec
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-10 11:31:18 -0800

    update Linked_List_Cycle

--- lc/LinkedList/Linked_List_Cycle/Question.java
+++ lc/LinkedList/Linked_List_Cycle/Question.java
@@ -16,23 +16,24 @@ public class Question {
 	 * 
 	 * Follow up: Can you solve it without using extra space?
 	 */
+	
 	//http://blog.csdn.net/linhuanmars/article/details/21200601
-    public static boolean hasCycle(ListNode head) {
-        if(head ==null || head.next == null){//head.next==null就是说只有一个node
-            return false;
-        }
-        ListNode current = head;
-        ListNode runner = head;
-        while(runner != null && runner.next !=null){
-        	current = current.next;
-        	runner = runner.next.next;
-        	if (current == runner){
-        		return true;
-        	}
-        }
-        //runner已经跑到最后了current和runner仍然没有相遇，则返回false
-        return false;
-    }
+	public boolean hasCycle(ListNode head) {
+		if (head == null || head.next == null) {// head.next==null就是说只有一个node
+			return false;
+		}
+		ListNode current = head;
+		ListNode runner = head;
+		while (runner != null && runner.next != null) {
+			current = current.next;
+			runner = runner.next.next;
+			if (current == runner) {
+				return true;
+			}
+		}
+		// runner已经跑到最后了current和runner仍然没有相遇，则返回false
+		return false;
+	}
     
 	
 

2ecc1e88cb314ad3e5ad14f3583e237607e7fd9f
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-15 21:34:09 -0800

    update Copy_List_with_Random_Pointer

--- lc/HashMap/Copy_List_with_Random_Pointer/Question.java
+++ lc/HashMap/Copy_List_with_Random_Pointer/Question.java
@@ -1,6 +1,7 @@
 package Copy_List_with_Random_Pointer;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Question {
 
@@ -21,7 +22,7 @@ public class Question {
 	 */
 	
 	//【注】画图 http://blog.csdn.net/linhuanmars/article/details/22463599
-	public RandomListNode copyRandomList(RandomListNode head) {
+	public RandomListNode copyRandomList2(RandomListNode head) {
 		HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
 		if (head == null) {
 			return null;
@@ -45,6 +46,34 @@ public class Question {
 			copyNode.random = map.get(node.random);
 			node = node.next;
 			copyNode = copyNode.next;
+		}
+		return copyHead;
+	}
+	
+	//手写版
+	public RandomListNode copyRandomList(RandomListNode head) {
+		HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+		if (head == null) {
+			return null;
+		}
+		RandomListNode copyHead = new RandomListNode(head.label);
+		map.put(head, copyHead);
+		RandomListNode copyPre = copyHead;
+		RandomListNode cur = head.next;
+		while (cur != null) {
+			RandomListNode copyCur = new RandomListNode(cur.label);// 根据cur的label，复制出一个内容一样的copyNode
+			map.put(cur, copyCur);// pair存入map
+			copyPre.next = copyCur; // 把copy出来的连上
+			copyPre = copyCur;
+			cur = cur.next;
+			
+		}
+		for (Map.Entry<RandomListNode, RandomListNode> entry : map.entrySet()) {
+			RandomListNode src = entry.getKey();
+			RandomListNode copy = entry.getValue();
+			RandomListNode srcRdm = src.random;
+			RandomListNode copyRdm = map.get(srcRdm);
+			copy.random = copyRdm;
 		}
 		return copyHead;
 	}

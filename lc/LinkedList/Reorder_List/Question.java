aad6313c53014f1ae920637a5bc6a06b6bcdde22
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-21 15:54:56 -0800

    update Reorder_List

--- lc/LinkedList/Reorder_List/Question.java
+++ lc/LinkedList/Reorder_List/Question.java
@@ -52,40 +52,40 @@ public class Question {
 	
 	//此題不需要dummy， 因为head已经确定了。
 	// http://blog.csdn.net/linhuanmars/article/details/21503215
-    public void reorderList(ListNode head) {
-    	if (head == null) {
-    		return;
-    	}
-    	ListNode walker = head;
-    	ListNode runner = head;
-    	while (runner != null && runner.next != null) {
-    		runner = runner.next.next;
-    		walker = walker.next;
-    	}
-    	ListNode head1 = head; // 不要真正地移动head，因为如果移动head，就无法用head所在位置表示该链表了
-    	ListNode head2 = walker.next; // head2为后半段的表头
-    	walker.next = null; // 把前半段和后半段分开
-    	head2 = reverse(head2);
-    	while (head1 != null && head2 != null) {
-    		ListNode next1= head1.next;
-    		ListNode next2= head2.next;
-    		head1.next = head2;
-    		head2.next = next1;
-    		head1 = next1;
-    		head2 = next2;
-    	}
-    }
+	public void reorderList(ListNode head) {
+		if (head == null) {
+			return;
+		}
+		ListNode walker = head;
+		ListNode runner = head;
+		while (runner != null && runner.next != null) {
+			runner = runner.next.next;
+			walker = walker.next;
+		}
+		ListNode head1 = head; // 不要真正地移动head，因为如果移动head，就无法用head所在位置表示该链表了
+		ListNode head2 = walker.next; // head2为后半段的表头
+		walker.next = null; // 把前半段和后半段分开
+		head2 = reverse(head2);
+		while (head1 != null && head2 != null) {
+			ListNode next1 = head1.next;
+			ListNode next2 = head2.next;
+			head1.next = head2;
+			head2.next = next1;
+			head1 = next1;
+			head2 = next2;
+		}
+	}
     
     //reverse链表要熟练掌握（一共四步）
     private ListNode reverse(ListNode head) {
     	ListNode tail = head; //【注】始终不更新cur，是因为cur指向最原始的头，它最终会变成尾。而head则一直在更新。
-    	while(tail != null && tail.next != null){
-    		ListNode next = tail.next;
-    		tail.next = next.next;
-    		next.next = head;
-    		head = next;
-    	}
-    	return head;
+		while (tail != null && tail.next != null) {
+			ListNode next = tail.next;
+			tail.next = next.next;
+			next.next = head;
+			head = next;
+		}
+		return head;
     }
 }
 

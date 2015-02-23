7569a0031244359fc34ffa6bd3fae7a95e5387f1
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-22 22:54:21 -0800

    update Same_Tree

--- lc/Tree_Nature/Same_Tree/Question.java
+++ lc/Tree_Nature/Same_Tree/Question.java
@@ -16,21 +16,20 @@ public class Question {
 	 * not. Two binary trees are considered equal if they are structurally
 	 * identical and the nodes have the same value.
 	 */
+	
 	//http://blog.csdn.net/linhuanmars/article/details/22839819
 	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if (p == null && q == null){//两个都到头了，返回true
+		if (p == null && q == null) { // 两个都到头了，返回true
 			return true;
 		}
-		if (p == null || q == null){//一个到头了，但是另一个没到头，所以不同，返回false
+		if (p == null || q == null) { // 一个到头了，但是另一个没到头，所以不同，返回false
 			return false;
 		}
-		if (p.val != q.val){//两个都不为空，但是值不相同，返回false
+		if (p.val != q.val) { // 两个都不为空，但是值不相同，返回false
 			return false;
 		}
-		//递归。这里之所以用&&，是因为这里需要：只要有一个为false，则整个为false
-		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right); // 递归。左右子树对应same，最终才能same
 	}
-
 }
 	
 class TreeNode {

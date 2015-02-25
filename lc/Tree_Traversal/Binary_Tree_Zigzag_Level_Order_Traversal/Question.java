ae823e55fe93507bb3e9d1bc01023f585615895b
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-25 12:36:55 -0800

    update Binary_Tree_Zigzag_Level_Order_Traversal

--- lc/Tree_Traversal/Binary_Tree_Zigzag_Level_Order_Traversal/Question.java
+++ lc/Tree_Traversal/Binary_Tree_Zigzag_Level_Order_Traversal/Question.java
@@ -2,6 +2,7 @@ package Binary_Tree_Zigzag_Level_Order_Traversal;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 
 public class Question {
@@ -35,57 +36,55 @@ public class Question {
 		]
 
 	 */
-	//类似https://oj.leetcode.com/problems/binary-tree-level-order-traversal/
-	//http://blog.csdn.net/linhuanmars/article/details/24509105
+	
+	//类似https://oj.leetcode.com/problems/binary-tree-level-order-traversal/ 不同之处在于由于每层遍历的方向不同，所以不能简单用queue来存node了。
 	//http://www.ninechapter.com/solutions/binary-tree-zigzag-level-order-traversal/ 
-	//ninechapter代码更简练，因为处理当前行是node值（即把node加入item），而linhuan处理的是下一行（增加了很多item.add()的代码）
-	//偶数层自左向右（正序），而奇数层自右向左（逆序）。(根为第0层)
-    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
-    	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-    	if(root == null){
-    		return result;
-    	}
-    	
-    	LinkedList<TreeNode> curStack = new LinkedList<TreeNode>();
-    	LinkedList<TreeNode> nextStack = new LinkedList<TreeNode>();
-    	LinkedList<TreeNode> temp;
-    	curStack.push(root);
-    	boolean normalOrder = true;//即偶数层，即自左向右
-    	while(!curStack.isEmpty()){
-    		ArrayList<Integer> item = new ArrayList<Integer>();
-    		while(!curStack.isEmpty()){
-    			TreeNode node = curStack.pop();
-        		item.add(node.val);//当前node值加入item
-        		//当前为偶数层，把下一层按正序压入栈。然后这一层处理完之后，处理下一层时，奇数层就能按逆序弹出栈，依次加入item中
-	    		if(normalOrder){
-	    			if(node.left != null){
-	    				nextStack.push(node.left);
-	    			}
-	    			if(node.right != null){
-	    				nextStack.push(node.right);
-	    			}
-	    		}else{//奇数层，和偶数层正好相反
-	    			if(node.right != null){
-	    				nextStack.push(node.right);
-	    			}
-	    			if(node.left != null){
-	    				nextStack.push(node.left);
-	    			}
-	    		}
-    		}
-    		//swap stack
-    		//意图是：最开始时候curStack里面有node，而nextStack里没有。随着处理，curStack里的node依次被弹出，而nextStack依次被压入这些node的孩子们。
-    		//直到curStack为空，即curStack里面的node都处理完了。而nextStack里现在压入了原来curStack的孩子们。
-    		//接下来该处理nextStack了，就把curStack和nextStack的指针交换一下，重新让curStack装着node，而nextStack为空。
-    		temp = curStack;
-    		curStack = nextStack;
-    		nextStack = temp;
-    		
-    		normalOrder = !normalOrder;//翻转normalOrder的boolean值
-    		result.add(item);
-    	}
-    	return result;
-    }
+	//此题需要两个stack(curStack和nextStack)分别存当前层node以及下一层的node。其中nextStack存的即是curStack所有node的孩子们
+	//而nextStack是按照何种方式入栈，取决于当前层的奇偶性： 偶数层自左向右（正序），而奇数层自右向左（逆序）。(根为第0层)
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		if (root == null) {
+			return result;
+		}
+		LinkedList<TreeNode> curStack = new LinkedList<TreeNode>();
+		LinkedList<TreeNode> nextStack = new LinkedList<TreeNode>();
+		LinkedList<TreeNode> temp;
+		curStack.push(root);
+		boolean normalOrder = true;// 表示当前在第偶数层，即正序(自左向右)
+		while (!curStack.isEmpty()) {
+			List<Integer> item = new ArrayList<Integer>(); // 存本层内容
+			while (!curStack.isEmpty()) {
+				TreeNode node = curStack.pop();
+				item.add(node.val);// 当前node值加入item
+				// 当前为偶数层，把当前node的孩子按正序压入下一层的栈(nextStack)。(这样这一层处理完之后，处理下一层时，奇数层就能按逆序弹出栈，依次加入item中)
+				if (normalOrder) {
+					if (node.left != null) {
+						nextStack.push(node.left);
+					}
+					if (node.right != null) {
+						nextStack.push(node.right);
+					}
+				} else {// 奇数层，和偶数层正好相反
+					if (node.right != null) {
+						nextStack.push(node.right);
+					}
+					if (node.left != null) {
+						nextStack.push(node.left);
+					}
+				}
+			}
+			// swap stack。意图是：最开始时候curStack里面有node，而nextStack里没有。随着处理，curStack里的node依次被弹出并存入item，而nextStack依次被压入这些node的孩子们。
+			// 直到curStack为空，即curStack里面的node都处理完了。而nextStack里现在压入了原来curStack的所有孩子们。
+			// 接下来该处理nextStack了，就把curStack和nextStack的指针交换一下，重新让curStack装着node，而nextStack为空。
+			temp = curStack;
+			curStack = nextStack;
+			nextStack = temp;
+			
+			normalOrder = !normalOrder;// 勿忘翻转normalOrder的boolean值
+			result.add(item);
+		}
+		return result;
+	}
 }
 
 

c9da42d56a3aa44a081f9b008f7520814d959175
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-26 23:15:17 -0800

    update Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal

--- lc/Tree_Construct/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/Question.java
+++ lc/Tree_Construct/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/Question.java
@@ -19,36 +19,35 @@ public class Question {
 	 * tree.
 	 * Note: You may assume that duplicates do not exist in the tree.
 	 */
+	
 	//http://blog.csdn.net/linhuanmars/article/details/24390157
 	//此题和Construct Binary Tree from Preorder and Inorder Traversal，非常类似。唯一区别是后序排列根在最后。
 	//具体注释参考那一题即可
 	//还有一个注意点：
 	//"有朋友可能会想根据先序遍历和后序遍历能不能重新构造出树来，答案是否定的。只有中序便利可以根据根的位置切开左右子树，其他两种遍历都不能做到，
 	//其实先序遍历和后序遍历是不能唯一确定一棵树的，会有歧义发生，也就是两棵不同的树可以有相同的先序遍历和后序遍历，"
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(inorder == null || postorder == null){
-        	return null;
-        }
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i=0; i<inorder.length;i++){
-        	map.put(inorder[i], i);
-        }
-        return helper(postorder, 0, postorder.length-1, inorder, 0, inorder.length-1, map);
-        
-    }
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		if (inorder == null || postorder == null) {
+			return null;
+		}
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < inorder.length; i++) {
+			map.put(inorder[i], i);
+		}
+		return helper(postorder, 0, postorder.length - 1, inorder, 0,
+				inorder.length - 1, map);
+	}
     
-    private TreeNode helper(int[] postorder, int postL, int postR, int[] inorder, int inL, int inR, HashMap<Integer, Integer> map){
-    	//cannot be missed!!!
-    	if(postL>postR || inL>inR){
-    		return null;
-    	}
-    	int index = map.get(postorder[postR]);
-    	TreeNode root = new TreeNode(postorder[postR]);
-    	root.left = helper(postorder, postL, postL-1+index-inL, inorder, inL, index-1, map); //  new postL or postR should has some relationship 
-    	root.right = helper(postorder, postR-inR+index, postR-1, inorder, index+1, inR, map); // with previous postL or postR
-    	return root;
+    private TreeNode helper(int[] postorder, int postL, int postR, int[] inorder, int inL, int inR, HashMap<Integer, Integer> map) {
+		if (postL > postR || inL > inR) { // base case
+			return null;
+		}
+		int index = map.get(postorder[postR]);
+		TreeNode root = new TreeNode(postorder[postR]);
+		root.left = helper(postorder, postL, postL - 1 + index - inL, inorder, inL, index - 1, map);
+		root.right = helper(postorder, postR - inR + index, postR - 1, inorder, index + 1, inR, map);
+		return root;
     }
-
 }
 
 class TreeNode {

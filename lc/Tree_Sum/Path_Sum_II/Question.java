0106d1cc1255542a2c6527a9446b4cc1373dfaca
lixiangcliff <lixiang.cliff@gmail.com>
2015-02-26 20:50:22 -0800

    update Path_Sum_II

--- lc/Tree_Sum/Path_Sum_II/Question.java
+++ lc/Tree_Sum/Path_Sum_II/Question.java
@@ -34,7 +34,7 @@ public class Question {
 		]
 		
 	 */
-	//看【注】里的问题
+	
 	//http://www.ninechapter.com/solutions/path-sum-ii/
 	//http://blog.csdn.net/linhuanmars/article/details/23655413
     public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
@@ -47,6 +47,7 @@ public class Question {
     	return result;
     }
     
+    //backtracking
     private void helper(TreeNode root, int sum, ArrayList<Integer> item, ArrayList<ArrayList<Integer>> result) {
     	if (root == null) {
     		return;
@@ -56,18 +57,15 @@ public class Question {
     		if (sum == 0) { //刚好该叶子节点的值等于上次剩余的sum值
     			item.add(root.val); //当前叶子node加入item，使其成为solution之一
     			result.add(new ArrayList<Integer>(item));
-    			item.remove(item.size() - 1); //【注】操作完叶子节点之后为什么要做这一步？
+    			item.remove(item.size() - 1); //backtrack
     		}
     	}
     	//如果root不为叶子，则把当前root.val加入item，然后对左右子树 分别递归。
     	item.add(root.val);
     	helper(root.left, sum, item, result);
     	helper(root.right, sum, item, result);
-    	item.remove(item.size() - 1); //恢复调用递归前的现场
+    	item.remove(item.size() - 1); //backtrack
     }
-    
-    
-
 }
 
 class TreeNode {

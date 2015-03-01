8af078e7ea4fcdf95aa94f6d7674a33a9a59e9ab
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-01 11:07:25 -0800

    update Path_Sum_II

--- lc/Tree_Sum/Path_Sum_II/Question.java
+++ lc/Tree_Sum/Path_Sum_II/Question.java
@@ -40,7 +40,7 @@ public class Question {
     public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
     	ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
     	if (root == null){
-    		return null;
+    		return result;
     	}
     	ArrayList<Integer> item = new ArrayList<Integer>();
     	helper(root, sum, item, result);
@@ -52,18 +52,15 @@ public class Question {
     	if (root == null) {
     		return;
     	}
-    	sum -= root.val; //sum减去当前root.val之后的新sum值
-    	if (root.left == null && root.right == null) { //root为叶子node
-    		if (sum == 0) { //刚好该叶子节点的值等于上次剩余的sum值
-    			item.add(root.val); //当前叶子node加入item，使其成为solution之一
-    			result.add(new ArrayList<Integer>(item));
-    			item.remove(item.size() - 1); //backtrack
-    		}
+    	if (root.left == null && root.right == null && sum == root.val) { //root为叶子node
+			item.add(root.val); // 当前叶子node加入item，使其成为solution之一
+			result.add(new ArrayList<Integer>(item));
+			item.remove(item.size() - 1); //backtrack
     	}
-    	//如果root不为叶子，则把当前root.val加入item，然后对左右子树 分别递归。
+    	//如果root不为叶子，则把当前root.val加入item，然后对左右子树 分别递归，最后再backtrack。
     	item.add(root.val);
-    	helper(root.left, sum, item, result);
-    	helper(root.right, sum, item, result);
+    	helper(root.left, sum - root.val, item, result);
+    	helper(root.right, sum - root.val, item, result);
     	item.remove(item.size() - 1); //backtrack
     }
 }

9d84774c82d45219033e542b0e81d7f8ffde2e05
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-03 10:04:22 -0800

    update Insert_Node_in_a_Binary_Search_Tree

--- lc/Tree/Insert_Node_in_a_Binary_Search_Tree/Solution.java
+++ lc/Tree/Insert_Node_in_a_Binary_Search_Tree/Solution.java
@@ -9,23 +9,42 @@ public class Solution {
 		// TODO Auto-generated method stub
 
 	}
+	/**
+	 * http://lintcode.com/en/problem/insert-node-in-a-binary-search-tree/
+	 * Given a binary search tree  and a new tree node, insert the node into the tree. You should keep the tree still be a valid binary search tree.
+		Example 
+		Given binary search tree as follow:
+		     2
+		    /  \
+		   1     4
+		        /   
+		       3 
+		after Insert node 6, the tree should be:
+		     2
+		    /  \
+		   1    4
+		        / \ 
+		       3   6
+		Challenge 
+		Do it without recursion
+
+	 */
 	
-    /**
-     * @param root: The root of the binary search tree.
-     * @param node: insert this node into the binary search tree
-     * @return: The root of the new binary search tree.
-     */
+	// @return: The root of the new binary search tree.
     public TreeNode insertNode(TreeNode root, TreeNode node) {
         // write your code here
     	if (root == null) {
     		return node;
     	}
+    	if (node == null) {
+    		return root;
+    	}
     	TreeNode cur = root;
     	while (cur != null) {
     		if (cur.val < node.val) {
-    			if (cur.right == null) {
+    			if (cur.right == null) { // 如果已经走到子树的最右
     				cur.right = node;
-    				return cur;
+    				return root;
     			} 
     			cur = cur.right;
     		} else if (cur.val > node.val) {

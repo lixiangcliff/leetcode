496215eff413836c7305703578e33f5a469f5a5e
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-02 17:17:09 -0800

    update Lowest_Common_Ancestor

--- lc/Tree/Lowest_Common_Ancestor/Solution.java
+++ lc/Tree/Lowest_Common_Ancestor/Solution.java
@@ -7,7 +7,18 @@ public class Solution {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Solution s = new Solution();
+		TreeNode n4 = new TreeNode(4);
+		TreeNode n3 = new TreeNode(3);
+		TreeNode n7 = new TreeNode(7);
+		TreeNode n5 = new TreeNode(5);
+		TreeNode n6 = new TreeNode(6);
+		n4.left = n3;
+		n4.right = n7;
+		n7.left = n5;
+		n5.left = n6;
+		System.out.println(s.lowestCommonAncestor(n4, n3, n6).val);
+		
 	}
 	
     /**
@@ -15,26 +26,41 @@ public class Solution {
      * @param A and B: two nodes in a Binary.
      * @return: Return the least common ancestor(LCA) of the two nodes.
      */
+	/*
+	 * Given the root and two nodes in a Binary Tree. Find the lowest common ancestor(LCA) of the two nodes.
+		The lowest common ancestor is the node with largest depth which is the ancestor of both nodes.
+		Example
+		        4
+		       /  \
+		      3    7
+		          / \
+		        5    6
+		
+		For 3 and 5, the LCA is 4.
+		For 5 and 6, the LCA is 7.
+		For 6 and 7, the LCA is 7.
+	 */
+	
+	//http://blog.csdn.net/v_july_v/article/details/18312089
 	//返回值即为最小公共祖先
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
-        // root已经 为null或者碰到了A，B其中某一个点
-    	if (root == null || root == A || root == B) {
+    	if (root == null || root == A || root == B) { // base case。root已经 为null或者碰到了A，B其中某一个点
     		return root;
     	}
     	//divide
     	TreeNode left = lowestCommonAncestor(root.left, A, B);
     	TreeNode right = lowestCommonAncestor(root.right, A, B);
     	//conquer
-    	if (left != null && right != null) { //即，左右子树中分别有A和B中的一个，也就是找到了最小公共祖先
+    	if (left != null && right != null) { //【注】即，left和right分别对应的是A和B(或B和A)，也就是找到了最小公共祖先
     		return root;
     	}
-    	if (left != null) { //即AB都在左子树
+    	if (left != null) { //即AB都在左子树（即right == null）
     		return left;
     	}
-    	if (right != null) { //即AB都在右子树
+    	if (right != null) { //即AB都在右子树（即left == null）
     		return right;
     	} 
-    	return null; //两个子树都没有 则返回null
+    	return null; // 两个子树都没有 A和B的LCA，则返回null
     }
 
 }

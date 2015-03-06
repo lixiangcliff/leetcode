bd1182c20699bf6e9f00232271bc6d9fb5cdbc30
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-05 22:55:26 -0800

    update Rotate_String

--- lc/String/Rotate_String/Solution.java
+++ lc/String/Rotate_String/Solution.java
@@ -1,7 +1,5 @@
 package Rotate_String;
 
-import java.util.ArrayList;
-
 public class Solution {
 
 	/**
@@ -12,30 +10,36 @@ public class Solution {
 
 	}
 
-	/*
-	 * 【注】index vs offset
-	 * index是从前往后数
-	 * offset是从后往前数
+	/**
+	 * http://lintcode.com/en/problem/rotate-string/
+	 * Given a string and an offset, rotate string by offset. (rotate from left to right)
+		Example
+		Given "abcdefg"
+		for offset=0, return "abcdefg"
+		for offset=1, return "gabcdef"
+		for offset=2, return "fgabcde"
+		for offset=3, return "efgabcd"
 	 */
-    String rotateString(String A, int offset) {
-        // wirte your code here
-    	if (A == null || A.length() == 0){
-    		return "";
-    	}
-    	String left = reverse(A, 0, A.length() - offset % A.length() - 1); //【注】此处要取模，因为要防止给的offset比length还大。
-    	String right = reverse(A, A.length() - offset % A.length(), A.length() - 1);
-    	String str = left + right;
-    	return reverse(str, 0, A.length() - 1);
-    }
-    
-    private String reverse (String str, int start, int end) {
-    	if (start < 0 || end > str.length() - 1){
-    		return "";
-    	}
-    	StringBuilder sb = new StringBuilder();
-    	for (int i = str.length() - 1; i>= 0; i--){
-    		sb.append(str.charAt(i));
-    	}
-    	return sb.toString();
-    }
+	
+	//【注】index vs offset: index是从前往后数；offset是从后往前数
+	String rotateString(String A, int offset) {
+		if (A == null || A.length() == 0) {
+			return "";
+		}
+		String left = reverse(A, 0, A.length() - offset % A.length() - 1); // 【注】此处要取模，因为要防止给的offset比length还大。
+		String right = reverse(A, A.length() - offset % A.length(), A.length() - 1);
+		String str = left + right;
+		return reverse(str, 0, A.length() - 1);
+	}
+
+	private String reverse(String str, int start, int end) {
+		if (start < 0 || end > str.length() - 1) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = str.length() - 1; i >= 0; i--) {
+			sb.append(str.charAt(i));
+		}
+		return sb.toString();
+	}
 }

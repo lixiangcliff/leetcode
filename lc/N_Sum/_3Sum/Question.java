71201fa05d01ca21bab5c5c23fdce2f06d536a32
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-06 21:44:54 -0800

    update _3Sum

--- lc/N_Sum/_3Sum/Question.java
+++ lc/N_Sum/_3Sum/Question.java
@@ -38,19 +38,18 @@ public class Question {
 	 * (-1, 0, 1) 
 	 * (-1, -1, 2)
 	 */
-	// two pointers; O(1) Space, O(nlogn) Time;
+	
+	// two pointers; O(n) Space, O(n^2) Time;
 	// http://www.ninechapter.com/solutions/3sum/
 	// http://blog.csdn.net/linhuanmars/article/details/19711651
 	public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		if (num == null || num.length <= 2){
+		if (num == null || num.length <= 2) {
 			return result;
 		}
-		//先排序！
-		Arrays.sort(num);
-		for(int i = 0; i < num.length - 2; i++) { // i最远可以取到倒数第三个
-			//略过和num[i - 1]值相同的所有元素
-			if (i > 0 && num[i] == num[i - 1]) {
+		Arrays.sort(num); // 先排序！
+		for (int i = 0; i < num.length - 2; i++) { // i最远可以取到倒数第三个
+			if (i > 0 && num[i] == num[i - 1]) { // 略过和num[i - 1]值相同的所有元素
 				continue;
 			}
 			int l = i + 1;
@@ -63,11 +62,9 @@ public class Question {
 					item.add(num[l]);
 					item.add(num[r]);
 					result.add(item);
-					// 【注】做l++及r--是因为，以后的结果一定不会再用l或r了（题目要求结果集不重复）
-					l++;
+					l++; // 【注】做l++及r--是因为，以后的结果一定不会再用l或r了（题目要求结果集不重复）
 					r--;
-					// 剔除重复的
-					// 【注】此处应该为while而不是if,因为要一直删去素有重复的！
+					// 剔除重复的【注】此处应该为while而不是if,因为要一直删去素有重复的！
 					while (l < r && num[l] == num[l - 1]) {// 【注】之所以不是num[l] == num[l+1]，是因为l刚++过，所以l是当前位子，而l-1才是上一个
 						l++;
 					}

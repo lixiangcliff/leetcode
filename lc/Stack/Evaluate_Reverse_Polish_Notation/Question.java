1c81a4b805ecf63604b7b30ba478f06e1a054fd2
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-08 22:08:54 -0700

    update Evaluate_Reverse_Polish_Notation

--- lc/Stack/Evaluate_Reverse_Polish_Notation/Question.java
+++ lc/Stack/Evaluate_Reverse_Polish_Notation/Question.java
@@ -9,8 +9,9 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Question q = new Question();
 		String[] tokens = {"4", "13", "5", "/", "+"};
-		System.out.println(evalRPN(tokens));
+		System.out.println(q.evalRPN(tokens));
 	}
 	
 	/**
@@ -23,55 +24,34 @@ public class Question {
 	 * ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9 
 	 * ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
 	 */
-	//http://blog.csdn.net/linhuanmars/article/details/21058857
-    public static int evalRPN(String[] tokens) {
-    	if(tokens == null || tokens.length == 0){
-    		return 0;
-    	}
-        LinkedList<Integer> stack = new LinkedList<Integer>();
-    	/*
-    	 * 加法和乘法比较直接
-    	 * 减法和除法有trick。尤其除法要把弹出的两个数先存起来，再做运算。
-    	 * 每次pop之前，应该先判断stack是否为空，如果为空则报错。
-    	 */
-        for(int i=0;i<tokens.length;i++){
-        	if (!tokens[i].equals("+") && !tokens[i].equals("-") && !tokens[i].equals("*") && !tokens[i].equals("/")){
-        		stack.push(Integer.parseInt(tokens[i]));
-        	}else{
-        		int num1 = Integer.MAX_VALUE;
-        		int num2 = Integer.MAX_VALUE;
-        		if (!stack.isEmpty()){
-        			num1 = stack.pop();
-        		}else{
-        			return Integer.MAX_VALUE;
-        		}
-        		if (!stack.isEmpty()){
-        			num2 = stack.pop();
-        		}else{
-        			return Integer.MAX_VALUE;
-        		}
-        		
-            	if(tokens[i].equals("+")){
-            		stack.push(num1+num2);
-            	}else if(tokens[i].equals("-")){
-            		stack.push(num2-num1);
-            	}else if(tokens[i].equals("*")){
-            		stack.push(num1*num2);
-            	}else if(tokens[i].equals("/")){
-            		stack.push(num2/num1);
-            	}
-        	}
-        }
-        
-        int result = Integer.MAX_VALUE;
-        if(!stack.isEmpty()){
-        	result = stack.pop();
-        }
-        if(stack.isEmpty()){
-        	return result;
-        }else{//如果栈内还剩元素，则异常
-        	return Integer.MAX_VALUE;
-        }
-    }
+	
+	// http://www.ninechapter.com/solutions/evaluate-reverse-polish-notation/
+	// http://www.cnblogs.com/yuzhangcmu/p/4124870.html
+	//【注】 严谨的话，每次pop()之前都要先检查stack是否empty
+	public int evalRPN(String[] tokens) {
+		if (tokens == null || tokens.length == 0) {
+			return 0;
+		}
+		LinkedList<Integer> stack = new LinkedList<Integer>();
+		String opers = "+-*/";
+		for (String token : tokens) {
+			if (!opers.contains(token)) {
+				stack.push(Integer.parseInt(token));
+			} else {
+				int num2 = stack.pop();
+				int num1 = stack.pop();
+				if (token.equals("+")) {
+					stack.push(num1 + num2);
+				} else if (token.equals("-")) {
+					stack.push(num1 - num2);
+				} else if (token.equals("*")) {
+					stack.push(num1 * num2);
+				} else if (token.equals("/")) {
+					stack.push(num1 / num2);
+				}
+			}
+		}
+		return stack.pop();
+	}
     
 }

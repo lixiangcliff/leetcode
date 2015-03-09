308225c8277437606f4c3e0526f728eda608d351
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-08 21:19:28 -0700

    regroup Container_With_Most_Water

--- lc/Two_Pointers/Container_With_Most_Water/Question.java
+++ lc/Two_Pointers/Container_With_Most_Water/Question.java
@@ -23,31 +23,29 @@ public class Question {
 	 * 
 	 * Note: You may not slant the container.
 	 */
+	
 	//idea from http://answer.ninechapter.com/solutions/container-with-most-water/
 	// for any i, the maxium area will be the farthest j that has a[j] > a[i];
 	//that's why we start "left" from 0 and "right" from length-1
 	// http://blog.csdn.net/wzy_1988/article/details/17248209
-	/*
-	 * 思想：每次计算面积时的bottleneck是左右两个"侧边"中较低的那一个。
-	 * 假如左边的低，那么就把左边向右移动。这是因为右边向左移动只会使面积更小。
-	 * (面积=底边*min(左，右)，右边向左的话，底边变小，而min(左,右)不变)
-	 */
+	// 思想：每次计算面积时的bottleneck是左右两个"侧边"中较低的那一个。
+	// 假如左边的低，那么就把左边向右移动。这是因为右边向左移动只会使面积更小。
+	// (面积=底边*min(左，右)，右边向左的话，底边变小，而min(左,右)不变)
 	public static int maxArea(int[] height) {
-		if (height== null || height.length < 2){
+		if (height == null || height.length < 2) {
 			return 0;
 		}
-    	int max = 0;
-    	int left = 0;
-    	int right = height.length-1;
-    	while (left < right){
-    		max = Math.max(max, Math.min(height[left], height[right])*(right-left) );
-    		//means left is the smaller one--the bottleneck, we want to move to right trying to find a bigger left, to increase bottleneck
-    		if (height[left] < height[right]){ 
-    			left++;
-    		} else{
-    			right--;
-    		}
-    	}
-        return max; 
-    }
+		int max = 0;
+		int left = 0;
+		int right = height.length - 1;
+		while (left < right) {
+			max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+			if (height[left] < height[right]) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		return max;
+	}
 }

7aebed750292c917eed8ce5e19b6c99b22efb08d
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-10 14:53:24 -0700

    update Valid_Parentheses

--- lc/Stack/Valid_Parentheses/Question.java
+++ lc/Stack/Valid_Parentheses/Question.java
@@ -9,7 +9,8 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(isValid("([)]"));
+		Question q = new Question();
+		System.out.println(q.isValid("([)]"));
 	}
 	
 	/**
@@ -21,33 +22,31 @@ public class Question {
 	 * valid but "(]" and "([)]" are not.
 	 */
 	//http://blog.csdn.net/linhuanmars/article/details/19800789
-    public static boolean isValid(String s) {
-    	if (s == null || s.length() == 0 || s.length() %2 ==1){
-    		return false;
-    	}
-    	
-    	String left  = "([{";
-    	String right = ")]}";
-    	Stack<Character> stack = new Stack<Character>();
-    	for(int i=0;i<s.length();i++){
-    		char curChar = s.charAt(i);
-    		if(left.indexOf(curChar) > -1){//如果是左括号就入栈
-    			stack.push(curChar);
-    		}else if(right.indexOf(curChar) > -1){//如果是右括号，就来试着匹配栈顶的左括号
-    			if (stack.isEmpty()){//如果栈已经为空，则异常
-    				return false;
-    			}else{
-    				char preChar = stack.pop();
-    				if(left.indexOf(preChar) != right.indexOf(curChar)){//不匹配， 则false
-    					return false;
-    				}
-    			}
-    		}else{//当前char既不是左括号， 也不是右括号， 则false
-    			return false;
-    		}
-    	}
-    	//如果栈内什么都没剩下, 则true
-    	return stack.isEmpty();
-    }
+	public boolean isValid(String s) {
+		if (s == null || s.length() == 0 || s.length() % 2 == 1) {
+			return false;
+		}
+		String left = "([{";
+		String right = ")]}";
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			char curChar = s.charAt(i);
+			if (left.indexOf(curChar) > -1) {// 如果是左括号就入栈
+				stack.push(curChar);
+			} else if (right.indexOf(curChar) > -1) {// 如果是右括号，就来试着匹配栈顶的左括号
+				if (stack.isEmpty()) {// 如果栈已经为空，则异常
+					return false;
+				} else {
+					char preChar = stack.pop();
+					if (left.indexOf(preChar) != right.indexOf(curChar)) {// 不匹配，  则false
+						return false;
+					}
+				}
+			} else {// 当前char既不是左括号， 也不是右括号， 则false
+				return false;
+			}
+		}
+		return stack.isEmpty(); // 如果栈内什么都没剩下, 则true
+	}
 
 }

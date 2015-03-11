31cde3da10accab3a6e693cab6691707ea709ad9
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-11 12:36:17 -0700

    update Sort_Colors

--- lc/Sort/Sort_Colors/Question.java
+++ lc/Sort/Sort_Colors/Question.java
@@ -35,31 +35,32 @@ public class Question {
 	 * 
 	 * Could you come up with an one-pass algorithm using only constant space?
 	 */
+	
 	//http://www.cnblogs.com/yuzhangcmu/p/4048668.html
 	//思想类似partition array（分为两堆），此题是分为三堆
 	//partition array用一个标记k，将数组分成两堆，一堆<k，另一堆>=k
 	//类似地，此题用两个位置标记l和r，将数组分成三堆，l位之前的（包括l位）的数都为0；r位之后的（包括r位）的数都为2；l和r之间的都为1。
-	//【注】 与右边交换之后，i不能移动，因为你有可能交换过来是1或是0，还需要与左边交换。而与左边交换后，i就可以向右边移动了。
+	//【注】 与右边交换之后，i不能移动，因为你有可能交换过来是1或是0（如果是0，还需要与左边交换）。而与左边交换后，i就可以向右边移动了。
     public void sortColors(int[] A) {
         if(A == null || A.length == 0){
         	return;
         }
-        int l = 0; // 表示第一个不是0的index
-        int r = A.length - 1; // 表示第一个不是2的index
-        for (int i = 0; i <= r; i++) { // 【注】要使用cur <= right作为边界值。因为right 指向的是未判断的值。所以当i == right时，此值仍然需要继续判断。
+        int l = 0; // l的左边全是0（即0的右边缘）
+        int r = A.length - 1; // r的右边全是2
+        for (int i = 0; i <= r; i++) { // 【注】要使用i <= r作为边界值。因为right 指向的是未判断的值。所以当i == r时，此值仍然需要继续判断。
         	if (A[i] == 0) {
         		if (A[l] != 0) {
         			swap(A, l, i);
         		}
-        		l++; //【注】，只要找到一个0，l就要往右挪一个
-        		//【注】i不需停留，可以直接i++；因为从左边换过来的只可能是1，所有的2全部换到右边去了。
+        		l++; // 只要遇到一个0，l就往右挪一个以便多标记一个0出来（这是因为即使原来A[l]不为0，但是经过swap之后也为0了，需要重新标记0的右边缘了）
+        		//【注】i不需停留，可以直接i++；因为从左边换过来的只可能是1，所有的2全部换到右边去了。（因为i是从左往右遍历的）
         	} else if (A[i] == 2) {
         		if (A[r] != 2) {
         			swap(A, i, r);
         		}
         		r--;
-        		i--; //【注】i需停留，因为换过来的有可能是0，也有可能是1，所以i要停留
-        	}
+        		i--; //【注】i需停留，因为换过来的有可能是0，所以i要停留来处理掉这个0
+        	} 
         }
         return;
     }

61a2d76885c0a1fb88733b2466631f130863d276
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-12 17:33:58 -0700

    update Largest_Rectangle_in_Histogram

--- lc/Stack/Largest_Rectangle_in_Histogram/Question.java
+++ lc/Stack/Largest_Rectangle_in_Histogram/Question.java
@@ -30,6 +30,7 @@ public class Question {
 	 * 
 	 * For example, Given height = [2,1,5,6,2,3], return 10.
 	 */
+	
 	//Ma老师讲的思路：
 	//O(n)。看图！ 以每个元素值为高，左边第一个比它的值小的元素index作为左端，右边第一个比它的值小的元素index作为右端所组成的矩形。然后遍历所有元素，得到最大面积值。
 	//对于求左右端的办法，就是用stack：（通过每次处理height中的元素，来对stack进行push和pop操作来得到以每个元素为高的最大矩形面积）
@@ -43,19 +44,15 @@ public class Question {
     	}
     	LinkedList<Integer> stack = new LinkedList<Integer>();// 【注】stack中存的是各个元素的index
     	int max = 0;
-		for (int i = 0; i <= height.length; i++) { // i <= height小技巧，因为要在i==height处虚拟地再补一个“0”高度的元素，来把最后stack里的值都pop出来(如果stack还有值的话)
-			int rightIndex = i;
-			int rightHeight = i == height.length ? 0 : height[i]; // curHeight = 0的情况， 即上面说的小技巧
-			while (!stack.isEmpty()) {
-				if (rightHeight < height[stack.peek()]) { //说明当前i就是此时stack的栈顶元素的右端，可以将其pop出来并且计算以pop元素为高的最大矩形面积了
-					int popIndex = stack.pop();
-					int leftIndex = stack.isEmpty() ? -1 : stack.peek();
-					max = Math.max(max, (rightIndex - leftIndex - 1) * height[popIndex]); // 此处面积公式要分清：左端，右端和高分别是什么。
-				} else { // 即 如果当前i位置的元素值如果大于等于栈顶值，则直接跳出循环然后把该index压入栈
-					break; 
-				}
+		for (int i = 0; i <= height.length; i++) { // i <= height小技巧，因为要在i==height.length处虚拟地再补一个“0”高度的元素，来把最后stack里的值都pop出来(如果stack还有值的话)
+			int rightIdx = i;
+			int rightH = i == height.length ? 0 : height[i]; // curH = 0的情况， 即上面说的小技巧
+			while (!stack.isEmpty() && rightH < height[stack.peek()]) { //说明当前i就是此时stack的栈顶元素的右端，可以将其pop出来并且计算以pop元素为高的最大矩形面积了) 
+				int curH = height[stack.pop()];
+				int leftIdx = stack.isEmpty() ? -1 : stack.peek(); // 【注】当stack为空时，leftIdx值为-1 而不是0
+				max = Math.max(max, (rightIdx - leftIdx - 1) * curH); // 此处面积公式要分清：左端，右端和高分别是什么。
 			}
-			stack.push(i); // 无论如何最后都要把当前i压入栈
+			stack.push(rightIdx); // 无论如何最后都要把当前i压入栈
 		}
     	return max;
     }

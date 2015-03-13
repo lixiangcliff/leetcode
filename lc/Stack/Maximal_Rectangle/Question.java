62109b872ed9f7560bcdb5324fa900e2d06f41a3
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-12 17:33:45 -0700

    update Maximal_Rectangle

--- lc/Stack/Maximal_Rectangle/Question.java
+++ lc/Stack/Maximal_Rectangle/Question.java
@@ -47,51 +47,22 @@ public class Question {
 	}
 
 	// from https://oj.leetcode.com/problems/largest-rectangle-in-histogram/
-	private int largestRectangleArea(int[] height) {
+    public int largestRectangleArea(int[] height) {
     	if (height == null || height.length == 0) {
     		return 0;
     	}
     	LinkedList<Integer> stack = new LinkedList<Integer>();// 【注】stack中存的是各个元素的index
     	int max = 0;
-    	// i <= height小技巧，因为要在i==height处虚拟地再补一个“0”高度的元素，来把最后stack里的值都pop出来(如果stack还有值的话)
-		for (int i = 0; i <= height.length; i++) {
-			int rightIndex = i;
-			int rightHeight = i == height.length ? 0 : height[i]; // curHeight = 0的情况， 即上面说的小技巧
-			while (!stack.isEmpty()) {
-				if (rightHeight < height[stack.peek()]) { //说明当前i就是此时stack的栈顶元素的右端，可以将其pop出来并且计算以pop元素为高的最大矩形面积了
-					int popIndex = stack.pop();
-					int leftIndex = stack.isEmpty() ? -1 : stack.peek();
-					max = Math.max(max, (rightIndex - leftIndex - 1) * height[popIndex]); // 此处面积公式要分清：左端，右端和高分别是什么。
-				} else { // 即 如果当前i位置的元素值如果大于等于栈顶值，则直接跳出循环然后把该index压入栈
-					break; 
-				}
+		for (int i = 0; i <= height.length; i++) { // i <= height小技巧，因为要在i==height.length处虚拟地再补一个“0”高度的元素，来把最后stack里的值都pop出来(如果stack还有值的话)
+			int rightIdx = i;
+			int rightH = i == height.length ? 0 : height[i]; // curH = 0的情况， 即上面说的小技巧
+			while (!stack.isEmpty() && rightH < height[stack.peek()]) { //说明当前i就是此时stack的栈顶元素的右端，可以将其pop出来并且计算以pop元素为高的最大矩形面积了) 
+				int curH = height[stack.pop()];
+				int leftIdx = stack.isEmpty() ? -1 : stack.peek(); // 【注】当stack为空时，leftIdx值为-1 而不是0
+				max = Math.max(max, (rightIdx - leftIdx - 1) * curH); // 此处面积公式要分清：左端，右端和高分别是什么。
 			}
-			stack.push(i); // 记得压栈的是index
+			stack.push(rightIdx); // 无论如何最后都要把当前i压入栈
 		}
     	return max;
     }
-	/*private int largestRectangleArea(int[] height) {
-		if (height == null || height.length == 0) {
-			return 0;
-		}
-		LinkedList<Integer> stack = new LinkedList<Integer>();// store index
-		int max = 0;
-		for (int i = 0; i < height.length; i++) {
-			while (!stack.isEmpty() && height[i] <= height[stack.peek()]) {
-				int index = stack.pop();
-				int curArea = stack.isEmpty() ? height[index] * i
-						: height[index] * (i - stack.peek() - 1);
-				max = Math.max(max, curArea);
-			}
-			stack.push(i);
-		}
-		while (!stack.isEmpty()) {
-			int index = stack.pop();
-			int curArea = stack.isEmpty() ? height[index] * height.length
-					: height[index] * (height.length - stack.peek() - 1);
-			max = Math.max(curArea, max);
-		}
-		return max;
-	}*/
-
 }

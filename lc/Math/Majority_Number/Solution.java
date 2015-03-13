67161b47737bc7993fc041190fe7a26dc6234d5e
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-13 12:40:51 -0700

    update Majority_Number

--- lc/Math/Majority_Number/Solution.java
+++ lc/Math/Majority_Number/Solution.java
@@ -23,26 +23,26 @@ public class Solution {
 	 * Expand O(n) time and O(1) space
 	 */
 
-    //中心思想是只要找到两个不一样的元素，就把他们抵消掉，最后剩下的就是超过 n / 2个数量的那个
-	public int majorityNumber(ArrayList<Integer> nums){
-    	if (nums == null || nums.size() == 0) {
-    		return Integer.MAX_VALUE;
-    	}
-        int candidate = nums.get(0);
-        int count = 1;
-        for (int i = 1; i < nums.size(); i++) {
-        	if (count == 0) { // 前面的全都抵消掉了
-        		candidate = nums.get(i);
-        		count = 1; //【注】勿忘count此时置1
-        	} else {
-        		if (candidate == nums.get(i)) { // 有出现了一个和candidate相同的数
-        			count++;
-        		} else { // 与candidate不同，则抵消掉一次。
-        			count--; 
-        		}
-        	}
-        }
-        return candidate;
-    }
+    //注释参考 Majority_Element https://oj.leetcode.com/problems/majority-element/
+	public int majorityNumber(ArrayList<Integer> nums) {
+		if (nums == null || nums.size() == 0) {
+			return Integer.MAX_VALUE;
+		}
+		int candidate = nums.get(0);
+		int count = 1;
+		for (int i = 1; i < nums.size(); i++) {
+			if (count == 0) {
+				candidate = nums.get(i);
+				count = 1;
+			} else {
+				if (candidate == nums.get(i)) {
+					count++;
+				} else {
+					count--;
+				}
+			}
+		}
+		return candidate;
+	}
 
 }

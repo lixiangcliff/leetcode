c12e3ae5cf89503120797eab6a5048f986d073f3
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-13 17:33:18 -0700

    update Merge_Intervals

--- lc/Sort/Merge_Intervals/Question.java
+++ lc/Sort/Merge_Intervals/Question.java
@@ -23,9 +23,38 @@ public class Question {
 	 * return[1,6],[8,10],[15,18].
 	 */
 	
+	//手写
+	public List<Interval> merge(List<Interval> intervals) {
+		List<Interval> result = new ArrayList<Interval>();
+		if (intervals == null || intervals.size() == 0) {
+			return result;
+		}
+		Comparator<Interval> comp = new Comparator<Interval>() { //【注】学习comparator的写法
+			@Override
+			public int compare(Interval i1, Interval i2) {
+				if (i1.start == i2.start) { // start相同，则比较end
+					return i1.end - i2.end;
+				}
+				return (i1.start - i2.start); // start不同，
+			}
+		};
+		Collections.sort(intervals, comp);
+		Interval pre = intervals.get(0);
+		for (int i = 1; i < intervals.size(); i++) {
+			if (intervals.get(i).start <= pre.end) { // pre的end在当前start的右边（即，上一个interval和当前interval有重合）
+				pre.end = Math .max(pre.end, intervals.get(i).end);// 则更新pre的end为：max（上一个end，当前end）
+			} else { // 上一个interval和当前interval没有重合，则将上一个interval直接加入结果集
+				result.add(pre);
+				pre = intervals.get(i);
+			}
+		}
+		result.add(pre); //【注】补上最后一个
+		return result;
+	}
+	
 	//看图
 	//http://blog.csdn.net/linhuanmars/article/details/21857617
-	public List<Interval> merge(List<Interval> intervals) {
+	public List<Interval> merge2(List<Interval> intervals) {
 		List<Interval> result = new ArrayList<Interval>();
 		if (intervals == null || intervals.size() == 0) {
 			return result;

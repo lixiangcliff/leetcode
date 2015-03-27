98ffdcddb0e4ed2e7f8e493b108369207f72cc48
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-27 00:04:33 -0700

    update Letter_Combinations_of_a_Phone_Number

--- lc/Backtracking/Letter_Combinations_of_a_Phone_Number/Question.java
+++ lc/Backtracking/Letter_Combinations_of_a_Phone_Number/Question.java
@@ -36,38 +36,36 @@ public class Question {
 	//http://blog.csdn.net/linhuanmars/article/details/19743197
 	public ArrayList<String> letterCombinations(String digits) {
 		ArrayList<String> result = new ArrayList<String>();
-		if (digits == null) {
+		if (digits == null || digits.length() == 0) {
 			return result;
 		}
-		HashMap<Character, char[]> num2LetterMap = new HashMap<Character, char[]>();
-		num2LetterMap.put('0', new char[]{}); // 0和1 对应的为空字符数组
-		num2LetterMap.put('1', new char[]{});
-		num2LetterMap.put('2', new char[]{'a','b','c'}); //【注】记住字符数组初始化的方法
-		num2LetterMap.put('3', new char[]{'d','e','f'});
-		num2LetterMap.put('4', new char[]{'g','h','i'});
-		num2LetterMap.put('5', new char[]{'j','k','l'});
-		num2LetterMap.put('6', new char[]{'m','n','o'});
-		num2LetterMap.put('7', new char[]{'p','q','r','s'});
-		num2LetterMap.put('8', new char[]{'t','u','v'});
-		num2LetterMap.put('9', new char[]{'w', 'x','y','z'});
-		StringBuilder sb = new StringBuilder();
-		helper(result, num2LetterMap, sb, digits);
+		HashMap<Character, String> map = new HashMap<Character, String>();
+		map.put('0', ""); // 0和1 对应的为空字符数组
+		map.put('1', "");
+		map.put('2', "abc");
+		map.put('3', "def");
+		map.put('4', "ghi");
+		map.put('5', "jkl");
+		map.put('6', "mno");
+		map.put('7', "pqrs");
+		map.put('8', "tuv");
+		map.put('9', "wxyz");
+		StringBuilder item = new StringBuilder();
+		helper(result, item, map, digits);
 		return result;
     }
 	
-	private void helper(ArrayList<String> result, HashMap<Character, char[]> num2LetterMap, StringBuilder sb, String digits) {
-		if (sb.length() == digits.length()) {
-			result.add(sb.toString());
+	private void helper(ArrayList<String> result, StringBuilder item, HashMap<Character, String> map, String digits) {
+		if (item.length() == digits.length()) {
+			result.add(item.toString());
 			return;
 		}
-		//num2LetterMap.get(digits.charAt(sb.length()))这个要想明白：
-		//举例说明比如digits==“23”，已经取到了第"1"位(设最左边位为第0位),要append到sb里的c应该是'd', 'e' 或者 'f'。
-		//于是char c : sth，这个sth就是{'d','e','f'}, 即num2LetterMap.get('3')
-		//又可以知道'3'的来源是digits.charAt(1), 而1的来源是此时sb的长度
-		for (char c : num2LetterMap.get(digits.charAt(sb.length()))) {
-			sb.append(c); // 找到一个符合的字符，append到sb后
-			helper(result, num2LetterMap, sb, digits); // 递归
-			sb.deleteCharAt(sb.length() - 1); // 回溯
+		int pos = item.length(); // 本轮要处理的digit中的位置
+		String letters = map.get(digits.charAt(pos)); // 该位置所有可能的字符
+		for (int i = 0; i < letters.length(); i++) {
+			item.append(letters.charAt(i));
+			helper(result, item, map, digits);
+			item.deleteCharAt(item.length() - 1);
 		}
 	}
 }

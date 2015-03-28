afc3554bb704067b5daaf2cb39e9691947bdb737
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-28 14:49:24 -0700

    adding zhe's method

--- lc/Backtracking/Sudoku_Solver/Question.java
+++ lc/Backtracking/Sudoku_Solver/Question.java
@@ -22,7 +22,63 @@ public class Question {
 	 * http://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Sudoku-by-L2G-20050714_solution.svg/250px-Sudoku-by-L2G-20050714_solution.svg.png
 	 * ...and its solution numbers marked in red.
 	 */
-	
+	//Zhe's method:
+/*	public void solveSudoku(char[][] board) {
+		char[][] result = new char[9][9];
+		try {
+			helper(board, 0, 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void helper(char[][] board, int i, int j) throws Exception {
+		if (i == 9) {
+			throw (new Exception());
+		}
+		if (board[i][j] == '.') {
+			for (int k = 1; k < 10; k++) {
+				if (isValid(board, i, j, k)) {
+					board[i][j] = (char) ('0' + k);
+					if (j == 8) {
+						helper(board, i + 1, 0);
+					} else {
+						helper(board, i, j + 1);
+					}
+					board[i][j] = '.';
+				}
+			}
+		} else {
+			if (j == 8) {
+				helper(board, i + 1, 0);
+			} else {
+				helper(board, i, j + 1);
+			}
+		}
+	}
+
+	private boolean isValid(char[][] board, int i, int j, int num) {
+		for (int k = 0; k < 9; k++) {
+			if (board[i][k] == (char) ('0' + num)) {
+				return false;
+			}
+		}
+		for (int k = 0; k < 9; k++) {
+			if (board[k][j] == (char) ('0' + num)) {
+				return false;
+			}
+		}
+		for (int p = (i - i % 3); p < (i - i % 3 + 3); p++) {
+			for (int q = (j - j % 3); q < (j - j % 3 + 3); q++) {
+				if (board[p][q] == (char) ('0' + num)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}*/
+
+
 	//DFS
 	//http://www.cnblogs.com/yuzhangcmu/p/4067733.html
 	//http://blog.csdn.net/linhuanmars/article/details/20748761

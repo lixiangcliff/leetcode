86d63c76ed4f47e2f366ca1c9f90a4c9f377e63f
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-28 14:27:01 -0700

    adding Implement_Heap

---
+++ lc/True_Interviews/Implement_Heap/Question.java
@@ -0,0 +1,25 @@
+package Implement_Heap;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * http://www.glassdoor.com/Interview/Describe-how-to-implement-a-max-heap-the-main-question-was-the-following-given-a-directed-graph-check-if-there-is-a-cycl-QTN_894669.htm
+	 * Describe how to implement a max-heap
+	 */
+	
+	//http://en.wikipedia.org/wiki/Binary_heap
+	//use array to implement
+	//time complexity:
+	//build heap: O(n)
+	//insert:     O(1)
+	//delete root O(nlgn)
+
+}

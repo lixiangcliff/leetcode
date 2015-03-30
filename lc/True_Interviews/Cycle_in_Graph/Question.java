a69860fe5709c7cfeeacd78c6f475c4d84c03260
lixiangcliff <lixiang.cliff@gmail.com>
2015-03-29 22:45:49 -0700

    adding new questions

---
+++ lc/True_Interviews/Cycle_in_Graph/Question.java
@@ -0,0 +1,19 @@
+package Cycle_in_Graph;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * http://www.glassdoor.com/Interview/Describe-how-to-implement-a-max-heap-the-main-question-was-the-following-given-a-directed-graph-check-if-there-is-a-cycl-QTN_894669.htm
+	 *  given a directed graph, check if there is a cycle in the graph. What is the complexity? 
+	 */
+	
+	
+}
---
+++ lc/True_Interviews/Sliding_Window_Maximum/Question.java
@@ -0,0 +1,20 @@
+package Sliding_Window_Maximum;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * http://articles.leetcode.com/2011/01/sliding-window-maximum.html
+	 * http://www.mitbbs.com/article_t/JobHunting/32918239.html
+	 */
+	
+	//discuss with Zhe
+
+}

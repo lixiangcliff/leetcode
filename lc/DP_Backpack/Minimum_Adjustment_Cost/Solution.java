9326b99da4b31e5d035ddeacf430e5401f12a6b4
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-01 00:26:40 -0700

    update Minimum_Adjustment_Cost and congrats Zhe to finish all 182 questions!

--- lc/DP_Backpack/Minimum_Adjustment_Cost/Solution.java
+++ lc/DP_Backpack/Minimum_Adjustment_Cost/Solution.java
@@ -9,6 +9,13 @@ public class Solution {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<Integer> A = new ArrayList<Integer>();
+		A.add(1);
+		A.add(4);
+		A.add(2);
+		A.add(3);
+		Solution s = new Solution();
+		System.out.println(s.MinAdjustmentCost(A, 1));
 
 	}
 	/**
@@ -26,9 +33,30 @@ public class Solution {
 	 * the adjustment cost is 2 and it's minimal. Return 2.
 	 */
 	
+	//http://www.cnblogs.com/yuzhangcmu/p/4153927.html
     public int MinAdjustmentCost(ArrayList<Integer> A, int target) {
         // write your code here
-    	return 0;
+    	int[][] res = new int[A.size()][101]; //res[i][v]把index = i的值修改为v，所需要的最小花费
+    	for (int i = 0; i < A.size(); i++) {
+    		for (int j = 1; j <= 100; j++) { // j表示v
+    			if (i == 0) {
+    				res[i][j] = Math.abs(j - A.get(i));
+    			} else {
+	    			res[i][j] = Integer.MAX_VALUE;
+	    			for (int k = 1; k < 101; k++) { //k表示v'
+	    				if (Math.abs(j - k) > target) {
+	    					continue;
+	    				}
+	    				res[i][j] = Math.min(res[i][j], res[i - 1][k] + Math.abs(A.get(i) - j));
+	    			}
+    			}
+    		}
+    	}
+    	int min = Integer.MAX_VALUE;
+    	for (int j = 1; j < 101; j++) {
+    		min = Math.min(min, res[A.size() - 1][j]);
+    	}
+    	return min;
     }
 
 }

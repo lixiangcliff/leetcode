297e2edcd3e5122c78c1ddbddfc22c9951a29d19
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-03 22:59:05 -0700

    adding question

---
+++ lc/Leetcode_New_Questions/Maximum_Gap/Question.java
@@ -0,0 +1,23 @@
+package Maximum_Gap;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * https://leetcode.com/problems/maximum-gap/
+	 * Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+		Try to solve it in linear time/space.
+		Return 0 if the array contains less than 2 elements.
+		You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+	 */
+	
+    public int maximumGap(int[] num) {
+        return 0;
+    }
+}
---
+++ lc/True_Interviews/Circle_Move_of_Robot/Question.java
@@ -0,0 +1,25 @@
+package Circle_Move_of_Robot;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * http://www.mitbbs.com/article_t/JobHunting/32923135.html
+	 * 一个robot，只有前进一步G，左转L，右转R 三种操作,  可以根据命令循环操作。 假
+		定地面足够大。
+		“L”    一直左转
+		“GL”  正方形循环
+		“GRGL ：  不是循环
+		如何判断 输入命令 是个 循环？
+	 */
+	
+	//http://www.geeksforgeeks.org/check-if-a-given-sequence-of-moves-for-a-robot-is-circular-or-not/
+
+}

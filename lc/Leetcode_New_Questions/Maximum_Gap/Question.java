bcc1a775d4c47e6d20768a0997c447f99fb702d8
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-04 11:10:02 -0700

    update Maximum_Gap

--- lc/Leetcode_New_Questions/Maximum_Gap/Question.java
+++ lc/Leetcode_New_Questions/Maximum_Gap/Question.java
@@ -1,5 +1,7 @@
 package Maximum_Gap;
 
+import java.util.ArrayList;
+
 public class Question {
 
 	/**
@@ -7,7 +9,9 @@ public class Question {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Question q = new Question();
+		int[] num = {0, 9 ,10 ,19};
+		System.out.println(q.maximumGap(num));
 	}
 	/**
 	 * https://leetcode.com/problems/maximum-gap/
@@ -17,7 +21,43 @@ public class Question {
 		You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
 	 */
 	
+	//bucket: http://blog.csdn.net/u012162613/article/details/41936569
+	//radix: http://yucoding.blogspot.com/2014/12/leetcode-question-maximum-gap.html
     public int maximumGap(int[] num) {
-        return 0;
+    	if (num == null || num.length == 0) {
+    		return 0;
+    	}
+    	int min = Integer.MAX_VALUE;
+    	int max = Integer.MIN_VALUE;
+    	for (int val : num) {
+    		min = Math.min(val, min);
+    		max = Math.max(val, max);
+    	}
+    	int bkSize = (max - min) / num.length + 1; //【注】桶排序从整体分布上来讲，趋于每个桶里放一个元素
+    	int bkCount = (max - min) / bkSize + 1;
+    	ArrayList<ArrayList<Integer>> bks = new ArrayList<ArrayList<Integer>>();
+    	for (int i = 0; i < bkCount; i++) {
+    		bks.add(new ArrayList<Integer>());
+    	}
+    	for (int val : num) {
+    		int idx = (val - min) / bkSize;
+    		if (!bks.get(idx).isEmpty()) {
+    			bks.get(idx).set(0, Math.min(val, bks.get(idx).get(0))); //local min
+    			bks.get(idx).set(1, Math.max(val, bks.get(idx).get(1))); // local max
+    		} else {
+    			bks.get(idx).add(val);
+    			bks.get(idx).add(val);
+    		}
+    	}
+    	int preIdx = 0;
+    	int gap = 0;
+    	for (int i = 1; i < bkCount; i++) {
+    		if (bks.get(i).isEmpty()) {
+    			continue;
+    		}
+    		gap = Math.max(gap, bks.get(i).get(0) - bks.get(preIdx).get(1));//桶号大的min - 桶号大的max
+    		preIdx = i;
+    	}
+    	return gap;
     }
 }

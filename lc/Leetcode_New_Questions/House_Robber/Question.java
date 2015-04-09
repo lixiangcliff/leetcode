96e119b7816f1be519005e7127eb1224ab9c5b11
lixiangcliff <lixiang.cliff@gmail.com>
2015-04-08 17:51:17 -0700

    adding House_Robber

---
+++ lc/Leetcode_New_Questions/House_Robber/Question.java
@@ -0,0 +1,38 @@
+package House_Robber;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * 
+	 * You are a professional robber planning to rob houses along a street. 
+	 * Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that 
+	 * adjacent houses have security system connected and it will automatically contact the police
+	 *  if two adjacent houses were broken into on the same night.
+		Given a list of non-negative integers representing the amount of money of each house, 
+		determine the maximum amount of money you can rob tonight without alerting the police.
+	 */
+
+	//1 seq DP http://www.cnblogs.com/pkuYang/p/4391020.html
+	//res[i]前i个数的符合条件的最大sum res[i]
+	//res[i] = Math(res[i - 1], res[i - 2] + num[i]) 即不取num[i]和取num[i]
+	public int rob(int[] num) {
+		if (num == null || num.length == 0) {
+			return 0;
+		}
+		int[] res = new int[num.length + 1];
+		res[0] = 0;
+		res[1] = num[0];
+		for (int i = 2; i <= num.length; i++) {
+			res[i] = Math.max(res[i - 1], num[i - 1] + res[i - 2]); // 位差
+		}
+		return res[num.length];
+	}
+}

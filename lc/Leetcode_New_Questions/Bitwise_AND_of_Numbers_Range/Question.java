013e51860890349e6a94e8250ca12c479990dcd7
lixiangcliff <lixiang.cliff@gmail.com>
2015-05-12 22:21:37 -0700

    add Bitwise_AND_of_Numbers_Range

---
+++ lc/Leetcode_New_Questions/Bitwise_AND_of_Numbers_Range/Question.java
@@ -0,0 +1,24 @@
+package Bitwise_AND_of_Numbers_Range;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Given a range [m, n] where 0 <= m <= n <= 2147483647, 
+	 * return the bitwise AND of all numbers in this range, inclusive.
+	 * 
+	 * For example, given the range [5, 7], you should return 4.
+	 */
+	
+    public int rangeBitwiseAnd(int m, int n) {
+        return 0;
+    }
+
+}

fc6d70b2b6a5866fcc1e6bc341040d8497401682
lixiangcliff <lixiang.cliff@gmail.com>
2015-05-18 20:38:31 -0700

    update Multiply_Strings

--- lc/High_Accuracy/Multiply_Strings/Question.java
+++ lc/High_Accuracy/Multiply_Strings/Question.java
@@ -34,26 +34,21 @@ public class Question {
 		int len2 = num2.length();
 		int[] product = new int[len1 + len2]; // 【注】 技巧是用一个数组存每一位的product的和，这样即使在该位的累计超过9，之后仍可以用进位的方式处理
 		for (int i = 0; i < len1; i++) { // 计算相应位置的product.
-			for (int j = 0; j < len2; j++) { // 边界画图举例。num1和num2是从后往前相乘的，但存入product的时候是从前往后的。
-				product[i + j] += (int) (num1.charAt(len1 - 1 - i) - '0') * (int) (num2.charAt(len2 - 1 - j) - '0'); // 积累每一位乘积的和
+			for (int j = 0; j < len2; j++) { // 边界画图举例。
+				product[i + j] += (int) (num1.charAt(i) - '0') * (int) (num2.charAt(j) - '0'); // 积累每一位乘积的和
 			}
 		}
 		StringBuilder result = new StringBuilder();
 		int carry = 0;
-		for (int i = 0; i < product.length; i++) {
+		for (int i = product.length - 2; i >= 0 ; i--) { //【注】 i的起始位置
 			int sum = product[i] + carry;
 			int digit = sum % 10;
 			result.append(digit);
 			carry = sum / 10;
 		}
-		//【注】下面“去0”的操作，必须在把product的每一位进位都处理好存入result之后才能做，下面是如果处理进位之前就做会产生的问题：
-		// Input:	"9", "9"
-		// Output:	"1"
-		// Expected:	"81"
-		if (result.length() > 1 && result.charAt(result.length() - 1) == '0') { // 去掉前面可能的'0'
-			result.deleteCharAt(result.length() - 1);
+		if (carry != 0) { // 【注】如果仍有进位，再把carry给append了
+		   result.append(carry); 
 		}
-
 		return result.reverse().toString();
 	}
 }

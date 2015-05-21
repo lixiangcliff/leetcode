789b8e10955e78b6da979317ddd5a71c4ca0cad1
lixiangcliff <lixiang.cliff@gmail.com>
2015-05-20 22:41:42 -0700

    update Bitwise_AND_of_Numbers_Range

--- lc/Math/Bitwise_AND_of_Numbers_Range/Question.java
+++ lc/Math/Bitwise_AND_of_Numbers_Range/Question.java
@@ -24,6 +24,8 @@ public class Question {
 	
 	//http://www.meetqun.com/thread-8769-1-1.html
 	//https://leetcode.com/discuss/32278/8line-c-simple-clear-solution
+	//1.只要m和n转化为bin之后，位数不相等，则结果必为0
+	//2.如果位数相等，m和n分别右移，直到m和n相等之前，所有右边的位“and”之后的结果都为0
 	public int rangeBitwiseAnd(int m, int n) {
 		int i = 0;
 		while (m != n) {
@@ -34,6 +36,7 @@ public class Question {
 		return m << i;
 	}
 	
+	//http://stackoverflow.com/questions/15370250/how-does-this-line-works-n-nn-1
     public int rangeBitwiseAnd2(int m, int n) {
     	while (n > m) {
     		n &= n - 1;

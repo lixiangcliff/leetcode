7740fe9469da129812cd29f130dbbcafa6af6358
lixiangcliff <lixiang.cliff@gmail.com>
2015-05-27 09:11:51 -0700

    update Evaluate_Reverse_Polish_Notation

--- lc/Stack/Evaluate_Reverse_Polish_Notation/Question.java
+++ lc/Stack/Evaluate_Reverse_Polish_Notation/Question.java
@@ -27,7 +27,8 @@ public class Question {
 	
 	// http://www.ninechapter.com/solutions/evaluate-reverse-polish-notation/
 	// http://www.cnblogs.com/yuzhangcmu/p/4124870.html
-	//【注】 严谨的话，每次pop()之前都要先检查stack是否empty
+	//【注】 严谨的话，每次pop()之前都要先检查stack是否empty。
+	//代码里三个【可能】可以写的更严谨
 	public int evalRPN(String[] tokens) {
 		if (tokens == null || tokens.length == 0) {
 			return 0;
@@ -35,23 +36,23 @@ public class Question {
 		LinkedList<Integer> stack = new LinkedList<Integer>();
 		String opers = "+-*/";
 		for (String token : tokens) {
-			if (!opers.contains(token)) {
+			if (!opers.contains(token)) { //【注】判断是符号还是数字的技巧
 				stack.push(Integer.parseInt(token));
 			} else {
-				int num2 = stack.pop();
-				int num1 = stack.pop();
+				int num2 = stack.pop(); //先弹出操作数
+				int num1 = stack.pop(); //后弹出被操作数
 				if (token.equals("+")) {
 					stack.push(num1 + num2);
 				} else if (token.equals("-")) {
 					stack.push(num1 - num2);
 				} else if (token.equals("*")) {
-					stack.push(num1 * num2);
+					stack.push(num1 * num2);	//【可能】溢出。
 				} else if (token.equals("/")) {
-					stack.push(num1 / num2);
+					stack.push(num1 / num2); //【可能】num2 == 0。
 				}
 			}
 		}
-		return stack.pop();
+		return stack.pop(); //【可能】pop完之后stack里仍有剩余。
 	}
     
 }

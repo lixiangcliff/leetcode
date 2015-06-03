709dfecfa1b68ff0f4b8ec58bdd11fedb1088324
lixiangcliff <lixiang.cliff@gmail.com>
2015-06-02 22:03:10 -0700

    add Implement_Trie_Prefix_Tree

---
+++ lc/Leetcode_New_Questions/Implement_Trie_Prefix_Tree/Question.java
@@ -0,0 +1,19 @@
+package Implement_Trie_Prefix_Tree;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Implement a trie with insert, search, and startsWith methods.
+	 * Note:
+	 * You may assume that all inputs are consist of lowercase letters a-z.
+	 */
+
+}

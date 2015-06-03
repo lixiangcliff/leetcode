acb5064935fde9a0e7696d803c66e4ea38ecf295
lixiangcliff <lixiang.cliff@gmail.com>
2015-06-03 15:29:26 -0700

    update Implement_Trie_Prefix_Tree

--- lc/Leetcode_New_Questions/Implement_Trie_Prefix_Tree/Question.java
+++ lc/Leetcode_New_Questions/Implement_Trie_Prefix_Tree/Question.java
@@ -11,6 +11,7 @@ public class Question {
 	}
 	
 	/**
+	 * https://leetcode.com/problems/implement-trie-prefix-tree/
 	 * Implement a trie with insert, search, and startsWith methods.
 	 * Note:
 	 * You may assume that all inputs are consist of lowercase letters a-z.

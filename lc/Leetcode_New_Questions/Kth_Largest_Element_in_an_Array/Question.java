23d76b06769ff1bfd6a7f8671e375083cdf696c6
lixiangcliff <lixiang.cliff@gmail.com>
2015-06-09 22:55:04 -0700

    update Kth_Largest_Element_in_an_Array

--- lc/Leetcode_New_Questions/Kth_Largest_Element_in_an_Array/Question.java
+++ lc/Leetcode_New_Questions/Kth_Largest_Element_in_an_Array/Question.java
@@ -26,6 +26,7 @@ public class Question {
 	 */
 	
 	//PriorityQueue == min heap
+	//way to impement max heap: PriorityQueue<Integer> queue = new PriorityQueue<>(10, Collections.reverseOrder());
     public int findKthLargest(int[] nums, int k) {
     	PriorityQueue<Integer> heap = new PriorityQueue<Integer>(k);
     	for (int i = 0; i < k; i++) {

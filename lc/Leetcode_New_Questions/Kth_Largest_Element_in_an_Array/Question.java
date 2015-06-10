2b211e55e9da42e5f01dfc872bbe48ea678a07c9
lixiangcliff <lixiang.cliff@gmail.com>
2015-06-09 22:53:56 -0700

    add Kth_Largest_Element_in_an_Array

---
+++ lc/Leetcode_New_Questions/Kth_Largest_Element_in_an_Array/Question.java
@@ -0,0 +1,42 @@
+package Kth_Largest_Element_in_an_Array;
+
+import java.util.PriorityQueue;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Question q = new Question();
+		int[] nums = {3,2,1,5,6,4};
+		int k = 2;
+		System.out.println(q.findKthLargest(nums, k));
+	}
+	
+	/**
+	 * Find the kth largest element in an unsorted array. 
+	 * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+	 * 
+	 * For example,
+	 * Given [3,2,1,5,6,4] and k = 2, return 5.
+	 * Note: 
+	 * You may assume k is always valid, 1 ≤ k ≤ array's length.
+	 */
+	
+	//PriorityQueue == min heap
+    public int findKthLargest(int[] nums, int k) {
+    	PriorityQueue<Integer> heap = new PriorityQueue<Integer>(k);
+    	for (int i = 0; i < k; i++) {
+    		heap.add(nums[i]);
+    	}
+    	for (int i = k; i < nums.length; i++) {
+    		if (nums[i] > heap.peek()) {
+    			heap.remove();
+    			heap.add(nums[i]);
+    		}
+    	}
+    	return heap.peek();
+    }
+}

dac9ef085b0ce3efd654c449d58b74bfe20085c6
lixiangcliff <lixiang.cliff@gmail.com>
2015-06-11 16:47:03 -0700

    add Contains_Duplicate_III

---
+++ lc/Leetcode_New_Questions/Contains_Duplicate_III/Question.java
@@ -0,0 +1,23 @@
+package Contains_Duplicate_III;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * https://leetcode.com/problems/contains-duplicate-iii/
+	 * Given an array of integers, find out whether there are two distinct indices i and j in the array 
+	 * such that the difference between nums[i] and nums[j] is at most t and the difference between i and j is at most k.
+	 */
+	
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        return false;
+    }
+
+}

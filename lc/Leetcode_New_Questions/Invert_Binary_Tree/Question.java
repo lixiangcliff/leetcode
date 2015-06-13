3fa1f3915527f16403e259c943292076ed67492b
lixiangcliff <lixiang.cliff@gmail.com>
2015-06-12 23:24:55 -0700

    add Invert_Binary_Tree

---
+++ lc/Leetcode_New_Questions/Invert_Binary_Tree/Question.java
@@ -0,0 +1,52 @@
+package Invert_Binary_Tree;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * https://leetcode.com/problems/invert-binary-tree/
+	 * Invert a binary tree.
+			
+			     4
+			   /   \
+			  2     7
+			 / \   / \
+			1   3 6   9
+			
+			to
+			     4
+			   /   \
+			  7     2
+			 / \   / \
+			9   6 3   1
+	 */
+	
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+        	return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}

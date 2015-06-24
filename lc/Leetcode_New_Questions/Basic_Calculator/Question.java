26db0c377ad615c3deb2aac5afe73913e722ac43
lixiangcliff <lixiang.cliff@gmail.com>
2015-06-23 23:17:17 -0700

    Add Basic_Calculator

---
+++ lc/Leetcode_New_Questions/Basic_Calculator/Question.java
@@ -0,0 +1,28 @@
+package Basic_Calculator;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * https://leetcode.com/problems/basic-calculator/
+	 * Implement a basic calculator to evaluate a simple expression string.
+	 * The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+	 * You may assume that the given expression is always valid.
+	 * "1 + 1" = 2
+	 * " 2-1 + 2 " = 3
+	 * "(1+(4+5+2)-3)+(6+8)" = 23
+	 * Note: Do not use the eval built-in library function.
+	 */
+
+    public int calculate(String s) {
+    	int res = 0;
+        return res;
+    }
+}

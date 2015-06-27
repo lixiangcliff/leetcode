31a0f40ab9ce863075e23f2bfadd05ae54885ddf
lixiangcliff <lixiang.cliff@gmail.com>
2015-06-26 22:48:28 -0700

    add Basic_Calculator_II

---
+++ lc/Leetcode_New_Questions/Basic_Calculator_II/Question.java
@@ -0,0 +1,32 @@
+package Basic_Calculator_II;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * https://leetcode.com/problems/basic-calculator-ii/
+	 * Implement a basic calculator to evaluate a simple expression string.
+	 * The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . 
+	 * The integer division should truncate toward zero.
+	 * You may assume that the given expression is always valid.
+	 * 
+	 * Some examples:
+	 * "3+2*2" = 7
+	 * " 3/2 " = 1
+	 * " 3+5 / 2 " = 5
+	 * 
+	 * Note: Do not use the eval built-in library function.
+	 */
+	
+    public int calculate(String s) {
+        return 0;
+    }
+
+}

ae8562e4719df75bafc730e86c5d057a7252473b
lixiangcliff <lixiang.cliff@gmail.com>
2015-07-07 22:23:52 -0700

    Add Summary_Ranges

---
+++ lc/Leetcode_New_Questions/Summary_Ranges/Question.java
@@ -0,0 +1,27 @@
+package Summary_Ranges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * https://leetcode.com/problems/summary-ranges/
+	 * Given a sorted integer array without duplicates, return the summary of its ranges.
+	 * For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+	 */
+	
+    public List<String> summaryRanges(int[] nums) {
+    	List<String> res = new ArrayList<String>();
+    	return res;
+    }
+
+}

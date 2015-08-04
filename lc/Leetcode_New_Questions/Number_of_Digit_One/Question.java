db2d02dc75c6ca870145017614c906b1142e692f
lixiangcliff <lixiang.cliff@gmail.com>
2015-08-04 06:54:33 -0700

    add new question but not resolved

---
+++ lc/Leetcode_New_Questions/Number_of_Digit_One/Question.java
@@ -0,0 +1,27 @@
+package Number_of_Digit_One;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * https://leetcode.com/problems/number-of-digit-one/
+	 * Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
+	 * 
+	 * For example:
+	 * Given n = 13,
+	 * Return 6, because digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+	 */
+	
+	//http://www.cnblogs.com/grandyang/p/4629032.html
+    public int countDigitOne(int n) {
+        
+    }
+
+}
---
+++ lc/Leetcode_New_Questions/Palindrome_Linked_List/Question.java
@@ -0,0 +1,34 @@
+package Palindrome_Linked_List;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * https://leetcode.com/problems/palindrome-linked-list/
+	 * Given a singly linked list, determine if it is a palindrome.
+	 * Follow up:
+	 * Could you do it in O(n) time and O(1) space?
+	 */
+	
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+
+}
+
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}

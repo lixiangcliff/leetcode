7a552208bc1b07d4c5160fde4808524596d8ee82
lixiangcliff <lixiang.cliff@gmail.com>
2015-08-22 09:58:29 -0700

    update Multiply_Strings

--- lc/High_Accuracy/Multiply_Strings/Question.java
+++ lc/High_Accuracy/Multiply_Strings/Question.java
@@ -35,12 +35,12 @@ public class Question {
 		int[] product = new int[len1 + len2]; // 【注】 技巧是用一个数组存每一位的product的和，这样即使在该位的累计超过9，之后仍可以用进位的方式处理
 		for (int i = 0; i < len1; i++) { // 计算相应位置的product.
 			for (int j = 0; j < len2; j++) { // 边界画图举例。
-				product[i + j] += (int) (num1.charAt(i) - '0') * (int) (num2.charAt(j) - '0'); // 积累每一位乘积的和
+				product[i + j + 1] += (int) (num1.charAt(i) - '0') * (int) (num2.charAt(j) - '0'); // 积累每一位乘积的和
 			}
 		}
 		StringBuilder result = new StringBuilder();
 		int carry = 0;
-		for (int i = product.length - 2; i >= 0 ; i--) { //【注】 i的起始位置
+		for (int i = product.length - 1; i >= 1 ; i--) { //【注】 i的边界位置
 			int sum = product[i] + carry;
 			int digit = sum % 10;
 			result.append(digit);

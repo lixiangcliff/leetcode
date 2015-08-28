11a03a7c9ae1e8c987ea2c0ae785c3f8ee4aa884
lixiangcliff <lixiang.cliff@gmail.com>
2015-08-28 16:56:27 -0700

    update

--- lc/Leetcode_New_Questions/Factor_Combinations/Question.java
+++ lc/Leetcode_New_Questions/Factor_Combinations/Question.java
@@ -74,12 +74,10 @@ public class Question {
 	}
 
 	private void helper(List<List<Integer>> result, List<Integer> item, int n, int start){
-	    if (n <= 1) {
-	        if (item.size() > 1) {
-	            result.add(new ArrayList<Integer>(item));
-	        }
-	        return;
-	    }
+		if (n <= 1 && item.size() > 1) {
+			result.add(new ArrayList<Integer>(item));
+			return;
+		}
 	    for (int i = start; i <= n; ++i) {
 	        if (n % i == 0) {
 				item.add(i);

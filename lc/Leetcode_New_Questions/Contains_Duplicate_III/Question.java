56153fcd1d678838748822d8a0f5e6944632600b
lixiangcliff <lixiang.cliff@gmail.com>
2015-08-28 13:29:44 -0700

    update

--- lc/Leetcode_New_Questions/Contains_Duplicate_III/Question.java
+++ lc/Leetcode_New_Questions/Contains_Duplicate_III/Question.java
@@ -1,5 +1,8 @@
 package Contains_Duplicate_III;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Question {
 
 	/**
@@ -16,8 +19,25 @@ public class Question {
 	 * such that the difference between nums[i] and nums[j] is at most t and the difference between i and j is at most k.
 	 */
 	
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        return false;
-    }
+	//时间复杂度为 O(n logk)
+	//http://blog.csdn.net/xudli/article/details/46323247
+	public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+		if (k < 1 || t < 0 || nums == null || nums.length < 2) {
+			return false;
+		}
+		SortedSet<Long> set = new TreeSet<Long>();
+
+		for (int j = 0; j < nums.length; j++) {
+			SortedSet<Long> subSet = set.subSet((long) nums[j] - t, (long) nums[j] + t + 1);
+			if (!subSet.isEmpty())
+				return true;
+
+			if (j >= k) {
+				set.remove((long) nums[j - k]);
+			}
+			set.add((long) nums[j]);
+		}
+		return false;
+	}
 
 }

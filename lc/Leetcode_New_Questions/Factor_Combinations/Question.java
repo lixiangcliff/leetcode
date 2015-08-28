0d3416cba399849b555aebc5d9043c125ad4021b
lixiangcliff <lixiang.cliff@gmail.com>
2015-08-27 20:31:16 -0700

    add Question

---
+++ lc/Leetcode_New_Questions/Factor_Combinations/Question.java
@@ -0,0 +1,92 @@
+package Factor_Combinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Question q = new Question();
+		List<List<Integer>> result = q.getFactors(24);
+		
+		for (int i = 0; i < result.size(); i++) {
+			ArrayList<Integer> item = (ArrayList<Integer>) result.get(i);
+			for (int j = 0; j < item.size(); j++) {
+				System.out.print(item.get(j)+ ",");
+			}
+			System.out.println("");
+		}
+	}
+	
+	/**
+	 * Numbers can be regarded as product of its factors. For example,
+			8 = 2 x 2 x 2;
+		  	= 2 x 4.
+		Write a function that takes an integer n and return all possible combinations of its factors.
+		Note:
+		Each combination’s factors must be sorted ascending, for example: The factors of 2 and 6 is [2, 6], not [6, 2].
+		You may assume that n is always positive.
+		Factors should be greater than 1 and less than n.
+		Examples:
+		
+		input: 1
+		
+		output:
+		[]
+		
+		input: 37
+		output:
+		[]
+		
+		input: 12
+		output:
+		[
+		  [2, 6],
+		  [2, 2, 3],
+		  [3, 4]
+		]
+		
+		input: 32
+		output:
+		[
+		  [2, 16],
+		  [2, 2, 8],
+		  [2, 2, 2, 4],
+		  [2, 2, 2, 2, 2],
+		  [2, 4, 4],
+		  [4, 8]
+		]																																			
+	 */
+	
+	// 类似combination sum
+	// https://leetcode.com/discuss/51250/my-recursive-dfs-java-solution
+	public List<List<Integer>> getFactors(int n) {
+	    List<List<Integer>> result = new ArrayList<List<Integer>>();
+	    List<Integer> item = new ArrayList<Integer>();
+	    int start = 2;
+	    helper(result, item , n, start);
+	    return result;
+	}
+
+	public void helper(List<List<Integer>> result, List<Integer> item, int n, int start){
+	    if (n <= 1) {
+	        if (item.size() > 1) {
+	            result.add(new ArrayList<Integer>(item));
+	        }
+	        return;
+	    }
+	    for (int i = start; i <= n; ++i) {
+	        if (n % i == 0) {
+				item.add(i);
+				helper(result, item, n / i, i);
+				item.remove(item.size() - 1);
+	        }
+	    }
+	}
+
+}

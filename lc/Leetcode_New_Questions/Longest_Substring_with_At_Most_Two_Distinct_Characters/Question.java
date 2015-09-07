6818c8ce8ce62dabff10277e19d0d52363477fa8
lixiangcliff <lixiang.cliff@gmail.com>
2015-09-07 07:06:11 -0700

    add Longest_Substring_with_At_Most_Two_Distinct_Characters

---
+++ lc/Leetcode_New_Questions/Longest_Substring_with_At_Most_Two_Distinct_Characters/Question.java
@@ -0,0 +1,27 @@
+package Longest_Substring_with_At_Most_Two_Distinct_Characters;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Given a string S, find the length of the longest substring T that contains at most two
+		distinct characters.
+		For example,
+		Given S = “eceba”,
+		T is "ece" which its length is 3.
+	 */
+	
+	//leetcode cleancodehandbook
+	public int lengthOfLongestSubstringTwoDistinct(String s) {
+		int max = 0;
+		return max;
+	}
+
+}
